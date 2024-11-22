c334857294ceb963aa58a3dcb4bd4f907ef74f3a
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2024-11-22 17:18:18 -0600

    Time: 0 ms (100.00%), Space: 41.6 MB (30.67%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -17,35 +17,35 @@ class Solution
 {
     public List<Integer> inorderTraversal(TreeNode root)
     {
-        ArrayList<Integer> al = new ArrayList<>();
+        ArrayList<Integer> inorder = new ArrayList<>();
         TreeNode curr = root;
-        while(curr!=null)
-        {
-            if(curr.left==null)
-            {
-                al.add(curr.val);
-                curr = curr.right;
-            }
-            else
-            {
-                TreeNode prev = curr.left;
-                while(prev.right!=null && prev.right!=curr)
-                {
-                    prev = prev.right;
-                }
-                if(prev.right==null)
-                {
-                    prev.right = curr;
-                    curr = curr.left;
-                }
-                else
-                {
-                    prev.right = null;
-                    al.add(curr.val);
-                    curr = curr.right;
-                }
-            }
-        }
-        return al;
+         while(curr!=null)
+         {
+             if(curr.left==null)
+             {
+                 inorder.add(curr.val);
+                 curr = curr.right;
+             }
+             else
+             {
+                 TreeNode prev = curr.left;
+                 while(prev.right!= null && prev.right!=curr)
+                 {
+                     prev = prev.right;
+                 }
+                 if(prev.right==null)
+                 {
+                     prev.right = curr;
+                     curr = curr.left;
+                 }
+                 else
+                 {
+                     prev.right = null;
+                     inorder.add(curr.val);
+                     curr = curr.right;
+                 }
+             }
+         }
+        return inorder;
     }
 }
