0249de97794417958ff43da7ffb4836b9cf3066c
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2024-12-18 13:43:07 -0600

    Time: 2 ms (84.13%), Space: 44.5 MB (76.36%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -7,23 +7,18 @@ class Solution {
 
     public void findCombinations(int[] arr, int target, int ind, List<Integer> ds, List<List<Integer>> ans) {
         if (target == 0) {
-            // Add the current combination to the result and stop recursion
             ans.add(new ArrayList<>(ds));
             return;
         }
         if (ind == arr.length || target < 0) {
-            // Stop further recursion if index exceeds bounds or target is negative
             return;
         }
 
-        // Include the current element
         if (target >= arr[ind]) {
             ds.add(arr[ind]);
             findCombinations(arr, target - arr[ind], ind, ds, ans);
             ds.remove(ds.size() - 1);
         }
-
-        // Exclude the current element and move to the next
         findCombinations(arr, target, ind + 1, ds, ans);
     }
 }
