98f22a3df697d22643531a4891ba405bc44dacc1
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-01-13 12:50:23 -0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Cycle in a Directed Graph/cycle-in-a-directed-graph.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V; i++) list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if (new Solution().isCyclic(V, list) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution 
+{
+    
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        int n = adj.size();
+        int visited[] = new int[n];
+        int pathvisited[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            if(visited[i]==0)
+            {
+                if(dfs(adj,visited,pathvisited,i)==true)
+                    return true;
+            }
+        }
+        return false;
+    }
+    public boolean dfs(ArrayList<ArrayList<Integer>> adj, int visited[], 
+    int []pathvisited,int node)
+    {
+        visited[node]=1;
+        pathvisited[node]=1;
+        for(int con:adj.get(node))
+        {
+            if(visited[con]==0)
+            {
+                if(dfs(adj,visited,pathvisited,con)==true)
+                    return true;
+            }
+            else if(visited[con]==1 && pathvisited[con]==1)
+            {
+                return true;
+            }
+        }
+        pathvisited[node]=0;
+        return false;
+    }
+}
