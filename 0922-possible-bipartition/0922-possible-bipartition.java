0208fd80ba241efc9000494b8b090c1de8f1d8ff
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-01-16 15:57:07 -0600

    Time: 50 ms (6.24%), Space: 53.3 MB (29.49%) - LeetHub

---
+++ 0922-possible-bipartition/0922-possible-bipartition.java
@@ -0,0 +1,59 @@
+class Solution 
+{
+    public boolean possibleBipartition(int n, int[][] dislikes) 
+    {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int likes[] = new int[n+1];
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=0;i<n+1;i++)
+        {
+            adj.add(new ArrayList<>());
+            likes[i]=-1;
+        }
+        for(int i=0;i<dislikes.length;i++)
+        {
+            int a = dislikes[i][0];
+            int b = dislikes[i][1];
+            adj.get(a).add(b);
+            adj.get(b).add(a);
+        }
+        System.out.println(adj);
+        // q.offer(1);
+        // likes[1] = 0;
+        for(int i=1;i<n+1;i++)
+        {
+            if(likes[i]==-1)
+            {
+                if(bipartite(adj,i,likes,q)==false)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean bipartite(ArrayList<ArrayList<Integer>> adj,int node,int likes[],Queue<Integer> q)
+    {
+        q.offer(node);
+        likes[node]=0;
+        
+        while(!q.isEmpty())
+        {
+            int pop=q.poll();
+            for(int adjn:adj.get(pop))
+            {
+                if(likes[adjn]==-1)
+                {
+                    likes[adjn]=1-likes[pop];
+                    q.offer(adjn);
+                }
+                else if(likes[adjn]==likes[pop])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
