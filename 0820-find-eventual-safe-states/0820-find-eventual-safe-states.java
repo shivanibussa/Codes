dcaca85c2e6817dba56f9c44da2531464147d930
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-01-15 19:01:10 -0600

    Time: 29 ms (19.29%), Space: 55.6 MB (50.58%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -0,0 +1,52 @@
+class Solution 
+{
+    public List<Integer> eventualSafeNodes(int[][] graph) 
+    {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int N = graph.length;
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int i=0;i<N;i++)
+        {
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<graph[i].length;j++)
+            {
+                adj.get(graph[i][j]).add(i);
+            }
+        }
+        int inorder[] = new int[N];
+        for(int i=0;i<N;i++)
+        {
+            for(int it:adj.get(i))
+            {
+                inorder[it]++;
+            }
+        }
+        for(int i=0;i<N;i++)
+        {
+            if(inorder[i]==0)
+            {
+                q.offer(i);
+            }
+        }
+        while(!q.isEmpty())
+        {
+            int pop = q.poll();
+            arr.add(pop);
+            for(int it:adj.get(pop))
+            {
+                inorder[it]--;
+                if(inorder[it]==0)
+                {
+                    q.offer(it);
+                }
+            }
+        }
+        Collections.sort(arr);
+        return arr;
+    }
+}
