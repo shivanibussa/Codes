00f103eb2138aaae4c57f076752e337ff0b4f0b7
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-01-22 16:30:53 -0600

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -37,40 +37,86 @@ public class Main {
 
 
 // User function Template for Java
+class DisjointSet
+{
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjointSet(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            parent.add(i);
+            size.add(1);
+        }
+    }
+    
+    public int findUParent(int node)
+{
+    if(node == parent.get(node))
+    {
+        return node;
+    }
+    parent.set(node, findUParent(parent.get(node)));
+    return parent.get(node);
+}
 
+    
+    public void UnionBySize(int u,int v)
+    {
+        int ulp_u = findUParent(u);
+        int ulp_v = findUParent(v);
+        if(size.get(ulp_u)<size.get(ulp_v))
+        {
+            parent.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
+        }
+        else
+        {
+            parent.set(ulp_v,ulp_u);
+            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
+        }
+    }
+}
+class Edge implements Comparable<Edge> {
+    int src, dest, weight;
+    Edge(int _src, int _dest, int _wt) {
+        this.src = _src; this.dest = _dest; this.weight = _wt;
+    }
+    
+    public int compareTo(Edge compareEdge) {
+        return this.weight - compareEdge.weight;
+    }
+}
 class Solution 
 {
     static int spanningTree(int V, int E, List<List<int[]>> adj) 
     {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
-        int vis[] = new int[V];
-        int sum=0;
-        pq.offer(new int[]{0,0});
-        while(!pq.isEmpty())
-        {
-            int pops[] = pq.poll();
-            int weight = pops[0];
-            int node = pops[1];
-            if(vis[node]==1)
-            {
-                continue;
+        List<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                int adjNode = adj.get(i).get(j)[0];
+                int wt = adj.get(i).get(j)[1];
+                int node = i;
+                Edge temp = new Edge(i, adjNode, wt);
+                edges.add(temp);
             }
-            vis[node]=1;
-            sum = sum+weight;
-            
-            for(int adjn[]:adj.get(node))
-            {
-                int vertex = adjn[0];
-                int wt = adjn[1];
-                if(vis[vertex]==0)
-                {
-                    pq.offer(new int[]{wt,vertex});
-                }
-                
-            }
-            
         }
-        
-        return sum;
+    
+    DisjointSet ds = new DisjointSet(V);
+
+        Collections.sort(edges);
+        int mstWt = 0;
+        for (int i = 0; i < edges.size(); i++) {
+            int wt = edges.get(i).weight;
+            int u = edges.get(i).src;
+            int v = edges.get(i).dest;
+
+            if (ds.findUParent(u) != ds.findUParent(v)) {
+                mstWt += wt;
+                ds.UnionBySize(u, v);
+            }
+        }
+
+        return mstWt;
     }
 }
