7bbc88e328803b01a1fd2870725e4170b0da9ef8
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-01-26 16:57:45 -0600

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Maximum Connected group/maximum-connected-group.java
+++ Difficulty: Hard/Maximum Connected group/maximum-connected-group.java
@@ -110,14 +110,16 @@ class Solution
                 }
             }
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    int parent = ds.findUparent(i * n + j);
-                    max = Math.max(max, ds.size.get(parent)); // Update `max` with the largest connected component size
-                }
-            }
-        }
+        // for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (grid[i][j] == 1) {
+        //             int parent = ds.findUparent(i * n + j);
+        //             max = Math.max(max, ds.size.get(parent)); // Update `max` with the largest connected component size
+        //         }
+        //     }
+        // }
+        for(int i=0;i<ds.size.size();i++)
+        max = Math.max(max,ds.size.get(i));
         
         int cnt=0;
         for(int i=0;i<m;i++)
