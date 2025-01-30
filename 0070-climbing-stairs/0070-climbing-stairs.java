8c08d415178a87bb12efa1549d9076d9bc2c9440
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-01-29 23:06:00 -0600

    Time: 0 ms (100%), Space: 40.1 MB (90.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,7 +5,7 @@ class Solution
        
         int prev2 = 0;
         int prev = 1;
-        for(int i=1;i<=n;i++)
+        for(int i=2;i<=n+1;i++)
         {
            int curr = prev+prev2;
             prev2 = prev;
