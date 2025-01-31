872e33d8dcd77540c86a21897782fa1286379f88
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-01-31 17:20:29 -0600

    Time: 0 ms (100%), Space: 40.8 MB (89.98%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -2,9 +2,9 @@ class Solution
 {
     public int solve (int nums[],int s, int e)
     {
-        int prev = 0;
+        int prev = nums[s];
         int prev2 =0;
-        for(int i=s;i<=e;i++)
+        for(int i=s+1;i<=e;i++)
         {
             int take = nums[i];
             if(i>1)
@@ -21,6 +21,7 @@ class Solution
     {
         int n = nums.length;
         if (n == 1) return nums[0];
+
         int ans1 = solve(nums,0,n-2);
         int ans2 = solve(nums,1,n-1);
 
