7095bd47e7a2a598de865c4f3f02b9d1d1731fc6
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-02-03 23:23:20 -0600

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -35,94 +35,38 @@ public class Main {
 }
 // } Driver Code Ends
 
-
-// User function Template for Java
-class DisjointSet
-{
-    List<Integer> parent = new ArrayList<>();
-    List<Integer> size = new ArrayList<>();
-    public DisjointSet(int n)
-    {
-        for(int i=0;i<n;i++)
-        {
-            parent.add(i);
-            size.add(1);
-        }
-    }
-    
-    public int findUParent(int node)
-{
-    if(node == parent.get(node))
-    {
-        return node;
-    }
-    // Perform path compression
-    parent.set(node, findUParent(parent.get(node)));
-    return parent.get(node);
-}
-
-    
-    public void UnionBySize(int u,int v)
-    {
-        int ulp_u = findUParent(u);
-        int ulp_v = findUParent(v);
-        if(size.get(ulp_u)<size.get(ulp_v))
-        {
-            parent.set(ulp_u,ulp_v);
-            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
-        }
-        else
-        {
-            parent.set(ulp_v,ulp_u);
-            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
-        }
-    }
-}
-// class Edge implements Comparable<Edge> {
-//     int src, dest, weight;
-//     Edge(int _src, int _dest, int _wt) {
-//         this.src = _src; this.dest = _dest; this.weight = _wt;
-//     }
-    
-//     public int compareTo(Edge compareEdge) {
-//         return this.weight - compareEdge.weight;
-//     }
-// }
 class Solution 
 {
     static int spanningTree(int V, int E, List<List<int[]>> adj) 
     {
-        DisjointSet ds = new DisjointSet(V);
-        int mwt=0;
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
-        for(int i=0;i<adj.size();i++)
-        {
-            for (int j = 0; j < adj.get(i).size(); j++)
-            {
-                int u = i;
-                int v = adj.get(i).get(j)[0];
-                int wt = adj.get(i).get(j)[1];
-                
-                pq.offer(new int[]{wt,u,v});
-            }
-        }
+        int visited[] = new int[V];
+        int mwt=0;
+            
+        pq.offer(new int[]{0,0});
         
         while(!pq.isEmpty())
         {
             int pops[] = pq.poll();
             int wt = pops[0];
             int u = pops[1];
-            int v = pops[2];
             
-            if(ds.findUParent(u)!=ds.findUParent(v))
+            if(visited[u]==0)
             {
-                ds.UnionBySize(u,v);
+                visited[u]=1;
                 mwt+=wt;
+                for(int it[]:adj.get(u))
+                {
+                    int w = it[0];
+                    int neigh = it[1];
+                    
+                    if(visited[neigh]==0)
+                    {
+                        pq.offer(new int[]{w,neigh});
+                    }
+                }
             }
-            // else
-            // {
-            //     continue;
-            // }
+            
         }
         return mwt;
     }
