d2e6e6bf9b16891c1e32f956ad305f29598e83ec
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-02-05 21:52:41 -0600

    Time: 35 ms (27.14%), Space: 46.3 MB (29.02%) - LeetHub

---
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
@@ -0,0 +1,56 @@
+class Solution 
+{
+    public int cherryPickup(int[][] grid) 
+    {
+        int m = grid.length, n=grid[0].length;
+        int dp[][][] = new int[m][n][n];
+        for(int j1=0;j1<n;j1++)
+        {
+            for(int j2=0;j2<n;j2++)
+            {
+                if(j1==j2)
+                {
+                    dp[m-1][j1][j2] = grid[m-1][j1];
+                }
+                else
+                {
+                    dp[m-1][j1][j2] = grid[m-1][j1]+grid[m-1][j2];
+                }
+            }
+        }
+
+        for(int i=m-2;i>=0;i--)
+        {
+            for(int j1=0;j1<n;j1++)
+            {
+                for(int j2=0;j2<n;j2++)
+                {
+                    int maxi=Integer.MIN_VALUE;
+                    for(int dj1=-1;dj1<=1;dj1++)
+                    {
+                        for(int dj2=-1;dj2<=1;dj2++)
+                        {
+                            int ans=0;
+                            if(j1==j2)
+                            {
+                                ans=grid[i][j1];
+                            }
+                            else
+                            {
+                                ans=grid[i][j1]+grid[i][j2];
+                            }
+                            if((j1+dj1<0 || j1+dj1>=n)||(j2+dj2<0 || j2+dj2>=n))
+                                ans = -1000000000;
+                            else
+                                ans+=dp[i+1][j1+dj1][j2+dj2];
+
+                            maxi=Math.max(maxi,ans);
+                        }
+                    }
+                    dp[i][j1][j2]=maxi;
+                }
+            }
+        }
+        return dp[0][0][n-1];
+    }
+}
