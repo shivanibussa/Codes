3c8ae7eaeb52cb4ce2d4e6e380b6fc45b667ba3b
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-02-22 22:45:25 -0600

    Time: 0 ms (100%), Space: 42.3 MB (12.12%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -21,12 +21,13 @@ class Solution
             return true;
         return f(root.left,root.right);
     }
-    public boolean f(TreeNode p, TreeNode q)
+    public boolean f(TreeNode p,TreeNode q)
     {
         if(p==null || q==null)
             return p==q;
         if(p.val!=q.val)
             return false;
-        return f(p.left,q.right) && f(p.right,q.left);
+        return f(p.left,q.right)&& f(q.left,p.right);
     }
+
 }
