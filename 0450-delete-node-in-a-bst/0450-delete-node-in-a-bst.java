d893052953583dbda49926d519e156a366ddc7f4
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-02-23 19:26:59 -0600

    Time: 0 ms (100%), Space: 46.3 MB (32.71%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -17,61 +17,50 @@ class Solution
 {
     public TreeNode deleteNode(TreeNode root, int key) 
     {
-        
-        if(root == null)
-            return root;
+        TreeNode curr = root;
+        if(root==null)
+            return null;
         if(root.val==key)
             return helper(root);
-        TreeNode dummy = root;
         while(root!=null)
         {
             if(root.val>key)
             {
-                if(root.left!=null && root.left.val ==key)
-                {
+                if(root.left!=null && root.left.val == key)
                     root.left = helper(root.left);
-                    break;
-                }
                 else
-                {
                     root = root.left;
-                }
             }
             else
             {
-                if(root.right!=null && root.right.val==key)
-                {
+                if(root.right!=null && root.right.val == key)
                     root.right = helper(root.right);
-                    break;
-                }
                 else
-                {
                     root = root.right;
-                }
+            }
+            
+        }
+        return curr;
+    }
+        public  TreeNode helper(TreeNode node)
+        {
+            if(node.left==null)
+                return node.right;
+            else if(node.right==null)
+                return node.left;
+            else
+            {
+                TreeNode rightChild = node.right;
+                TreeNode lastChild = findLast(node.left);
+                lastChild.right = rightChild;
+                return node.left;
             }
         }
-        return dummy;
-    }
-    public TreeNode helper(TreeNode root)
-    {
-        if(root.left==null)
-            return root.right;
-        else if(root.right==null)
-            return root.left;
-        else
+        public TreeNode findLast(TreeNode root)
         {
-            TreeNode rightChild = root.right;
-            TreeNode lastRight = findLast(root.left);
-            lastRight.right = rightChild;
-            return root.left;
+            while(root.right!=null)
+                root= root.right;
+            return root;
         }
-    }
-    public TreeNode findLast(TreeNode root)
-    {
-        while(root.right!=null)
-        {
-            root= root.right;
-        }
-        return root;
-    }
+    
 }
