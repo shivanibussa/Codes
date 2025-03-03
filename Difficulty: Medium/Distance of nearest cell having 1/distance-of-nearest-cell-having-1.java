9cf53713e078fb5dfb9bc0e9534b1c573119a887
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-03-02 21:49:48 -0600

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.java
+++ Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.java
@@ -41,48 +41,44 @@ class Solution
     
     public int[][] nearest(int[][] grid)
     {
-        int n = grid[0].length,m=grid.length;
-        int visited[][] = new int[m][n];
+        int m = grid.length,n=grid[0].length;
+        boolean visited[][] = new boolean[m][n];
         int dist[][] = new int[m][n];
         Queue<int[]> q = new LinkedList<>();
-        
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==1)
                 {
-                    q.offer(new int[]{i,j,0});
+                    q.add(new int[]{i,j,0});
                 }
             }
         }
-        
         while(!q.isEmpty())
         {
             int pops[] = q.poll();
-            int row = pops[0];
-            int col = pops[1];
-            int distance = pops[2];
+            int u = pops[0];
+            int v = pops[1];
+            int d = pops[2];
             
             int dr[] = {-1,0,1,0};
             int dc[] = {0,-1,0,1};
             
             for(int i=0;i<4;i++)
             {
-                int nr = dr[i]+row;
-                int nc = dc[i]+col;
+                int nr = u+dr[i];
+                int nc = v+dc[i];
                 
-                if(nr>=0 && nc>=0 && nr<m && nc<n 
-                && visited[nr][nc]==0 && grid[nr][nc]==0)
+                if(nr>=0 && nr<m && nc>=0 && nc<n && dist[nr][nc]==0
+                && grid[nr][nc]==0 && !visited[nr][nc])
                 {
-                    q.offer(new int[]{nr,nc,distance+1});
-                    visited[nr][nc]=1;
-                    dist[nr][nc] = distance+1;
+                    visited[nr][nc] = true;
+                    dist[nr][nc] = d+1;
+                    q.add(new int[]{nr,nc,d+1});
                 }
             }
         }
         return dist;
-        
     }
-    
 }
