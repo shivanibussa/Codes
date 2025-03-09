991b781aa11bfc3f5db0463dfa388fce67499d3a
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-03-08 20:05:53 -0600

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.java
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.java
@@ -33,48 +33,40 @@ class GFG {
 // } Driver Code Ends
 
 
-// User function template for JAVA
+
 
 class Solution 
 {
     public void shortestDistance(int[][] mat) 
     {
-        for(int i=0;i<mat.length;i++)
-        {
-            for(int j=0;j<mat[0].length;j++)
-            {
-                if(mat[i][j]==-1)
-                {
-                    mat[i][j] = (int)(1e9);
-                }
+        int m = mat.length;
+       for(int i=0;i<m;i++)
+       {
+           for(int j=0;j<m;j++)
+           {
+               if(mat[i][j]==-1)
+                mat[i][j] = (int)1e8;
                 if(i==j)
-                {
-                    mat[i][j]=0;
-                }
-            }
-        }
-        
-        for(int k=0;k<mat.length;k++)
-        {
-            for(int i=0;i<mat.length;i++)
-            {
-                for(int j=0;j<mat[0].length;j++)
-                {
-                    mat[i][j] = Math.min(mat[i][j],mat[i][k]+mat[k][j]);
-                }
-            }
-        }
-        
-        
-        for(int i=0;i<mat.length;i++)
-        {
-            for(int j=0;j<mat[0].length;j++)
-            {
-                if(mat[i][j]==(int)(1e9))
-                {
-                    mat[i][j] = -1;
-                }
-            }
-        }
+                    mat[i][j] = 0;
+           }
+       }
+       for(int k=0;k<m;k++)
+       {
+           for(int i=0;i<m;i++)
+           {
+               for(int j=0;j<m;j++)
+               {
+                   mat[i][j] = Math.min(mat[i][j],(mat[i][k]+mat[k][j]));
+               }
+           }
+       }
+       for(int i=0;i<m;i++)
+       {
+           for(int j=0;j<m;j++)
+           {
+               if(mat[i][j]==(int)1e8)
+                mat[i][j] = -1;
+           }
+       }
     }
 }
