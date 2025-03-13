676e5ad9e655abde100d415447a34f1405c52876
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-03-13 18:00:42 -0500

    Time: 38 ms (22.66%), Space: 46.5 MB (25.49%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
@@ -4,50 +4,41 @@ class Solution
     {
         int m = grid.length, n=grid[0].length;
         int dp[][][] = new int[m][n][n];
-        for(int j1=0;j1<n;j1++)
+        for(int j=0;j<n;j++)
         {
             for(int j2=0;j2<n;j2++)
             {
-                if(j1==j2)
-                {
-                    dp[m-1][j1][j2] = grid[m-1][j1];
-                }
+                if(j==j2)
+                    dp[m-1][j][j2] = grid[m-1][j];
                 else
-                {
-                    dp[m-1][j1][j2] = grid[m-1][j1]+grid[m-1][j2];
-                }
+                    dp[m-1][j][j2] = grid[m-1][j]+grid[m-1][j2];
             }
         }
-
         for(int i=m-2;i>=0;i--)
         {
             for(int j1=0;j1<n;j1++)
             {
                 for(int j2=0;j2<n;j2++)
                 {
-                    int maxi=Integer.MIN_VALUE;
-                    for(int dj1=-1;dj1<=1;dj1++)
+                    int maxi = Integer.MIN_VALUE;
+                    for(int dj1 =-1;dj1<=1;dj1++)
                     {
                         for(int dj2=-1;dj2<=1;dj2++)
                         {
-                            int ans=0;
+                            int value=0;
                             if(j1==j2)
-                            {
-                                ans=grid[i][j1];
-                            }
+                                value+=grid[i][j1];
                             else
-                            {
-                                ans=grid[i][j1]+grid[i][j2];
-                            }
-                            if((j1+dj1<0 || j1+dj1>=n)||(j2+dj2<0 || j2+dj2>=n))
-                                ans = -1000000000;
+                                value=grid[i][j1]+grid[i][j2];
+                            if(j1+dj1>=0 && j2+dj2>=0 && j1+dj1<n && j2+dj2<n)
+                                value+=dp[i+1][j1+dj1][j2+dj2];
                             else
-                                ans+=dp[i+1][j1+dj1][j2+dj2];
+                                value = Integer.MIN_VALUE;
 
-                            maxi=Math.max(maxi,ans);
+                            maxi = Math.max(maxi,value);
                         }
                     }
-                    dp[i][j1][j2]=maxi;
+                    dp[i][j1][j2] = maxi;
                 }
             }
         }
