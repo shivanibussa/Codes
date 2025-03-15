c73f4339b347d91061ff62da597a90635c3a1629
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-03-14 19:48:56 -0500

    Time: 14 ms (85.43%), Space: 44.8 MB (41.18%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -3,26 +3,28 @@ class Solution
     public int coinChange(int[] coins, int amount) 
     {
         int n = coins.length;
-        int dp[][] = new int[n][amount+1];
+        int dp[] = new int[amount+1];
         for(int T=0;T<=amount;T++)
         {
             if(T%coins[0]==0)
-                dp[0][T] = (T/coins[0]);
+                dp[T] = (T/coins[0]);
             else 
-                dp[0][T] = (int)1e+8;
+                dp[T] = (int)1e+8;
         }
         for(int i=1;i<n;i++)
         {
+            int temp[] = new int[amount+1];
             for(int j=0;j<=amount;j++)
             {
-                int not_take = dp[i-1][j];
+                int not_take = dp[j];
                 int take =(int)1e+8;
                 if(j>=coins[i])
-                    take = 1+dp[i][j-coins[i]];
+                    take = 1+temp[j-coins[i]];
 
-                dp[i][j] = Math.min(take,not_take);
+                temp[j] = Math.min(take,not_take);
             }
+            dp=temp;
         }
-        return dp[n-1][amount]!=(int)1e+8?dp[n-1][amount]:-1;
+        return dp[amount]!=(int)1e+8?dp[amount]:-1;
     }
 }
