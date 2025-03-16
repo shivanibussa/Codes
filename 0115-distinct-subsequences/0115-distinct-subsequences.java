af5b5967b50f6ccda1899e2abc85f18bf61d61a0
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-03-16 09:17:01 -0500

    Time: 15 ms (89.45%), Space: 41.6 MB (95.49%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -8,20 +8,20 @@ class Solution
             dp[0]=1;
         for(int i=1;i<=m;i++)
         {
-            int temp[] = new int[n+1];
-            temp[0]=1;
-            for(int j=1;j<=n;j++)
+            // int temp[] = new int[n+1];
+            // temp[0]=1;
+            for(int j=n;j>0;j--)
             {
                 if(s.charAt(i-1)==t.charAt(j-1))
                 {
-                    temp[j] = dp[j-1]+dp[j];
+                    dp[j] = dp[j-1]+dp[j];
                 }
                 else
                 {
-                    temp[j] = dp[j];
+                    dp[j] = dp[j];
                 }
             }
-            dp=temp;
+            // dp=temp;
         }
         return dp[n];
     }
