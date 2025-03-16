12b47d6d2153e4b71babe585d76d99486d93b10e
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-03-16 10:45:37 -0500

    Time: 4 ms (75.22%), Space: 44.6 MB (96.2%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -3,28 +3,29 @@ class Solution
     public int minDistance(String s1, String s2) 
     {
         int m =s1.length(),n=s2.length();
-        int dp[][] = new int[m+1][n+1];
-        for(int i=0;i<=m;i++)
-            dp[i][0] = i;
+        int dp[] = new int[n+1];
         for(int j=0;j<=n;j++)
-            dp[0][j] = j;
+            dp[j] = j;
         for(int i=1;i<=m;i++)
         {
+            int temp[] = new int[n+1];
+            temp[0] = i;
             for(int j=1;j<=n;j++)
             {
                 if(s1.charAt(i-1)==s2.charAt(j-1))
                 {
-                    dp[i][j] = dp[i-1][j-1];
+                    temp[j] = dp[j-1];
                 }
                 else
                 {
-                    int insertions = dp[i][j-1];
-                    int deletion = dp[i-1][j];
-                    int replace = dp[i-1][j-1];
-                    dp[i][j] = 1+Math.min(Math.min(insertions,deletion),replace);
+                    int insertions = temp[j-1];
+                    int deletion = dp[j];
+                    int replace = dp[j-1];
+                    temp[j] = 1+Math.min(Math.min(insertions,deletion),replace);
                 }
             }
+            dp=temp;
         }
-        return dp[m][n];
+        return dp[n];
     }
 }
