9befb8dc3e596c45e752d3ced480b00d67924dbc
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-03-16 18:15:42 -0500

    Time: 23 ms (41.56%), Space: 44.3 MB (84.62%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,32 +1,44 @@
 class Solution 
 {
-    public boolean isMatch(String s, String p) 
+    public boolean isMatch(String text, String pattern) 
     {
-        int m = s.length(),n=p.length();
-        boolean dp[][] = new boolean[m+1][n+1]; 
-        dp[0][0] = true; // Both strings are empty
+        int m = pattern.length(), n=text.length();
+		boolean dp[][] = new boolean[m+1][n+1];
+		dp[0][0] = true;
+		for(int j=1;j<=n;j++)
+			dp[0][j] = false;
 
-        // Initialize dp[0][j] for pattern p
-        for (int j = 1; j <= n; j++) {
-            dp[0][j] = p.charAt(j - 1) == '*' && dp[0][j - 1];
-        }
-
-        // Initialize dp[i][0] for string s
-        for (int i = 1; i <= m; i++) {
-            dp[i][0] = false; // Non-empty string can't match empty pattern
-        }
-        for(int i=1;i<=m;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-                if(p.charAt(j-1)=='?' || s.charAt(i-1)==p.charAt(j-1))
-                    dp[i][j] = dp[i-1][j-1];
-                else if(p.charAt(j-1)=='*')
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
-                else
-                    dp[i][j] = false;
-            }
-        }
-        return dp[m][n];
+		for(int i=1;i<=m;i++)
+		{
+			boolean flag = true;
+			for(int ii=1;ii<=i;ii++)
+			{
+				if(pattern.charAt(ii-1)!='*')
+				{
+					flag = false;
+					break;
+				}
+			}
+			dp[i][0] = flag;
+		}
+		for(int i=1;i<=m;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				if(pattern.charAt(i-1)==text.charAt(j-1) || pattern.charAt(i-1)=='?')
+				{
+					dp[i][j] = dp[i-1][j-1];
+				}
+				else if(pattern.charAt(i-1)=='*')
+				{
+					dp[i][j] = dp[i][j-1] | dp[i-1][j];
+				}
+				else
+				{
+					dp[i][j] = false;
+				}
+			}
+		}
+		return dp[m][n];
     }
 }
