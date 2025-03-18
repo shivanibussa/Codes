42570c8fb1ef948d2dfcfdf3b3d2732fcacfeeec
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-03-18 17:22:37 -0500

    Time: 31 ms (98.03%), Space: 42.8 MB (41.31%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -8,28 +8,22 @@ class Solution
         int nc = newNums.length;
         newNums[0]=1;
         newNums[nc-1]=1;
-        int dp[][] = new int[n+1][n+1];
-        for(int i=0;i<=n;i++)
+        int dp[][] = new int[n+2][n+2];
+        for(int i=n;i>=1;i--)
         {
-            for(int j=0;j<=n;j++)
+            for(int j=1;j<=n;j++)
             {
-                dp[i][j]=-1;
+                if(i>j)
+                    continue;
+                int maxP = Integer.MIN_VALUE;
+                for(int k=i;k<=j;k++)
+                {
+                    int points = newNums[i-1]*newNums[k]*newNums[j+1] + dp[i][k-1]+dp[k+1][j];
+                    maxP = Math.max(maxP,points);
+                }
+                dp[i][j] = maxP;
             }
         }
-        return f(1,n,newNums,dp);
-    }
-    int f (int i,int j,int []nums, int [][]dp)
-    {
-        if(i>j)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int maxP = Integer.MIN_VALUE;
-        for(int k=i;k<=j;k++)
-        {
-            int points = nums[i-1]*nums[k]*nums[j+1] + f(i,k-1,nums,dp)+f(k+1,j,nums,dp);
-            maxP = Math.max(maxP,points);
-        }
-        return dp[i][j]=maxP;
+        return dp[1][n];
     }
 }
