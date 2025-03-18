762f0c20b6f21fcf2b57f327d57dc55b74cd33a0
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-03-17 23:37:41 -0500

    Time: 11 ms (90.19%), Space: 42.4 MB (72.4%) - LeetHub

---
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -0,0 +1,29 @@
+class Solution 
+{
+    public int minCost(int n, int[] cuts) 
+    {
+        int c = cuts.length;
+        int[] newCuts = new int[c + 2];
+        System.arraycopy(cuts, 0, newCuts, 1, c);
+        newCuts[0] = 0;
+        newCuts[c + 1] = n;
+        Arrays.sort(newCuts);
+        int dp[][] = new int[c+2][c+2];
+        for(int i=c;i>=1;i--)
+        {
+            for(int j=1;j<=c;j++)
+            {
+                if(i>j)
+                    continue;
+                int mini = Integer.MAX_VALUE;
+                for(int k=i;k<=j;k++)
+                {
+                    int cost = newCuts[j+1]-newCuts[i-1]+dp[i][k-1]+dp[k+1][j];
+                    mini = Math.min(mini,cost);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][c];
+    }
+}
