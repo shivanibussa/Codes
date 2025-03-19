22a807a654f44c5e675fc00b18778b7ae5e5de7a
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-03-18 19:57:46 -0500

    Time: 7 ms (56.98%), Space: 42.3 MB (49.09%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
@@ -4,26 +4,19 @@ class Solution
     {
         int n = arr.length;
         int dp[] = new int[n+1];
-        for(int i=0;i<=n;i++)
-            dp[i]=-1;
-        return f(0,arr,k,dp);
-    }
-    public int f(int i,int[] arr,int k,int[] dp)
-    {
-        int n = arr.length;
-        if(i>=n)
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        int maxSum=Integer.MIN_VALUE;
-        int len=0,maxi=Integer.MIN_VALUE;
-        for(int j=i;j<Math.min(i+k,n);j++)
+        for(int i=n-1;i>=0;i--)
         {
-            len++;
-            maxi = Math.max(maxi,arr[j]);
-            int sum = len*maxi+f(j+1,arr,k,dp);
-            maxSum = Math.max(maxSum,sum);
+            int maxSum=Integer.MIN_VALUE;
+            int len=0,maxi=Integer.MIN_VALUE;
+            for(int j=i;j<Math.min(i+k,n);j++)
+            {
+                len++;
+                maxi = Math.max(maxi,arr[j]);
+                int sum = len*maxi+dp[j+1];
+                maxSum = Math.max(maxSum,sum);
+            }
+            dp[i] = maxSum;
         }
-        return dp[i] = maxSum;
+        return dp[0];
     }
 }
