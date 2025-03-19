9d9a6b63809888baeb8f0bd779a7bfb6de7dcc31
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-03-19 17:19:16 -0500

    Time: 2 ms (30.26%), Space: 43.7 MB (30.67%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,30 +1,34 @@
-class DisjointSet
-{
-    ArrayList<Integer> parent = new ArrayList<>();
-    ArrayList<Integer> size = new ArrayList<>();
-
-    public DisjointSet(int n)
+class DisjointSet{
+    
+    ArrayList<Integer> size;
+    ArrayList<Integer> parent;
+    
+    DisjointSet(int n)
     {
+        size = new ArrayList<>();
+        parent = new ArrayList<>();
         for(int i=0;i<n;i++)
         {
-            parent.add(i);
             size.add(1);
+            parent.add(i);
         }
     }
-
-    public int findUParent(int node)
+    
+    public int findUparent(int node)
     {
         if(node==parent.get(node))
             return node;
-        parent.set(node,findUParent(parent.get(node)));
-        return parent.get(node);
+        int root = findUparent(parent.get(node));
+        parent.set(node,root);
+        return root;
     }
-
+    
     public void UnionBySize(int u,int v)
     {
-        int ulp_u = findUParent(u);
-        int ulp_v = findUParent(v);
-
+        int ulp_u = findUparent(u);
+        int ulp_v = findUparent(v);
+        if(ulp_u==ulp_v)
+            return;
         if(size.get(ulp_u)<size.get(ulp_v))
         {
             parent.set(ulp_u,ulp_v);
@@ -37,34 +41,34 @@ class DisjointSet
         }
     }
 }
-class Solution 
-{
+class Solution {
     public int[] findRedundantConnection(int[][] edges) 
     {
-        int n =0;
+        int n = edges.length;
         int res[] = new int[2];
+        DisjointSet ds = new DisjointSet(n+1);
+        int extras=0,cnt=0;
         for(int edge[]:edges)
         {
-            n = Math.max(n, Math.max(edge[0], edge[1]));
-        }
-        DisjointSet ds = new DisjointSet(n+1);
-
-        for(int i=0;i<edges.length;i++)
-        {
-            int u = edges[i][0];
-            int v = edges[i][1];
-
-            if(ds.findUParent(u)==ds.findUParent(v))
+            int a = edge[0];
+            int b = edge[1];
+            
+            if(ds.findUparent(a)==ds.findUparent(b))
             {
-                res[0]=u;
-                res[1]=v;
+                return edge;
             }
             else
             {
-                ds.UnionBySize(u,v);
+                ds.UnionBySize(a,b);
             }
-
         }
-        return res;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(i==ds.parent.get(i))
+                cnt++;
+        }
+        
+        return new int[0];
     }
 }
