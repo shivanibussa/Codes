112b3fb7b3ab87fcf7f5a4fd81a307c52c4a2ef2
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-03-18 19:24:43 -0500

    Time: 2524 ms (21.82%), Space: 41.3 MB (95.02%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -3,27 +3,20 @@ class Solution
     public int minCut(String s) 
     {
         int n = s.length();
-        int dp[] = new int[n+1];
-        for(int i=0;i<n;i++)
-            dp[i]=-1;
-        return f(0,s,dp)-1;
-    }
-    public int f(int i,String s,int[] dp)
-    {
-        int n = s.length();
-        if(dp[i]!=-1)
-            return dp[i];
-        if(i>=n)
-            return 0;
-        int minCuts = Integer.MAX_VALUE;
-        for(int j=i;j<n;j++)
+        int dp[] = new int[n+2];
+        for(int i=n-1;i>=0;i--)
         {
-            if(isPalindrome(i,j,s)==true){
-                int ways = 1+f(j+1,s,dp);
-                minCuts = Math.min(minCuts,ways);
+            int minCuts = Integer.MAX_VALUE;
+            for(int j=i;j<n;j++)
+            {
+                if(isPalindrome(i,j,s)==true){
+                    int ways = 1+dp[j+1];
+                    minCuts = Math.min(minCuts,ways);
+                }
             }
+            dp[i] = minCuts;
         }
-        return dp[i] = minCuts;
+        return dp[0]-1;
     }
     boolean isPalindrome(int f,int s,String str)
     {
