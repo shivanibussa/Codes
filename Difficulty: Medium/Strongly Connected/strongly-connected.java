2eb72aa3962cd27a1b564fa5a7d73de551944d8a
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-03-19 22:43:02 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Strongly Connected/strongly-connected.java
+++ Difficulty: Medium/Strongly Connected/strongly-connected.java
@@ -4,71 +4,69 @@ import java.util.*;
 
 
 // } Driver Code Ends
+
 // User function Template for Java
 
 class Solution 
 {
-    public void dfs(int node, int []visited,Stack<Integer> st,ArrayList<ArrayList<Integer>> adj)
+    public void dfs(int node,int visited[],ArrayList<ArrayList<Integer>> adj,Stack<Integer> st)
     {
         visited[node]=1;
-        
         for(int it:adj.get(node))
         {
             if(visited[it]==0)
-                dfs(it,visited,st,adj);
+            dfs(it,visited,adj,st);
         }
         st.push(node);
     }
-    public void dfs2(int node,int visited[],ArrayList<ArrayList<Integer>> adjT)
+    public void dfs3(int node,int visited[],ArrayList<ArrayList<Integer>> adjT)
     {
         visited[node]=1;
         for(int it:adjT.get(node))
         {
             if(visited[it]==0)
-            {
-                dfs2(it,visited,adjT);
-            }
+            dfs3(it,visited,adjT);
         }
     }
     public int kosaraju(ArrayList<ArrayList<Integer>> adj) 
     {
-        int visited[] = new int[adj.size()];
+        int V = adj.size();
         Stack<Integer> st = new Stack<>();
-        for(int i=0;i<adj.size();i++)
+        int visited[] = new int[V];
+        for(int i=0;i<V;i++)
         {
             if(visited[i]==0)
             {
-                dfs(i,visited,st,adj);
+                dfs(i,visited,adj,st);
             }
         }
-        
         ArrayList<ArrayList<Integer>> adjT = new ArrayList<>();
-        for(int i=0;i<adj.size();i++)
-        {
+        for(int i=0;i<V;i++)
             adjT.add(new ArrayList<>());
-        }
-        for(int i=0;i<adj.size();i++)
+        for(int i=0;i<V;i++)
         {
-             visited[i]=0;
+            visited[i]=0;
             for(int it:adj.get(i))
             {
-               
                 adjT.get(it).add(i);
             }
         }
-        int cnt=0;
+        
+        int scc=0;
         while(!st.isEmpty())
         {
-            int pop = st.pop();
-            if(visited[pop]==0)
+            int node = st.pop();
+            if(visited[node]==0)
             {
-                cnt++;
-                dfs2(pop,visited,adjT);
+                scc++;
+                dfs3(node,visited,adjT);
             }
         }
-        return cnt;
+        return scc;
     }
+    
 }
+
 
 //{ Driver Code Starts.
 
