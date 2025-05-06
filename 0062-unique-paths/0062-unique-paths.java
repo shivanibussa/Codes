8eca4ef799cc15aeb29d9b0e8d59349a6e6bd2a4
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-06 09:53:40 -0500

    Time: 0 ms (100%), Space: 40.7 MB (27.11%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -8,7 +8,9 @@ class Solution
             for(int j=0;j<n;j++)
             {
                 if(i==0 && j==0)
-                    dp[i][j]=1;
+                {
+                    dp[i][j] = 1;
+                }
                 else
                 {
                     int up=0,left=0;
@@ -16,10 +18,24 @@ class Solution
                         up = dp[i-1][j];
                     if(j>0)
                         left = dp[i][j-1];
+
                     dp[i][j] = up+left;
                 }
+
             }
         }
-        return dp[m-1][n-1];   
+        return dp[m-1][n-1];
+    }
+    public int f(int i,int j)
+    {
+        if(i==0 && j==0)
+            return 1;
+        int up=0,left=0;
+        if(i>0)
+            up = f(i-1,j);
+        if(j>0)
+            left = f(i,j-1);
+
+        return up+left;
     }
 }
