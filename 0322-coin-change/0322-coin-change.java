4b19573c83c32430c899f8f8029840a790aede30
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-06 18:49:18 -0500

    Time: 15 ms (77.75%), Space: 45.5 MB (18.29%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,30 +1,43 @@
-class Solution 
-{
+class Solution {
     public int coinChange(int[] coins, int amount) 
     {
         int n = coins.length;
-        int dp[] = new int[amount+1];
-        for(int T=0;T<=amount;T++)
+        int dp[][] = new int[n][amount+1];
+        for(int j=0;j<=amount;j++)
         {
-            if(T%coins[0]==0)
-                dp[T] = (T/coins[0]);
-            else 
-                dp[T] = (int)1e+8;
+            if(j%coins[0]==0)
+                dp[0][j] = j/coins[0];
+            else
+                dp[0][j] = (int)1e8;
         }
         for(int i=1;i<n;i++)
         {
-            int temp[] = new int[amount+1];
-            for(int j=0;j<=amount;j++)
+            for(int j = 0;j<=amount;j++)
             {
-                int not_take = dp[j];
-                int take =(int)1e+8;
+                int not_take = dp[i-1][j];
+                int take = (int)1e8;
                 if(j>=coins[i])
-                    take = 1+temp[j-coins[i]];
+                    take = 1+dp[i][j-coins[i]];
 
-                temp[j] = Math.min(take,not_take);
+                dp[i][j] = Math.min(take,not_take);
             }
-            dp=temp;
         }
-        return dp[amount]!=(int)1e+8?dp[amount]:-1;
+        return dp[n-1][amount] ==(int)1e8 ? -1: dp[n-1][amount];
+    }
+    int f(int coins[],int amount,int ind)
+    {
+        if(ind==0)
+        {
+            if(amount%coins[0]==0)
+                return amount/coins[0];
+            else
+                return (int)1e8;
+        }
+        int not_take = f(coins,amount,ind-1);
+        int take = Integer.MAX_VALUE;
+        if(amount>=coins[ind])
+            take = 1+f(coins,amount-coins[ind],ind);
+
+        return Math.min(take,not_take);
     }
 }
