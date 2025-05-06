a4061fd7d1ecfe4624a75a0fdd7a803554904b17
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-06 12:08:25 -0500

    Time: 7 ms (58.74%), Space: 45.6 MB (40.82%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,32 +1,31 @@
 class Solution {
-    int[][] dp;
-    public int solve(int i, int j, int[][] grid){
-        
-        if(i<0 || j<0 || j>=grid.length || i>=grid.length) return (int)1e9;
-        if(i==0) return grid[i][j];
-
-        if(dp[i][j]!=Integer.MAX_VALUE) return dp[i][j];
-
-        int up = solve(i-1, j, grid);
-        int dLeft = solve(i-1, j-1, grid);
-        int dRight = solve(i-1, j+1, grid);
-
-        return dp[i][j] = Math.min(up, Math.min(dLeft, dRight)) + grid[i][j];
-    }
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
-        int ans = Integer.MAX_VALUE;
-        dp = new int[n+1][n+1];
+        int[][] dp = new int[n][n];
 
-        for(int[] d: dp){
-            Arrays.fill(d, Integer.MAX_VALUE);
+        // Initialize the first row of dp with the first row of the matrix
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = matrix[0][j];
         }
 
-        // In question it is stated min of all paths 
-        // not like usual just a min path !!!! was getting wrong bcoz of this
-        for(int i=0; i<n; i++){
-            ans = Math.min(ans, solve(n-1, i, matrix));
+        // Fill the DP table row by row
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // Minimum of the three possible paths from the previous row
+                int up = dp[i - 1][j];
+                int leftDiagonal = (j > 0) ? dp[i - 1][j - 1] : Integer.MAX_VALUE;
+                int rightDiagonal = (j < n - 1) ? dp[i - 1][j + 1] : Integer.MAX_VALUE;
+
+                dp[i][j] = matrix[i][j] + Math.min(up, Math.min(leftDiagonal, rightDiagonal));
+            }
         }
-        return ans;
+
+        // Find the minimum in the last row of dp
+        int minSum = Integer.MAX_VALUE;
+        for (int j = 0; j < n; j++) {
+            minSum = Math.min(minSum, dp[n - 1][j]);
+        }
+
+        return minSum;
     }
 }
