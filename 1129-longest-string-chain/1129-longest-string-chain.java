b437ff3116052de67925c0b675e8b56b812b62c2
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-07 13:09:02 -0500

    Time: 52 ms (42.98%), Space: 44.4 MB (79%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.java
+++ 1129-longest-string-chain/1129-longest-string-chain.java
@@ -6,7 +6,8 @@ class Solution
         int maxi=1;
         int n = words.length;
         int dp[] = new int[n];
-        for(int i=0;i<n;i++)
+        dp[0]=1;
+        for(int i=1;i<n;i++)
         {
             dp[i] = 1;
             for(int j=0;j<i;j++)
