1daf37a2cc0ee40741dfdba1f34c1417e6291252
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-06 19:17:19 -0500

    Time: 5 ms (86.84%), Space: 43.5 MB (48.35%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,33 +1,32 @@
-class Solution {
-    public int findTargetSumWays(int[] arr, int d) {
-        int sum=0;
-        int n = arr.length;
-		for(int num:arr)
-			sum+=num;
-		if( (sum-d)<0 || (sum-d)%2==1)
+class Solution 
+{
+    public int findTargetSumWays(int[] arr, int target) 
+    {
+        int sum=0,n=arr.length;
+        for(int no:arr)
+            sum+=no;
+        int s = (sum-target)/2;
+        if( (sum-target)<0 || (sum-target)%2==1)
 			return 0;
-		int tar = (sum-d)/2;
-		int dp[][] = new int[n][tar+1];
-		for(int i=0;i<n;i++)
+        int dp[][] = new int[n][s+1];
+        for(int i=0;i<n;i++)
 			dp[i][0] = 1;
 		if (arr[0] == 0)
             dp[0][0] = 2;
-        else if (arr[0] <= tar)
+        else if (arr[0] <= s)
             dp[0][arr[0]] = 1;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=s;j++)
+            {
+                int not_take = dp[i-1][j];
+                int take = 0;
+                if(j>=arr[i])
+                    take = dp[i-1][j-arr[i]];
 
-		for(int i=1;i<n;i++)
-		{
-			for(int j=0;j<=tar;j++)
-			{
-				int take=0;
-				int not_take = dp[i-1][j];
-				if(arr[i]<=j)
-					take = dp[i-1][j-arr[i]];
-
-				dp[i][j] = (take+not_take);
-			}
-		}
-		return dp[n-1][tar];
-        
+                dp[i][j] = take+not_take;
+            }
+        }
+        return dp[n-1][s];
     }
 }
