9f3133eaf90fef8fbc1e4fb6949c1a13665be34f
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-07 13:05:52 -0500

    Time: 52 ms (42.98%), Space: 45 MB (41.69%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.java
+++ 1129-longest-string-chain/1129-longest-string-chain.java
@@ -2,33 +2,33 @@ class Solution
 {
     public int longestStrChain(String[] words) 
     {
-        int n = words.length,maxi=0;
+        Arrays.sort(words,(a,b)->a.length()-b.length());
+        int maxi=1;
+        int n = words.length;
         int dp[] = new int[n];
-        Arrays.sort(words, (a, b) -> a.length() - b.length());
-        for(int i=0;i<n;i++)
-            dp[i]=1;
         for(int i=0;i<n;i++)
         {
+            dp[i] = 1;
             for(int j=0;j<i;j++)
             {
-                if(compareWords(words[i],words[j]) && dp[j]+1>dp[i])
+                if(compare(words[i],words[j]) && 1+dp[j]>dp[i])
                 {
                     dp[i] = 1+dp[j];
                 }
             }
             maxi = Math.max(maxi,dp[i]);
         }
-
         return maxi;
     }
-    public boolean compareWords(String s1, String s2)
+    public boolean compare(String s1, String s2) 
     {
-        if(s1.length()-s2.length()!=1)
+        if(s1.length()!=s2.length()+1)
             return false;
+
         int f=0,s=0;
         while(f<s1.length())
         {
-            if(s<s2.length() && s1.charAt(f)==s2.charAt(s))
+            if(s<s2.length()&& s1.charAt(f)==s2.charAt(s))
             {
                 f++;
                 s++;
