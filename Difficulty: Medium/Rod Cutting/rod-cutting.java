bb52b03f7a10a6c3f467300ee7b9519514a2f4ab
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-06 20:07:29 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.java
+++ Difficulty: Medium/Rod Cutting/rod-cutting.java
@@ -32,22 +32,37 @@ class Solution
     {
         int n = price.length;
         int dp[][] = new int[n][n+1];
-        for(int i=0;i<=n;i++)
+        for(int i=0;i<n+1;i++)
         {
             dp[0][i] = i*price[0];
         }
+        
         for(int i=1;i<n;i++)
         {
-            for(int j=0;j<=n;j++)
+            for(int j=0;j<n+1;j++)
             {
                 int not_take = dp[i-1][j];
                 int take = Integer.MIN_VALUE;
-                if(j>=i+1)
-                    take = dp[i][j-(i+1)]+price[i];
+                int rl = i+1;
+                if(j>=rl)
+                    take = price[i]+dp[i][j-rl];
                     
-                dp[i][j] = Math.max(take,not_take);
+                dp[i][j] = Math.max(not_take,take);
             }
         }
         return dp[n-1][n];
+    }
+    public int f(int price[],int n,int ind)
+    {
+        if(ind==0)
+        {
+            return n*price[0]; 
+        }
+        int not_take = 0+f(price,n,ind-1);
+        int take = Integer.MIN_VALUE;
+        if(n>=ind+1)
+            take = price[ind]+f(price,n-ind+1,ind);
+            
+        return Math.max(take,not_take);
     }
 }
