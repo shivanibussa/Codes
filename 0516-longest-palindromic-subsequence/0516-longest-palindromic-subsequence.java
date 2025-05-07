d7353969716cfe7f46badd7c2254a2dcbe4b856a
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-06 20:40:59 -0500

    Time: 58 ms (35.84%), Space: 54.5 MB (84.22%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -3,19 +3,21 @@ class Solution
     public int longestPalindromeSubseq(String s) 
     {
         StringBuilder sb = new StringBuilder(s);
-        String t = sb.reverse().toString();  
-        int n = t.length();
-        int dp[][] = new int[n+1][n+1];
-        for(int i=1;i<=n;i++)
+        sb = sb.reverse();
+        String text2 = sb.toString();
+        int m = s.length(), n=text2.length();
+        int dp[][] = new int[m+1][n+1];
+        for(int i=1;i<m+1;i++)
         {
-            for(int j=1;j<=n;j++)
+            for(int j=1;j<n+1;j++)
             {
-                if(s.charAt(i-1)==t.charAt(j-1))
+                if(s.charAt(i-1)==text2.charAt(j-1))
                     dp[i][j] = 1+dp[i-1][j-1];
+
                 else
                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        return dp[n][n];
+        return dp[m][n];
     }
 }
