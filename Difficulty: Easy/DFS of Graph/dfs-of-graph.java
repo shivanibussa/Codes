9e3d219e824e0ef0800935d8e02e29656d8396a1
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-08 10:46:28 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.java
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -1,31 +1,34 @@
 //{ Driver Code Starts
 // Initial Template for Java
 import java.io.*;
-import java.lang.*;
 import java.util.*;
 
 class GFG {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
-            for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.dfsOfGraph(adj);
-            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
-            System.out.println();
+        int tc = Integer.parseInt(br.readLine().trim());
 
+        while (tc-- > 0) {
+            int V = Integer.parseInt(br.readLine().trim());
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+            for (int i = 0; i < V; i++) {
+                String[] input = br.readLine().trim().split(" ");
+                ArrayList<Integer> node = new ArrayList<>();
+                for (String s : input) {
+                    if (!s.isEmpty()) {
+                        node.add(Integer.parseInt(s));
+                    }
+                }
+                adj.add(node);
+            }
+
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfs(adj);
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
             System.out.println("~");
         }
     }
@@ -36,24 +39,24 @@ class GFG {
 
 class Solution 
 {
-    public void dfs(int node,ArrayList<ArrayList<Integer>> adj,boolean visited[],ArrayList<Integer> res)
-    {
-        visited[node] = true;
-        res.add(node);
-        for(int neighbors:adj.get(node))
-        {
-            if(!visited[neighbors])
-            {
-                dfs(neighbors,adj,visited,res);
-            }
-        }
-    }
-    public ArrayList<Integer> dfsOfGraph(ArrayList<ArrayList<Integer>> adj) 
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) 
     {
         int V = adj.size();
-        boolean visited[] = new boolean[V+1];
+        boolean visited[] = new boolean[V];
         ArrayList<Integer> res = new ArrayList<>();
-        dfs(0,adj,visited,res);
+        dfs(adj,res,visited,0);
         return res;
+    }
+    public void dfs(ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> res,boolean visited[],int node)
+    {
+        res.add(node);
+        visited[node] = true;
+        for(int nv:adj.get(node))
+        {
+            if(!visited[nv])
+            {
+                dfs(adj,res,visited,nv);
+            }
+        }
     }
 }
