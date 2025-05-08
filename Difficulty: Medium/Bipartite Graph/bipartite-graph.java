1b292c02bc9e77f852a938c643d56249c9633a5d
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-08 13:57:33 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bipartite Graph/bipartite-graph.java
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.java
@@ -1,37 +1,26 @@
 //{ Driver Code Starts
 import java.io.*;
-import java.lang.*;
 import java.util.*;
 
 class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] S = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(S[0]);
-            S = br.readLine().trim().split(" ");
-            int E = Integer.parseInt(S[0]);
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) {
-                adj.add(new ArrayList<Integer>());
-            }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            int[][] edges = new int[E][2];
             for (int i = 0; i < E; i++) {
-                String[] s = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(s[0]);
-                int v = Integer.parseInt(s[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
+                edges[i][0] = sc.nextInt();
+                edges[i][1] = sc.nextInt();
             }
-            Solution obj = new Solution();
-            boolean ans = obj.isBipartite(adj);
-            if (ans)
-                System.out.println("true");
-            else
-                System.out.println("false");
 
+            Solution obj = new Solution();
+            boolean ans = obj.isBipartite(V, edges);
+            System.out.println(ans ? "true" : "false");
             System.out.println("~");
         }
+        sc.close();
     }
 }
 // } Driver Code Ends
@@ -39,39 +28,49 @@ class GFG {
 
 class Solution 
 {
-    public boolean isBipartite(ArrayList<ArrayList<Integer>> adj) 
+    public boolean isBipartite(int V, int[][] edges) 
     {
-        int V = adj.size();
+        List<List<Integer>> adj = new ArrayList<>();
         int color[] = new int[V];
-        for(int i=0;i<V;i++)
-            color[i] = -1;
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<Integer>());
+            color[i]=-1;
+        }
+        for(int pair[]:edges)
+        {
+            int x = pair[0];
+            int y = pair[1];
+            adj.get(x).add(y);
+            adj.get(y).add(x);
+        }
         for(int i=0;i<V;i++)
         {
             if(color[i]==-1)
             {
-                if(bfs(i,adj,color)== false)
+                if(bfs(i,adj,color)==false)
                     return false;
             }
         }
         return true;
     }
-    public boolean bfs(int node,ArrayList<ArrayList<Integer>> adj,int color[])
+    public boolean bfs(int node, List<List<Integer>> adj, int color[])
     {
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q= new LinkedList<>();
         q.add(node);
         color[node]=0;
         while(!q.isEmpty())
         {
             int pop = q.poll();
-            for(int neighbor:adj.get(pop))
+            
+            for(int it:adj.get(pop))
             {
-                if(color[neighbor]==-1)
+                if(color[it]==-1)
                 {
-                    color[neighbor] = 1-color[pop];
-                    q.add(neighbor);
+                    q.add(it);
+                    color[it] = 1-color[pop];
                 }
-                else if(color[pop]==color[neighbor])
-                {   
+                else if(color[it]==color[pop])
+                {
                     return false;
                 }
             }
