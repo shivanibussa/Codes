02d2cc62dfa86d941bd545519c4db7bc993fdd0d
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-08 17:35:49 -0500

    Time: 7 ms (49.05%), Space: 45.6 MB (42.04%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,52 +1,41 @@
-class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adjLst = new ArrayList<>();
-
-        for(int i = 0; i < numCourses; i++){
-            adjLst.add(new ArrayList<>());
-        }
-
-        //fill the adjList iterating over prereq
-        for(int[] prereqiste : prerequisites){
-            adjLst.get(prereqiste[1]).add(prereqiste[0]);
-        }
-
-        //array to track the visited nodes 
-        int[] visitState = new int[numCourses];
-
-        //perform dfs on the graph 
-        for(int course = 0; course < numCourses;course++){
-            if(visitState[course] == 0){
-                //mechanism to detect cycle - if it has cycle we can terminate the funciton 
-                if(hasCycle(course, adjLst, visitState)) return false;
-            }
-        }
-        return true;   
-    }
-
-        //helper function to detect cycle
-        private boolean hasCycle(int course, List<List<Integer>> adjList, int[] visitState){
+class Solution 
+{
+    public boolean canFinish(int V, int[][] edges) 
+    {
+        List<List<Integer>> adj = new ArrayList<>();
+        ArrayList<Integer> al = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        int indegree[] = new int[V];
+        
+        for(int i =0;i<V;i++)
+            adj.add(new ArrayList<>());
             
-                
-            //mark that course as visited 
-            visitState[course] = 1;
-            //explore the connected edges
-            for(int neighbor : adjList.get(course)){
-
-                //if this neighbor is already in visitState arr 
-                if(visitState[neighbor] == 1){
-                    return true;
-                }
-                //if we havent found the visited state 
-                if(visitState[neighbor] == 0){
-                    if(hasCycle(neighbor, adjList, visitState)){
-                        return true;
-                    }
+        for(int pair[]:edges)
+        {
+            int a = pair[0], b = pair[1];
+            adj.get(a).add(b);
+            indegree[b]++;
+        }
+        
+        for(int i=0;i<V;i++)
+        {
+            if(indegree[i]==0)
+                q.offer(i);
+        }
+        
+        while(!q.isEmpty())
+        {
+            int pop = q.poll();
+            al.add(pop);
+            for(int it:adj.get(pop))
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                {
+                    q.offer(it);
                 }
             }
-            //some sort of flag to set the curr node as complete 
-            visitState[course] = 2;
-            return false;
         }
- 
+        return al.size()==V?true:false;
+    }
 }
