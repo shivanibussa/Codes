e555e586a589be2d7b6e67b017fbd6b944ed8254
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-08 17:40:29 -0500

    Time: 5 ms (79.23%), Space: 46 MB (52.34%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,53 +1,44 @@
-class Solution 
-{
-    public int[] findOrder(int N, int[][] prerequisites) 
+class Solution {
+    public int[] findOrder(int V, int[][] edges) 
     {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int p = prerequisites.length;
+        List<List<Integer>> adj = new ArrayList<>();
+        int al[] = new int[V];
+        int c=0;
         Queue<Integer> q = new LinkedList<>();
-        int res[] = new int[N];
-        int ind=0;
-
-        for(int i=0;i<N;i++)
-        {
+        int indegree[] = new int[V];
+        
+        for(int i =0;i<V;i++)
             adj.add(new ArrayList<>());
-        }
-        for(int i=0;i<p;i++)
+            
+        for(int pair[]:edges)
         {
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
-            adj.get(v).add(u);
+            int a = pair[0], b = pair[1];
+            adj.get(b).add(a);
+            indegree[a]++;
         }
-        int inorder[] = new int[N];
-        for(int i=0;i<N;i++)
+        
+        for(int i=0;i<V;i++)
         {
-            for(int it:adj.get(i))
-            {
-                inorder[it]++;
-            }
-        }
-        for(int i=0;i<N;i++)
-        {
-            if(inorder[i]==0)
-            {
+            if(indegree[i]==0)
                 q.offer(i);
-            }
         }
+        
         while(!q.isEmpty())
         {
             int pop = q.poll();
-            res[ind++]=pop;
+            al[c++]=pop;
             for(int it:adj.get(pop))
             {
-                inorder[it]--;
-                if(inorder[it]==0)
+                indegree[it]--;
+                if(indegree[it]==0)
                 {
                     q.offer(it);
                 }
             }
         }
-        if(ind!=N)
+        if(c!=V)
             return new int[]{};
-        return res;
+        return al;
+        
     }
 }
