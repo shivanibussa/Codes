54685ac623d8241718fd01d9304a36a0a0a95a33
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-10 18:08:25 -0500

    Time: 90 ms (58.92%), Space: 132.9 MB (9.12%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
@@ -1,43 +1,50 @@
 class Solution 
 {
-    private int timer=1;
-    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections)
+    private int timer = 1;
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) 
     {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++)
-            adj.add(new ArrayList<>());
-
-        for(int i=0;i<connections.size();i++)
-        {
-            int u = connections.get(i).get(0);
-            int v = connections.get(i).get(1);
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-        }
-        int vis[] = new int[n];
+        boolean visited[] = new boolean[n];
         int tin[] = new int[n];
         int low[] = new int[n];
         List<List<Integer>> bridges = new ArrayList<>();
-        dfs(0,-1,vis,tin,low,adj,bridges);
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            adj.add(new ArrayList<>());
+        }
+
+        for(List<Integer> it:connections)
+        {
+            int u = it.get(0), v = it.get(1);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        dfs(0,-1,adj,visited,tin,low,bridges);
         return bridges;
+
     }
-    public void dfs(int node,int parent,int [] vis, int[] tin,int[] low,ArrayList<ArrayList<Integer>> adj,List<List<Integer>> bridges)
+    public void dfs(int node, int parent,List<List<Integer>> adj, boolean visited[],int tin[],int low[],List<List<Integer>> bridges)
     {
-        vis[node]=1;
-        tin[node]=timer;low[node]=timer;
+        visited[node] = true;
+        tin[node] = timer;
+        low[node] = timer;
         timer++;
 
         for(int it:adj.get(node))
         {
             if(it==parent)
                 continue;
-            if(vis[it]==0)
+            if(!visited[it])
             {
-                dfs(it,node,vis,tin,low,adj,bridges);
+                dfs(it,node,adj,visited,tin,low,bridges);
                 low[node] = Math.min(low[node],low[it]);
                 if(low[it]>tin[node])
-                    bridges.add(Arrays.asList(it,node));
-                    
+                {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(it);temp.add(node);
+                    bridges.add(temp);
+                }   
             }
             else
             {
