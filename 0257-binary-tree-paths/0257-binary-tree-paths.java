ece55ca7008c01591b263333b0b8c45973c9e1ef
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-20 12:42:58 -0500

    Time: 2 ms (80.53%), Space: 42.4 MB (84.45%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -17,41 +17,26 @@ class Solution
 {
     public List<String> binaryTreePaths(TreeNode root) 
     {
-        ArrayList<ArrayList<Integer>> resz = new ArrayList<>();
-        f(root,new ArrayList<Integer>(),resz);
-        //  System.out.println(resz);
-        return Stringify(resz);
+        List<String> ans = new ArrayList<String>();
+        List<String> path = new ArrayList<String>();
+        dfs(root,ans,path);
+        return ans;
     }
-    public static void f(TreeNode root,ArrayList<Integer> path, 
-    ArrayList<ArrayList<Integer>> res)
+
+    public void dfs(TreeNode root, List<String> ans, List<String> path )
     {
         if(root==null)
             return;
-        path.add(root.val);
-        if(root.left==null && root.right==null)
-            res.add(new ArrayList<>(path));
 
-        f(root.left,path,res);
-        f(root.right,path,res);
-        path.remove(path.size()-1);
-    }
-    public static List<String> Stringify(ArrayList<ArrayList<Integer>> resz)
-    {
-        
-        List <String>res = new ArrayList<>();
-        // System.out.println(resz);
-        for(int i=0;i<resz.size();i++)
+        path.add(Integer.toString(root.val));
+        if(root.left==null && root.right==null)
+            ans.add(String.join("->",path));
+
+        else
         {
-            String ans="";
-            for(int num:resz.get(i))
-            {
-                ans+=String.valueOf(num)+"->";
-            }
-            ans=ans.substring(0,ans.length()-2);
-            res.add(ans);
+            dfs(root.left,ans,path);
+            dfs(root.right,ans,path);
         }
-        // ans=ans.subString(0,ans.length()-1);
-        System.out.println(res);
-        return res;
+        path.remove(path.size()-1);
     }
 }
