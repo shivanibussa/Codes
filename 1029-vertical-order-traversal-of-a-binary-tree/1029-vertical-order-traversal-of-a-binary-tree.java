60cb3e11ed516391fc423f7645a07bbcaaa524fe
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-19 21:32:06 -0500

    Time: 4 ms (41.7%), Space: 42.6 MB (50.68%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -16,47 +16,50 @@
 class Pair
 {
     TreeNode node;
-    int row;
-    int col;
+    int row,col;
+
     Pair(TreeNode node,int row,int col)
     {
-        this.node=node;
-        this.row=row;
-        this.col=col;
+        this.node = node;
+        this.row = row;
+        this.col = col;
     }
 }
 class Solution 
 {
     public List<List<Integer>> verticalTraversal(TreeNode root) 
     {
-        TreeMap<Integer,TreeMap<Integer,ArrayList<Integer>>> tm = new TreeMap<>();
-        Queue<Pair>q = new LinkedList<>();
-        q.offer(new Pair(root,0,0));
         List<List<Integer>> res = new ArrayList<>();
+        TreeMap<Integer,TreeMap<Integer,ArrayList<Integer>>> hm = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(root,0,0));
+
         while(!q.isEmpty())
         {
             Pair pop = q.poll();
             TreeNode node = pop.node;
             int x = pop.row, y = pop.col;
-            tm.computeIfAbsent(x, k -> new TreeMap<>());
-            tm.get(x).computeIfAbsent(y,k->new ArrayList<>());
-            tm.get(x).get(y).add(node.val);
+
+            hm.computeIfAbsent(x,k-> new TreeMap<>());
+            hm.get(x).computeIfAbsent(y,k-> new ArrayList<>());
+            hm.get(x).get(y).add(node.val);
+
             if(node.left!=null)
             {
-                q.offer(new Pair(node.left,x-1,y+1));
+                q.add(new Pair(node.left,x-1,y+1));
             }
             if(node.right!=null)
             {
-                q.offer(new Pair(node.right,x+1,y+1));
+                q.add(new Pair(node.right,x+1,y+1));
             }
         }
-        for(TreeMap<Integer,ArrayList<Integer>> ys:tm.values())
+        for(TreeMap<Integer,ArrayList<Integer>> tm : hm.values())
         {
-            ArrayList<Integer>temp = new ArrayList<>();
-            for(ArrayList<Integer> vals:ys.values())
+            ArrayList<Integer> temp = new ArrayList<>();
+            for(ArrayList<Integer> es: tm.values())
             {
-                Collections.sort(vals);
-                temp.addAll(vals);
+                Collections.sort(es);
+                temp.addAll(es);            
             }
             res.add(temp);
         }
