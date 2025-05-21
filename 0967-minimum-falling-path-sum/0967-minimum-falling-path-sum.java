6c3fd4e2d839077308b7df129ef8bcb80f5c35b2
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-21 11:35:33 -0500

    Time: 8 ms (34.53%), Space: 45.4 MB (51.17%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,31 +1,48 @@
-class Solution {
-    public int minFallingPathSum(int[][] matrix) {
-        int n = matrix.length;
-        int[][] dp = new int[n][n];
+class Solution 
+{
+    public int minFallingPathSum(int[][] matrix) 
+    {
+        int m = matrix.length, n = matrix[0].length,min = Integer.MAX_VALUE;
+        int dp[][] = new int[m][n];
 
-        // Initialize the first row of dp with the first row of the matrix
-        for (int j = 0; j < n; j++) {
+        for(int j=0;j<n;j++)
+        {
             dp[0][j] = matrix[0][j];
         }
 
-        // Fill the DP table row by row
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                // Minimum of the three possible paths from the previous row
-                int up = dp[i - 1][j];
-                int leftDiagonal = (j > 0) ? dp[i - 1][j - 1] : Integer.MAX_VALUE;
-                int rightDiagonal = (j < n - 1) ? dp[i - 1][j + 1] : Integer.MAX_VALUE;
-
-                dp[i][j] = matrix[i][j] + Math.min(up, Math.min(leftDiagonal, rightDiagonal));
+        for(int i=1;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int up = dp[i-1][j],ld = Integer.MAX_VALUE,rd = Integer.MAX_VALUE;
+                if(j-1>=0)
+                    ld = dp[i-1][j-1];
+                if(j+1<=n-1)
+                    rd = dp[i-1][j+1];
+                dp[i][j] = matrix[i][j]+Math.min(up,Math.min(ld,rd));
             }
         }
-
-        // Find the minimum in the last row of dp
-        int minSum = Integer.MAX_VALUE;
-        for (int j = 0; j < n; j++) {
-            minSum = Math.min(minSum, dp[n - 1][j]);
+        for(int j=0;j<n;j++)
+        {
+            min = Math.min(min,dp[m-1][j]);
         }
+        return min;
+    }
 
-        return minSum;
+    public int f(int i,int j,int matrix[][])
+    {
+        int m = matrix.length, n = matrix[0].length;
+        if(i<0 || j<0 ||i>m-1 || j>n-1)
+            return Integer.MAX_VALUE;
+
+        if(i==0)
+            return matrix[i][j];
+
+        int up = f(i-1,j,matrix);
+        int ld = f(i-1,j-1,matrix);
+        int rd = f(i-1,j+1,matrix);
+
+        return matrix[i][j]+Math.min(up,Math.min(ld,rd));
+
     }
 }
