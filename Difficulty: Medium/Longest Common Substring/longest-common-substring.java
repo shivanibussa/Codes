42f72b6d508643dd746a2879c39f85c8616ec640
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-22 12:29:02 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.java
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.java
@@ -1,38 +1,14 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String S1 = read.readLine().trim();
-            String S2 = read.readLine().trim();
-
-            Solution ob = new Solution();
-            System.out.println(ob.longestCommonSubstr(S1, S2));
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution 
 {
     public int longestCommonSubstr(String s1, String s2) 
     {
-        int m = s1.length(), n= s2.length(),maxi=0;
+        int m = s1.length(), n= s2.length(),maxi=Integer.MIN_VALUE;
         int dp[][] = new int[m+1][n+1];
-        for(int i=1;i<m+1;i++)
+        for(int i=1;i<=m;i++)
         {
-            for(int j=1;j<n+1;j++)
+            for(int j=1;j<=n;j++)
             {
                 if(s1.charAt(i-1)==s2.charAt(j-1))
                 {
@@ -42,12 +18,6 @@ class Solution
                 {
                     dp[i][j] = 0;
                 }
-            }
-        }
-        for(int i=0;i<m+1;i++)
-        {
-            for(int j=0;j<n+1;j++)
-            {
                 maxi = Math.max(maxi,dp[i][j]);
             }
         }
