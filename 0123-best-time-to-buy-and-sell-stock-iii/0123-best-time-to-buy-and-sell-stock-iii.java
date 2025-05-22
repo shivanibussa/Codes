b958e702fbb25edfa181568b6da3321b2f6a563f
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-22 17:58:56 -0500

    Time: 74 ms (29.48%), Space: 64.1 MB (33.91%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -3,43 +3,36 @@ class Solution
     public int maxProfit(int[] prices) 
     {
         int n = prices.length;
-        int dp[][] = new int [2][3];
+        int dp[][][] = new int[n+1][2][3];
         for(int i=n-1;i>=0;i--)
         {
-            int temp[][] = new int[2][3];
             for(int j=0;j<2;j++)
             {
                 for(int k=2;k>0;k--)
                 {
                     if(j==1)
-                        temp[j][k] = Math.max(-prices[i]+dp[0][k],0+dp[1][k]);
+                    {
+                        dp[i][j][k] = Math.max(-prices[i]+dp[i+1][0][k],0+dp[i+1][1][k]);
+                    }
                     else
-                        temp[j][k] = Math.max(prices[i]+dp[1][k-1],0+dp[0][k]);
+                    {
+                        dp[i][j][k] = Math.max(prices[i]+dp[i+1][1][k-1],dp[i+1][0][k]);
+                    }
                 }
             }
-            dp = temp;
         }
-        return dp[1][2];
+        return dp[0][1][2];
     }
-    public int f(int prices[],int i,int buy,int cap)
+    public int f(int prices[],int ind,int buy,int cap)
     {
-        if(i==prices.length)
+        if(ind==prices.length)
             return 0;
 
         if(cap==0)
             return 0;
 
-        int profit = 0;
         if(buy==1)
-        {
-            profit = Math.max(-prices[i]+f(prices,i+1,0,cap),0+f(prices,i+1,1,cap));
-        }
-        else
-        {
-            profit = Math.max(prices[i]+f(prices,i+1,1,cap-1),0+f(prices,i+1,0,cap));
-        }
-
-        return profit;
+            return Math.max(-prices[ind]+f(prices,ind+1,0,cap),f(prices,ind+1,1,cap));
+        return Math.max(prices[ind]+f(prices,ind+1,1,cap-1),f(prices,ind+1,0,cap));
     }
-    
 }
