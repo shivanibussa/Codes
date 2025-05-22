596c079151f05d7bb6b933325a9b245e3fc85424
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-21 22:48:38 -0500

    Time: 17 ms (48.79%), Space: 45.2 MB (25.53%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,42 +1,45 @@
-class Solution {
+class Solution 
+{
     public int coinChange(int[] coins, int amount) 
     {
         int n = coins.length;
-        int dp[] = new int[amount+1];
+        int dp[][] = new int[n][amount+1];
         for(int j=0;j<=amount;j++)
         {
             if(j%coins[0]==0)
-                dp[j] = j/coins[0];
+            {
+                dp[0][j] = j/coins[0];
+            }
             else
-                dp[j] = (int)1e8;
+            {
+                dp[0][j] = (int)1e9;
+            }
         }
         for(int i=1;i<n;i++)
         {
-            int temp[] = new int[amount+1];
-            for(int j = 0;j<=amount;j++)
+            for(int j=0;j<=amount;j++)
             {
-                int not_take = dp[j];
-                int take = (int)1e8;
-                if(j>=coins[i])
-                    take = 1+temp[j-coins[i]];
+                int take = (int)1e9;
+                int not_take = dp[i-1][j];
+                if(coins[i]<=j)
+                    take = 1+dp[i][j-coins[i]];
 
-                temp[j] = Math.min(take,not_take);
+                dp[i][j] = Math.min(take,not_take);
             }
-            dp = temp;
         }
-        return dp[amount] ==(int)1e8 ? -1: dp[amount];
+        return dp[n-1][amount]==(int)1e9 ? -1 : dp[n-1][amount];
     }
-    int f(int coins[],int amount,int ind)
+    public int f(int coins[],int amount,int ind)
     {
         if(ind==0)
         {
             if(amount%coins[0]==0)
                 return amount/coins[0];
             else
-                return (int)1e8;
+                return (int)1e9;
         }
-        int not_take = f(coins,amount,ind-1);
-        int take = Integer.MAX_VALUE;
+        int not_take = 0+f(coins,amount,ind-1);
+        int take = (int)1e9;
         if(amount>=coins[ind])
             take = 1+f(coins,amount-coins[ind],ind);
 
