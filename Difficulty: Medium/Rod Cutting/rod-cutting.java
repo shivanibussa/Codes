8660000d5295b81b3471150be8a1ec768501fd94
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-21 23:38:50 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.java
+++ Difficulty: Medium/Rod Cutting/rod-cutting.java
@@ -1,67 +1,40 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class RodCutting {
-
-    public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-
-        int t = Integer.parseInt(in.readLine().trim());
-        while (t-- > 0) {
-            String s[] = in.readLine().trim().split(" ");
-            int[] arr = new int[s.length];
-            for (int i = 0; i < s.length; i++) arr[i] = Integer.parseInt(s[i]);
-
-            Solution ob = new Solution();
-            out.println(ob.cutRod(arr));
-
-            out.println("~");
-        }
-        out.close();
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution 
 {
     public int cutRod(int[] price) 
     {
         int n = price.length;
         int dp[][] = new int[n][n+1];
-        for(int i=0;i<n+1;i++)
+        
+        for(int j=0;j<=n;j++)
         {
-            dp[0][i] = i*price[0];
+            dp[0][j] = j*price[0];
         }
         
         for(int i=1;i<n;i++)
         {
-            for(int j=0;j<n+1;j++)
+            for(int j=0;j<=n;j++)
             {
-                int not_take = dp[i-1][j];
                 int take = Integer.MIN_VALUE;
-                int rl = i+1;
-                if(j>=rl)
-                    take = price[i]+dp[i][j-rl];
+                int not_take = dp[i-1][j];
+                if(j>=(i+1))
+                    take = price[i]+dp[i][j-(i+1)];
                     
-                dp[i][j] = Math.max(not_take,take);
+                dp[i][j] = Math.max(take,not_take);
             }
         }
         return dp[n-1][n];
     }
-    public int f(int price[],int n,int ind)
+    public int f(int price[],int L,int ind)
     {
         if(ind==0)
         {
-            return n*price[0]; 
+            return L*price[0];
         }
-        int not_take = 0+f(price,n,ind-1);
+        
+        int not_take = f(price,L,ind-1);
         int take = Integer.MIN_VALUE;
-        if(n>=ind+1)
-            take = price[ind]+f(price,n-ind+1,ind);
+        if(L>=(ind+1))
+            take = price[ind]+f(price,L-(ind+1),ind);
             
         return Math.max(take,not_take);
     }
