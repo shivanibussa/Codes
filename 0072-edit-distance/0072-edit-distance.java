fd1b44d88fe50ca46bbeba769b20843c75b4cf44
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-22 14:16:16 -0500

    Time: 4 ms (75.01%), Space: 44.7 MB (92.59%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -3,31 +3,34 @@ class Solution
     public int minDistance(String s1, String s2) 
     {
         int m = s1.length(), n = s2.length();
-        int dp[][] = new int[m+1][n+1];
+        int dp[] = new int[n+1];
         for(int i=0;i<=m;i++)
         {
-            dp[i][0] = i;
+            dp[0] = i;
         }
         for(int j=0;j<=n;j++)
         {
-            dp[0][j] = j;
+            dp[j] = j;
         }
         for(int i=1;i<=m;i++)
         {
+            int temp [] = new int[n+1];
+            temp[0] = i;
             for(int j=1;j<=n;j++)
             {
                 if(s1.charAt(i-1)==s2.charAt(j-1))
-                    dp[i][j] = dp[i-1][j-1];
+                    temp[j] = dp[j-1];
                 else
                 {
-                    int insert = dp[i][j-1];
-                    int update = dp[i-1][j-1];
-                    int delete = dp[i-1][j];
-                    dp[i][j] = 1+Math.min(insert,Math.min(update,delete));
+                    int insert = temp[j-1];
+                    int update = dp[j-1];
+                    int delete = dp[j];
+                    temp[j] = 1+Math.min(insert,Math.min(update,delete));
                 }
             }
+            dp = temp;
         }
-        return dp[m][n];
+        return dp[n];
     }
     public int f(String s1,String s2,int i,int j)
     {
