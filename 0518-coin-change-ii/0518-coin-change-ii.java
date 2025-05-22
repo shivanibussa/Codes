6c54bb6c1bccd87ab75ff2d89696ee8c5704f7bc
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-21 23:01:17 -0500

    Time: 8 ms (71.09%), Space: 45.3 MB (66.27%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,27 +1,34 @@
-class Solution {
-    public int change(int amount, int[] coins) {
+class Solution 
+{
+    public int change(int amount, int[] coins) 
+    {
         int n = coins.length;
         int dp[] = new int[amount+1];
         for(int j=0;j<=amount;j++)
         {
             if(j%coins[0]==0)
+            {
                 dp[j] = 1;
+            }
             else
+            {
                 dp[j] = 0;
+            }
         }
         for(int i=1;i<n;i++)
         {
             int temp[] = new int[amount+1];
-            for(int j = 0;j<=amount;j++)
+            for(int j=0;j<=amount;j++)
             {
-                int not_take = dp[j];
                 int take = 0;
-                if(j>=coins[i])
+                int not_take = dp[j];
+                if(coins[i]<=j)
                     take = temp[j-coins[i]];
+
 
                 temp[j] = take+not_take;
             }
-            dp = temp;
+            dp=temp;
         }
         return dp[amount];
     }
