6b7857f68abba086b0618e47159d549e254d50a8
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-22 19:58:26 -0500

    Time: 74 ms (24.64%), Space: 45.2 MB (23.67%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -3,20 +3,22 @@ class Solution
     public int lengthOfLIS(int[] nums)
     {
         int n = nums.length;
-        int dp[][] = new int[n+1][n+1];
+        int dp[] = new int[n+1];
         for(int i=n-1;i>=0;i--)
         {
+            int temp[] = new int[n+1];
             for(int j=i-1;j>=-1;j--)
             {
                 int take = Integer.MIN_VALUE;
-                int not_take = dp[i+1][j+1];
+                int not_take = dp[j+1];
                 if(j==-1 || nums[i]>nums[j])
-                    take = 1+dp[i+1][i+1];
+                    take = 1+dp[i+1];
 
-                dp[i][j+1] = Math.max(take,not_take);
+                temp[j+1] = Math.max(take,not_take);
             }
+            dp = temp;
         }
-        return dp[0][0];
+        return dp[0];
     }
     public int f(int nums[],int ind,int prev)
     {
