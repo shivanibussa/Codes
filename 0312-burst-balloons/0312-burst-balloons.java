3926e9f6a534099b8997380728ef4f6d6e6b332a
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-23 08:23:23 -0500

    Time: 31 ms (96.69%), Space: 43.1 MB (15.63%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,42 +1,43 @@
 class Solution 
 {
-    public int maxCoins(int[] nums)
+    public int maxCoins(int[] nums) 
     {
         int n = nums.length;
-        int newnums[] = new int[n+2];
-        System.arraycopy(nums,0,newnums,1,n);
-        newnums[0] = 1;
-        newnums[n+1] = 1;
-        // return f(newnums,1,n);
+        int newN[] = new int[n+2];
+        System.arraycopy(nums,0,newN,1,n);
+        newN[0] = 1;
+        newN[n+1] = 1;
         int dp[][] = new int[n+2][n+2];
+
         for(int i=n;i>=1;i--)
         {
-            
-            for(int j=1;j<=n;j++)
+          
+            for(int j=0;j<=n;j++)
             {
                 if(i>j)
                     continue;
-                int max = Integer.MIN_VALUE;
+                int maxi = Integer.MIN_VALUE;
                 for(int k=i;k<=j;k++)
                 {
-                    int points = newnums[i-1]*newnums[k]*newnums[j+1]+dp[i][k-1]+dp[k+1][j];
-                    max = Math.max(max,points);
+                    int points = newN[i-1]*newN[k]*newN[j+1]+dp[i][k-1]+dp[k+1][j];
+                    maxi = Math.max(maxi,points);
                 }
-                dp[i][j] = max;
+                dp[i][j] = maxi;
             }
         }
         return dp[1][n];
     }
-    public int f(int newnums[],int i,int j)
+    public int f(int newN[],int i,int j)
     {
         if(i>j)
             return 0;
+        
         int maxi = Integer.MIN_VALUE;
-        for(int k = i;k<=j;k++)
+        for(int k=i;k<=j;k++)
         {
-            int points = newnums[i-1]*newnums[k]*newnums[j+1]+f(newnums,i,k-1)+f(newnums,k+1,j);
+            int points = newN[i-1]*newN[k]*newN[j+1]+f(newN,i,k-1)+f(newN,k+1,j);
             maxi = Math.max(maxi,points);
         }
-        return maxi;   
-        }
+        return maxi;
+    }
 }
