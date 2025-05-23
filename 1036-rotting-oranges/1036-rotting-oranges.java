d508a907ff2c58635b7bbed3ea0c097d4fdaa2fb
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-23 16:53:38 -0500

    Time: 2 ms (68.35%), Space: 42.4 MB (62.42%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -3,44 +3,49 @@ class Solution
     public int orangesRotting(int[][] grid) 
     {
         Queue<int[]> q = new LinkedList<>();
-        int m = grid.length, n = grid[0].length;
+        int fresh=0,rot=0;
+        int m = grid.length, n = grid[0].length, t =0;
         int visited[][] = new int[m][n];
-        int totT=0, fresh = 0, rot = 0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==2)
                 {
-                    q.offer(new int[]{i,j,0});
+                    q.add(new int[]{i,j,0});
                     visited[i][j] = 2;
                 }
-                if(grid[i][j]==1)
+                else if(grid[i][j]==1)
                 {
                     fresh++;
                 }
-
             }
         }
-        int dr[] = {-1,0,1,0};
-        int dc[] = {0,-1,0,1};
+
+        int dr[] = new int[]{-1,0,1,0};
+        int dc[] = new int[]{0,-1,0,1};
 
         while(!q.isEmpty())
         {
             int pop[] = q.poll();
-            int x = pop[0],y=pop[1],t=pop[2];
-            totT = totT<t ? t :totT;
+            int r = pop[0], c = pop[1], time = pop[2];
+
             for(int k=0;k<4;k++)
             {
-                int nr = x+dr[k], nc = y+dc[k];
+                int nr = r+dr[k];
+                int nc = c+dc[k];
+
                 if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1 && visited[nr][nc]!=2)
                 {
                     visited[nr][nc] = 2;
-                    q.offer(new int[]{nr,nc,t+1});
+                    q.add(new int[]{nr,nc,time+1});
+                    t = Math.max(t,time+1);
                     rot++;
                 }
             }
         }
-        return fresh==rot ? totT : -1;
+        if(fresh!=rot)
+            return -1;
+        return t;
     }
 }
