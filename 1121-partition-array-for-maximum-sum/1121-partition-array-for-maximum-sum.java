28b2c05866bdb23a110ab4599faab0027185a30f
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-23 09:15:29 -0500

    Time: 7 ms (55.01%), Space: 41.9 MB (96.76%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
@@ -4,32 +4,30 @@ class Solution
     {
         int n = arr.length;
         int dp[] = new int[n+1];
-        
         for(int i=n-1;i>=0;i--)
         {
-            int len=0,max = Integer.MIN_VALUE,maxP=0;
+            int len=0,maxV=Integer.MIN_VALUE,maxAns = Integer.MIN_VALUE;
             for(int j=i;j<Math.min(i+k,n);j++)
             {
                 len++;
-                max = Math.max(max,arr[j]);
-                int points = (max*len)+dp[j+1];
-                maxP = Math.max(maxP,points);
+                maxV = Math.max(maxV,arr[j]);
+                int sum = (len*maxV)+dp[j+1];
+                maxAns = Math.max(maxAns,sum);
             }
-            dp[i] = maxP;
+            dp[i] = maxAns;
         }
         return dp[0];
     }
     public int f(int arr[],int i,int k)
     {
         int n = arr.length;
-        int len=0,max = Integer.MIN_VALUE,maxP=0;
+        if(i==n)
+            return 0;
+        int len=0,maxV=Integer.MIN_VALUE,maxAns = Integer.MIN_VALUE;
         for(int j=i;j<Math.min(i+k,n);j++)
         {
-            len++;
-            max = Math.max(max,arr[j]);
-            int points = (max*len)+f(arr,j+1,k);
-            maxP = Math.max(maxP,points);
+            
         }
-        return maxP;
+        return maxAns;
     }
 }
