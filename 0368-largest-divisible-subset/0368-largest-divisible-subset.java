fbce96f4f2139c457d230ca3ffdeb8bdbf2b0d77
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-22 20:23:23 -0500

    Time: 14 ms (54.07%), Space: 43 MB (91.44%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,19 +1,22 @@
 class Solution 
 {
-    public List<Integer> largestDivisibleSubset(int[] nums) 
+    public List<Integer> largestDivisibleSubset(int[] arr) 
     {
-        ArrayList<Integer> res = new ArrayList<>();
-        int n = nums.length;
+        Arrays.sort(arr);
+        int n = arr.length,lastindex=0,maxi=1;
         int dp[] = new int[n];
         int hash[] = new int[n];
-        int lastIndex=0,maxi=1;
-        Arrays.sort(nums);
+        ArrayList<Integer> res = new ArrayList<>();
         for(int i=0;i<n;i++)
         {
-            dp[i]=1;hash[i] = i;
+            dp[i] = 1;
+            hash[i] = i;
+        }
+        for(int i=1;i<n;i++)
+        {
             for(int j=0;j<i;j++)
             {
-                if(nums[i]%nums[j]==0 && 1+dp[j]>dp[i])
+                if(arr[i]%arr[j]==0 && 1+dp[j]>dp[i])
                 {
                     dp[i] = 1+dp[j];
                     hash[i] = j;
@@ -22,15 +25,17 @@ class Solution
             if(maxi<dp[i])
             {
                 maxi = dp[i];
-                lastIndex = i;
+                lastindex = i;
             }
         }
-        res.add(nums[lastIndex]);
-        while(lastIndex!=hash[lastIndex])
+        res.add(arr[lastindex]);
+        while(lastindex!=hash[lastindex])
         {
-            lastIndex = hash[lastIndex];
-            res.add(nums[lastIndex]);
+            lastindex = hash[lastindex];
+            res.add(arr[lastindex]);
         }
+        Collections.reverse(res);
         return res;
     }
+    
 }
