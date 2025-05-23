c4ec2fe32d29a6642648824d3f6ef02fd7381580
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-22 19:38:43 -0500

    Time: 73 ms (25.17%), Space: 77.3 MB (17.68%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -2,21 +2,32 @@ class Solution
 {
     public int lengthOfLIS(int[] nums)
     {
-        int n = nums.length,maxi=1;
-        int dp[] = new int[n];
-        for(int i=0;i<n;i++)
-            dp[i]=1;
-        for(int i=1;i<n;i++)
+        int n = nums.length;
+        int dp[][] = new int[n+1][n+1];
+        for(int i=n-1;i>=0;i--)
         {
-            for(int j=0;j<i;j++)
+            for(int j=i-1;j>=-1;j--)
             {
-                if(nums[i]>nums[j])
-                {
-                    dp[i] = Math.max(dp[i],1+dp[j]);
-                }
-                maxi = Math.max(maxi,dp[i]);
+                int not_take = dp[i+1][j+1];
+                int take = Integer.MIN_VALUE;
+                if(j==-1 || nums[i]>nums[j])
+                        take = 1+dp[i+1][i+1];
+
+                dp[i][j+1] = Math.max(take,not_take);
             }
-        }       
-        return maxi;
+        }
+        return dp[0][0];
+    }
+    public int f(int nums[],int ind,int prev)
+    {
+        if(ind==nums.length)
+            return 0;
+
+        int not_take = f(nums,ind+1,prev);
+        int take = Integer.MIN_VALUE;
+        if(prev==-1 || nums[ind]>nums[prev])
+            take = 1+f(nums,ind+1,ind);
+
+        return Math.max(not_take,take);
     }
 }
