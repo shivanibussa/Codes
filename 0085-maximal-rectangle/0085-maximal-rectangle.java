3a5cd58030016a69ba1eced3c189b7e9b83142b0
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-22 23:21:28 -0500

    Time: 13 ms (83.59%), Space: 46.7 MB (47.85%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,43 +1,43 @@
 class Solution 
 {
-    public int maxArea(int arr[])
-    {
-        Stack<Integer> st = new Stack<>();
-        int maxArea = Integer.MIN_VALUE;
-        int n= arr.length;
-        for(int i=0;i<=n;i++)
-        {
-            while(!st.isEmpty() && (i==n||arr[st.peek()]>=arr[i]))
-            {
-                int height = arr[st.pop()];
-                int width = 0;
-                if(st.isEmpty())
-                    width=i;
-                else
-                    width = i-st.peek()-1;
-
-                maxArea = Math.max(maxArea,height*width);
-            }
-            st.push(i);
-        }
-        return maxArea;
-    }
     public int maximalRectangle(char[][] matrix) 
     {
-        int n = matrix[0].length;
+        int m=matrix.length,n = matrix[0].length,maxA=0;
         int arr[] = new int[n];
-        int maxArea = Integer.MIN_VALUE;
-        for(int i=0;i<matrix.length;i++)
+        for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(matrix[i][j]=='1')
+                {
                     arr[j]++;
+                }
                 else
+                {
                     arr[j]=0;
+                }
             }
-            maxArea = Math.max(maxArea,maxArea(arr));
+            maxA = Math.max(maxA,maxArea(arr));
         }
-        return maxArea;
+        return maxA;
     }
+    public int maxArea(int arr[])
+    {
+        int n = arr.length,maxA=0;
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<=n;i++)
+        {
+            while(!st.isEmpty() && (i==n|| arr[i]<=arr[st.peek()]))
+            {
+                int height = arr[st.pop()];
+                int width = st.isEmpty()==true? i : (i-st.peek()-1);
+                int area = height*width;
+                maxA = Math.max(area,maxA);
+            }
+            st.push(i);
+        }
+        return maxA;
+    }
+
+
 }
