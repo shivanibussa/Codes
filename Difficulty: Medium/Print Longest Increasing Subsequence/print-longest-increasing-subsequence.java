a092a20ac5d45e4f85205837db7cf13c1b263d85
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-22 20:19:12 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Print Longest Increasing Subsequence/print-longest-increasing-subsequence.java
+++ Difficulty: Medium/Print Longest Increasing Subsequence/print-longest-increasing-subsequence.java
@@ -1,39 +1,13 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-
-        while (tc-- > 0) {
-            String str[] = br.readLine().split(" ");
-            int arr[] = new int[str.length];
-
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            Solution obj = new Solution();
-
-            ArrayList<Integer> ans = obj.getLIS(arr);
-            for (int i = 0; i < ans.size(); i++) {
-                System.out.print(ans.get(i) + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
-class Solution {
-    public ArrayList<Integer> getLIS(int nums[]) {
-        // Code here
-        int n = nums.length,lastIndex=0;
-        int dp[] = new int[n],maxi=1;
+class Solution 
+{
+    public ArrayList<Integer> getLIS(int arr[]) 
+    {
+        int n = arr.length,lastindex=0,maxi=1;
+        int dp[] = new int[n];
         int hash[] = new int[n];
+        ArrayList<Integer> res = new ArrayList<>();
         for(int i=0;i<n;i++)
         {
             dp[i] = 1;
@@ -43,24 +17,23 @@ class Solution {
         {
             for(int j=0;j<i;j++)
             {
-                if(nums[i]>nums[j] && 1+dp[j]>dp[i])
+                if(arr[i]>arr[j] && 1+dp[j]>dp[i])
                 {
                     dp[i] = 1+dp[j];
                     hash[i] = j;
                 }
             }
-            if(dp[i]>maxi)
+            if(maxi<dp[i])
             {
                 maxi = dp[i];
-                lastIndex = i;
+                lastindex = i;
             }
         }
-        ArrayList<Integer> res = new ArrayList<>();
-        res.add(nums[lastIndex]);
-        while(lastIndex!=hash[lastIndex])
+        res.add(arr[lastindex]);
+        while(lastindex!=hash[lastindex])
         {
-            lastIndex = hash[lastIndex];
-            res.add(nums[lastIndex]);
+            lastindex = hash[lastindex];
+            res.add(arr[lastindex]);
         }
         Collections.reverse(res);
         return res;
