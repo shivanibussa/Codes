a7c1a3a660843fcf11c7b057357a8a9fcc89be1f
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-23 17:41:19 -0500

    Time: 13 ms (84.88%), Space: 46.9 MB (85.31%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -2,45 +2,38 @@ class Solution
 {
     public int[][] updateMatrix(int[][] mat) 
     {
-        int m = mat.length,n=mat[0].length;
-        Queue<int[]> q = new LinkedList<>();
+        int m = mat.length, n = mat[0].length;
         int visited[][] = new int[m][n];
         int dist[][] = new int[m][n];
-
+        Queue<int[]> q = new LinkedList<>();
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(mat[i][j]==0)
                 {
-                    q.offer(new int[]{i,j,0});
+                    q.add(new int[]{i,j,0});
+                    visited[i][j] = 1;
                 }
             }
         }
-
+        int dr[] = new int[]{-1,0,1,0};
+        int dc[] = new int[]{0,-1,0,1};
         while(!q.isEmpty())
         {
-            int pops[] = q.poll();
-            int r = pops[0];
-            int c = pops[1];
-            int d = pops[2];
-
-            int dr[] = {-1,0,1,0};
-            int dc[] = {0,-1,0,1};
-
-            for(int i=0;i<4;i++)
+            int pop[] = q.poll();
+            int r = pop[0], c = pop[1], d = pop[2];
+            for(int k=0;k<4;k++)
             {
-                int nr = r+dr[i];
-                int nc = c+dc[i];
-
-                if(nr>=0 && nc>=0 && nr<m && nc<n
-                && visited[nr][nc]==0 && mat[nr][nc]==1)
+                int nr = r+dr[k];
+                int nc = c+dc[k];
+                if(nr>=0 && nc>=0 && nr<m && nc<n && visited[nr][nc]==0 && mat[nr][nc]==1)
                 {
-                    q.offer(new int[]{nr,nc,d+1});
-                    visited[nr][nc]=1;
+                    visited[nr][nc] = 1;
                     dist[nr][nc] = d+1;
+                    q.add(new int[]{nr,nc,dist[nr][nc]});
                 }
-            }
+            } 
         }
         return dist;
     }
