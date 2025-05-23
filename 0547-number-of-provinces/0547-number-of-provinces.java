b7e01b4410034d5352c3cd9c1746a6fb96897777
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-23 16:27:43 -0500

    Time: 1 ms (88.38%), Space: 48.7 MB (5.19%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -2,35 +2,35 @@ class Solution
 {
     public int findCircleNum(int[][] adj) 
     {
-       int cnt=0;
-       int V = adj.length;
-       boolean visited[] = new boolean[V];
-       for(int i=0;i<V;i++)
-       {
-           if(!visited[i])
-           {
-               cnt++;
-               bfs(i,adj,visited);
-           }
-       }
-       return cnt;
+        int V = adj[0].length,cnt=0;
+        boolean visited[] = new boolean[V];  
+        for(int i=0;i<V;i++)
+        {
+            if(!visited[i])
+            {
+                cnt++;
+                bfs(adj,visited,i);
+            }
+        }
+        return cnt;
     }
-    public void bfs(int node,int[][] adj,boolean visited[])
+    public void bfs(int adj[][],boolean visited[],int node)
     {
+        visited[node]=true;
+        int V = visited.length;
         Queue<Integer> q = new LinkedList<>();
         q.add(node);
-        visited[node] = true;
         while(!q.isEmpty())
         {
-            int poll = q.poll();
-            for(int i=0;i<adj.length;i++)
+            int pop = q.poll();
+            for(int i=0;i<V;i++)
             {
-                if(adj[poll][i]==1 && visited[i]==false)
+                if(adj[pop][i]==1 && visited[i]==false)
                 {
+                    visited[i]=true;
                     q.add(i);
-                    visited[i] = true;
                 }
             }
-        } 
+        }
     }
 }
