647d0e7af55e403cdc8ca0a916ebd0bfe730ba53
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-23 23:11:11 -0500

    Time: 5 ms (79.01%), Space: 46.1 MB (41.36%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,45 +1,45 @@
 class Solution {
     public int[] findOrder(int V, int[][] edges) 
     {
-        List<List<Integer>> adj = new ArrayList<>();
-        int al[] = new int[V];
-        int c=0;
-        Queue<Integer> q = new LinkedList<>();
-        int indegree[] = new int[V];
-        
-        for(int i =0;i<V;i++)
+       int indegree[] = new int[V];
+       ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+       Queue<Integer> q = new LinkedList<>();
+       int res[] = new int[V];
+       int c=0;
+       for(int i=0;i<V;i++)
             adj.add(new ArrayList<>());
             
-        for(int pair[]:edges)
-        {
-            int a = pair[0], b = pair[1];
-            adj.get(b).add(a);
-            indegree[a]++;
-        }
-        
-        for(int i=0;i<V;i++)
-        {
-            if(indegree[i]==0)
-                q.offer(i);
-        }
-        
-        while(!q.isEmpty())
-        {
-            int pop = q.poll();
-            al[c++]=pop;
-            for(int it:adj.get(pop))
-            {
-                indegree[it]--;
-                if(indegree[it]==0)
-                {
-                    q.offer(it);
-                }
-            }
-        }
-        if(c!=V)
-            return new int[]{};
-        else
-            return al;
-        
+       for(int i=0;i<edges.length;i++)
+       {
+           int a = edges[i][0];
+           int b = edges[i][1];
+           
+           adj.get(b).add(a);
+           indegree[a]++;
+       }
+       for(int i=0;i<V;i++)
+       {
+           if(indegree[i]==0)
+           {
+               q.add(i);
+           }
+       }
+       while(!q.isEmpty())
+       {
+           int pop = q.poll();
+           res[c++] = pop;
+           for(int it:adj.get(pop))
+           {
+               indegree[it]--;
+               if(indegree[it]==0)
+               {
+                   q.add(it);
+               }
+           }
+       }
+       if(c!=V)
+        return new int[]{};
+       else
+        return res;
     }
 }
