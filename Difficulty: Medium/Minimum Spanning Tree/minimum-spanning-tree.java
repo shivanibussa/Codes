4c432d949d4ebfdb61a107bd92a9998d1f94d9cc
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-24 18:28:23 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -1,29 +1,73 @@
+class DisjointSet
+{
+    ArrayList<Integer> parent;
+    ArrayList<Integer> size;
+    DisjointSet(int V)
+    {
+        parent = new ArrayList<>();
+        size = new ArrayList<>();
+        for(int i=0;i<V;i++)
+        {
+            parent.add(i);
+            size.add(1);
+        }
+    }
+    
+    public int getUParent(int node)
+    {
+        if(node==parent.get(node))
+            return node;
+            
+        int ulp = getUParent(parent.get(node));
+        parent.set(node,ulp);
+        return parent.get(node);
+    }
+    
+    public void UnionBySize(int u,int v)
+    {
+        int ulp_u = getUParent(u);
+        int ulp_v = getUParent(v);
+        if(ulp_u==ulp_v)
+            return;
+        if(size.get(ulp_u)>size.get(ulp_v))
+        {
+            parent.set(ulp_v,ulp_u);
+            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
+        }
+        else
+        {
+            parent.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
+        }
+    }
+}
 class Solution 
 {
-    static int spanningTree(int V, int E, List<List<int[]>> adj) 
+    static int spanningTree(int V, int E, List<List<int[]>> edges) 
     {
-      int visited[] = new int[V];
-      PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(a[0]-b[0]));
+      DisjointSet ds = new DisjointSet(V);
+      PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
       int res=0;
-      pq.offer(new int[]{0,0});
+      for(int i=0;i<edges.size();i++)
+      {
+          for(int j=0;j<edges.get(i).size();j++)
+            {
+                int an = edges.get(i).get(j)[0];
+                int wt = edges.get(i).get(j)[1];
+                
+                pq.offer(new int[]{wt,i,an});
+            }
+      }
       while(!pq.isEmpty())
       {
           int pop[] = pq.poll();
+          int u = pop[1];
+          int v = pop[2];
           int wt = pop[0];
-          int vertex = pop[1];
-          if(visited[vertex]==1)
-            continue;
-          visited[vertex] =1;   
-          res+=wt;
-          
-          for(int it[]:adj.get(vertex))
+          if(ds.getUParent(u)!=ds.getUParent(v))
           {
-              int nv = it[0];
-              int nw = it[1];
-              if(visited[nv]==0)
-              {
-                  pq.offer(new int[]{nw,nv});
-              }
+              ds.UnionBySize(u,v);
+              res+=wt;
           }
       }
       return res;
