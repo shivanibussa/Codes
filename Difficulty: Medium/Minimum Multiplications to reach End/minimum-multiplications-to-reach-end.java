7cda7ff465ab8f62bae0cbad496138d1279c6d16
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-24 11:16:30 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.java
+++ Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.java
@@ -1,38 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-// Position this line where user code will be pasted.
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            int n = sc.nextInt();
-            int[] a = new int[n];
-
-            for (int i = 0; i < n; i++) {
-                a[i] = sc.nextInt();
-            }
-
-            int start = sc.nextInt();
-            int end = sc.nextInt();
-
-            Solution ob = new Solution();
-            int ans = ob.minimumMultiplications(a, start, end);
-            System.out.println(ans);
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution 
@@ -42,33 +7,31 @@ class Solution
         if(start==end)
             return 0;
         int dist[] = new int[100000];
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{start,0});
         for(int i=0;i<100000;i++)
         {
-            dist[i] = Integer.MAX_VALUE;
+            dist[i] = (int)1e9;
         }
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,start});
         dist[start] = 0;
-        
         int mod = 100000;
         while(!q.isEmpty())
         {
             int pop[] = q.poll();
-            int a = pop[0];
-            int s = pop[1];
+            int steps = pop[0], v = pop[1];
             for(int i=0;i<arr.length;i++)
             {
-                int pro = (a*arr[i])%mod;
-                if(dist[pro]>1+s)
+                int pro = (v*arr[i])%mod;
+                if(dist[pro]>1+steps)
                 {
-                    dist[pro] = 1+s;
+                    dist[pro] = 1+steps;
                     if(pro==end)
                         return dist[pro];
-                    q.add(new int[]{pro,dist[pro]});
+                        
+                    q.add(new int[]{dist[pro],pro});
                 }
             }
         }
         return -1;
-        
     }
 }
