6b10efc097f9d4c30d7e251957897d2e371e8d13
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-23 23:45:44 -0500

    Time: 4 ms (98.89%), Space: 55.5 MB (56.52%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -2,53 +2,44 @@ class Solution
 {
     public List<Integer> eventualSafeNodes(int[][] graph) 
     {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int N = graph.length;
-        Queue<Integer> q = new LinkedList<>();
-        ArrayList<Integer> arr = new ArrayList<>();
-        int inorder[] = new int[N];
+        int V = graph.length;
+        int visited[] = new int[V];
+        int pathvisited[] = new int[V];
+        int check[] = new int[V];
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i=0;i<graph.length;i++)
+        {
+            if(visited[i]==0)
+            {
+                dfs(graph,visited,pathvisited,check,i);
+            }
+        }
+        for(int i=0;i<V;i++)
+        {
+            if(check[i]==1)
+                res.add(i);
+        }
+        return res;
+    }
+    public boolean dfs(int graph[][],int vis[],int pathvis[],int check[],int node)
+    {
+        vis[node]=1;
+        pathvis[node]=1;
 
-        for(int i=0;i<N;i++)
+        for(int it:graph[node])
         {
-            adj.add(new ArrayList<>());
-        }
-        for(int i=0;i<N;i++)
-        {
-            for(int j=0;j<graph[i].length;j++)
+            if(vis[it]==0)
             {
-                adj.get(graph[i][j]).add(i);
-                inorder[i]++;
+                if(dfs(graph,vis,pathvis,check,it)==true)
+                    return true;
+            }
+            else if(vis[it]==1 && pathvis[it]==1)
+            {
+                return true;
             }
         }
-        
-        // for(int i=0;i<N;i++)
-        // {
-        //     for(int it:adj.get(i))
-        //     {
-        //         inorder[it]++;
-        //     }
-        // }
-        for(int i=0;i<N;i++)
-        {
-            if(inorder[i]==0)
-            {
-                q.offer(i);
-            }
-        }
-        while(!q.isEmpty())
-        {
-            int pop = q.poll();
-            arr.add(pop);
-            for(int it:adj.get(pop))
-            {
-                inorder[it]--;
-                if(inorder[it]==0)
-                {
-                    q.offer(it);
-                }
-            }
-        }
-        Collections.sort(arr);
-        return arr;
+        check[node]=1;
+        pathvis[node]=0;
+        return false;
     }
 }
