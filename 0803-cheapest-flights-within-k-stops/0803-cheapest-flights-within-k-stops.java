df0075937d53bb77ba43817c1d0c8045eab5c11e
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-24 10:40:55 -0500

    Time: 8 ms (34.97%), Space: 44.7 MB (35.75%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -2,51 +2,47 @@ class Solution
 {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) 
     {
-        List<List<int[]>> adj = new ArrayList<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
+        int V = flights.length;
         int dist[] = new int[n];
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b)->a[0]-b[0]);
 
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
-            dist[i] = Integer.MAX_VALUE;
+            dist[i] = (int)1e9;
         }
-        pq.offer(new int[]{0,0,src});
-        dist[src]=0;
-
         for(int i=0;i<flights.length;i++)
         {
-            int u = flights[i][0];
-            int v = flights[i][1];
-            int t = flights[i][2];
-
-            adj.get(u).add(new int[]{v,t});
+            int a = flights[i][0];
+            int b = flights[i][1];
+            int c = flights[i][2];
+            adj.get(a).add(new int[]{b,c});
         }
-        
-        while(!pq.isEmpty())
+        dist[src]=0;
+        q.add(new int[]{0,0,src});
+
+        while(!q.isEmpty())
         {
-            int pops[] = pq.poll();
-            int stops = pops[0];
-            int time = pops[1];
-            int vertex = pops[2];
+            int pop[] = q.poll();
+            int stops = pop[0], wt = pop[1],ver = pop[2];
 
             if(stops>k)
                 continue;
-            for(int[] neigh:adj.get(vertex))
+            
+            for(int it[]:adj.get(ver))
             {
-                int neighv = neigh[0];
-                int neight = neigh[1];
+                int nv = it[0];
+                int nw = it[1];
 
-                if(stops<=k && dist[neighv]>time+neight)
+                if(stops<=k && dist[nv]>wt+nw)
                 {
-                    dist[neighv] = time+neight;
-                    pq.offer(new int[]{stops+1,dist[neighv],neighv});
+                    dist[nv] = wt+nw;
+                    q.offer(new int[]{stops+1,dist[nv],nv});
                 }
             }
         }
-        if(dist[dst]==Integer.MAX_VALUE)
+        if(dist[dst]==(int)1e9)
             return -1;
-        else
-            return dist[dst];
+        return dist[dst];
     }
 }
