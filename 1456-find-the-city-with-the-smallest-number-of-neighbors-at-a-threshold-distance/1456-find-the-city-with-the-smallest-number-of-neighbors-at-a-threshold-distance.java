1121c83414cfffc3a93e21e8e2f878eba7bd9ff3
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-24 12:55:25 -0500

    Time: 10 ms (59.04%), Space: 44.3 MB (84.27%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,35 +1,36 @@
 class Solution 
 {
-    public int findTheCity(int n, int[][] edges, int distanceThreshold) 
+    public int findTheCity(int n, int[][] edges, int threshold) 
     {
         int dist[][] = new int[n][n];
-        int res = n,vertex=-1;
+        int res=n+1,ans=n+1;
         for(int i=0;i<n;i++)
+        {
             for(int j=0;j<n;j++)
-                dist[i][j] = Integer.MAX_VALUE;
+            {
+                dist[i][j] = (int)1e8;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            dist[i][i] = 0;
+        }
         for(int i=0;i<edges.length;i++)
         {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int wt = edges[i][2];
-
-            dist[u][v] = wt;
+            int u = edges[i][0], v = edges[i][1], wt = edges[i][2];
             dist[v][u] = wt;
+            dist[u][v] = wt;
         }
-        for(int i=0;i<n;i++)
-        {
-            dist[i][i]=0;
-        }
-
         for(int k=0;k<n;k++)
         {
             for(int i=0;i<n;i++)
             {
                 for(int j=0;j<n;j++)
                 {
-                    if(dist[i][k] == Integer.MAX_VALUE || dist[k][j] == Integer.MAX_VALUE)
-                        continue;
-                    dist[i][j] = Math.min(dist[i][j],dist[i][k]+dist[k][j]);
+                    if(dist[i][k]!=(int)1e8 && dist[k][j]!=(int)1e8)
+                    {
+                        dist[i][j] = Math.min(dist[i][j],dist[i][k]+dist[k][j]);
+                    }
                 }
             }
         }
@@ -38,7 +39,7 @@ class Solution
             int c=0;
             for(int j=0;j<n;j++)
             {
-                if(dist[i][j]<=distanceThreshold)
+                if(dist[i][j]<=threshold)
                 {
                     c++;
                 }
@@ -46,9 +47,9 @@ class Solution
             if(c<=res)
             {
                 res = c;
-                vertex=i;
+                ans = i;
             }
         }
-        return vertex;
+        return ans;
     }
 }
