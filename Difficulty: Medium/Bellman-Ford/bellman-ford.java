f2d6357cc93e9e09c61a9d6ed055b46a55836569
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-24 12:21:14 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bellman-Ford/bellman-ford.java
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.java
@@ -1,50 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class DriverClass {
-    public static void main(String args[]) throws IOException {
-
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine().trim());
-
-        while (t-- > 0) {
-
-            int V = Integer.parseInt(read.readLine().trim());
-            int E = Integer.parseInt(read.readLine().trim());
-
-            int[][] edges = new int[E][3];
-
-            for (int i = 0; i < E; i++) {
-                String[] parts = read.readLine().trim().split(" ");
-                int u = Integer.parseInt(parts[0]);
-                int v = Integer.parseInt(parts[1]);
-                int w = Integer.parseInt(parts[2]);
-
-                edges[i][0] = u;
-                edges[i][1] = v;
-                edges[i][2] = w;
-            }
-
-            // Read source vertex
-            int S = Integer.parseInt(read.readLine().trim());
-
-            Solution ob = new Solution();
-            int[] ptr = ob.bellmanFord(V, edges, S);
-
-            // Print the result
-            for (int i = 0; i < ptr.length; i++) {
-                System.out.print(ptr[i] + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution 
@@ -53,38 +6,33 @@ class Solution
     {
         int dist[] = new int[V];
         for(int i=0;i<V;i++)
-            dist[i] = (int)1e8;
-            
-        dist[src] = 0;
-        for(int j=0;j<V-1;j++)
         {
-            for(int edge[] : edges)
+            dist[i] = (int)1e8;
+        }
+        dist[src]=0;
+        for(int i=0;i<V-1;i++)
+        {
+            for(int j=0;j<edges.length;j++)
             {
-                int a = edge[0];
-                int b = edge[1];
-                int wt = edge[2];
+                int u = edges[j][0];
+                int v = edges[j][1];
+                int w = edges[j][2];
                 
-                if(dist[a]!=(int)1e8 && dist[a]+wt<dist[b])
+                if(dist[u]!=(int)1e8 && w+dist[u]<dist[v])
                 {
-                    dist[b] = dist[a]+wt;
+                    dist[v] = w+dist[u];
                 }
-                
             }
         }
-        
-        for(int i=0;i<1;i++)
+        for(int j=0;j<edges.length;j++)
         {
-            for(int edge[] : edges)
+            int u = edges[j][0];
+            int v = edges[j][1];
+            int w = edges[j][2];
+            
+            if(dist[u]!=(int)1e8 && w+dist[u]<dist[v])
             {
-                int a = edge[0];
-                int b = edge[1];
-                int wt = edge[2];
-                
-                if(dist[a]!=(int)1e8 && dist[a]+wt<dist[b])
-                {
-                    return new int[]{-1};
-                }
-                
+                return new int[]{-1};
             }
         }
         return dist;
