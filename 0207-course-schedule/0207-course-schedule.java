3f1c1c9c9f1772e9bfe52894ffa010194a3e8cf7
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-23 23:06:18 -0500

    Time: 7 ms (48.42%), Space: 45.5 MB (56.12%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -2,40 +2,41 @@ class Solution
 {
     public boolean canFinish(int V, int[][] edges) 
     {
-        List<List<Integer>> adj = new ArrayList<>();
-        ArrayList<Integer> al = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
         int indegree[] = new int[V];
-        
-        for(int i =0;i<V;i++)
+       ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+       Queue<Integer> q = new LinkedList<>();
+       ArrayList<Integer> res = new ArrayList<>();
+       for(int i=0;i<V;i++)
             adj.add(new ArrayList<>());
             
-        for(int pair[]:edges)
-        {
-            int a = pair[0], b = pair[1];
-            adj.get(a).add(b);
-            indegree[b]++;
-        }
-        
-        for(int i=0;i<V;i++)
-        {
-            if(indegree[i]==0)
-                q.offer(i);
-        }
-        
-        while(!q.isEmpty())
-        {
-            int pop = q.poll();
-            al.add(pop);
-            for(int it:adj.get(pop))
-            {
-                indegree[it]--;
-                if(indegree[it]==0)
-                {
-                    q.offer(it);
-                }
-            }
-        }
-        return al.size()==V?true:false;
+       for(int i=0;i<edges.length;i++)
+       {
+           int a = edges[i][0];
+           int b = edges[i][1];
+           
+           adj.get(a).add(b);
+           indegree[b]++;
+       }
+       for(int i=0;i<V;i++)
+       {
+           if(indegree[i]==0)
+           {
+               q.add(i);
+           }
+       }
+       while(!q.isEmpty())
+       {
+           int pop = q.poll();
+           res.add(pop);
+           for(int it:adj.get(pop))
+           {
+               indegree[it]--;
+               if(indegree[it]==0)
+               {
+                   q.add(it);
+               }
+           }
+       }
+       return (res.size()==V);
     }
 }
