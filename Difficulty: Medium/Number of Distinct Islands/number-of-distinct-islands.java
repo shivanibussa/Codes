9d3dbf4c300df71609c2f49b0dd09c9a0bb5bb4c
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-23 22:01:19 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.java
+++ Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.java
@@ -1,75 +1,7 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-// Position this line where user code will be pasted.
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            int[][] grid = new int[n][m];
-
-            for (int i = 0; i < n; i++) {
-
-                for (int j = 0; j < m; j++) {
-                    grid[i][j] = sc.nextInt();
-                }
-            }
-
-            Solution ob = new Solution();
-            int ans = ob.countDistinctIslands(grid);
-            System.out.println(ans);
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution 
 {
-    public void bfs(int r,int c,int grid[][],int visited[][],ArrayList<String>al,int r0,int c0)
-    {
-        int rows = grid.length, cols = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        visited[r][c] = 1;
-        q.offer(new int[]{r,c});
-        int dr[] = {-1,0,1,0};
-        int dc[] = {0,-1,0,1};
-        
-        while(!q.isEmpty())
-        {
-            int pop[] = q.poll();
-            int x=pop[0],y=pop[1];
-            al.add(toString(x-r0,y-c0));
-            for(int k=0;k<4;k++)
-            {
-                int nr = x+dr[k];
-                int nc = y+dc[k];
-                
-                if(nr>=0 && nr<rows && nc>=0 && nc<cols && grid[nr][nc]==1 && visited[nr][nc]==0)
-                {
-                    q.offer(new int[]{nr,nc});
-                    visited[nr][nc]=1;
-                }
-            }
-        }
-    }
-    
-    public String toString(int x,int y)
-    {
-        return Integer.toString(x)+" "+Integer.toString(y);
-    }
 
     int countDistinctIslands(int[][] grid) 
     {
@@ -80,15 +12,50 @@ class Solution
         {
             for(int j=0;j<n;j++)
             {
-                if(grid[i][j]==1 && visited[i][j] ==0)
+                if(visited[i][j]==0 && grid[i][j]==1)
                 {
                     ArrayList<String> temp = new ArrayList<>();
-                    bfs(i,j,grid,visited,temp,i,j);
+                    bfs(i,j,visited,temp,grid,i,j);
                     hs.add(temp);
                 }
             }
         }
         return hs.size();
         
+    }
+    
+    public void bfs(int i,int j,int visited[][],ArrayList<String> temp, int grid[][],int r0,int c0)
+    {
+        int m = grid.length,n = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{i,j});
+        visited[i][j] = 1;
+        int dr[] = new int[]{-1,0,1,0};
+        int dc[] = new int[]{0,-1,0,1};
+        
+        while(!q.isEmpty())
+        {
+            int pop[] = q.poll();
+            int r = pop[0], c = pop[1];
+            temp.add(toString(r-r0,c-c0));
+            
+            for(int k=0;k<4;k++)
+            {
+                
+                int nr = r+dr[k];
+                int nc = c+dc[k];
+                if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1 && visited[nr][nc]==0)
+                {
+                    q.add(new int[]{nr,nc});
+                    visited[nr][nc]=1;
+                }
+                
+            }
+            
+        }
+    }
+    public String toString(int a,int b)
+    {
+        return String.valueOf(a)+" "+String.valueOf(b);
     }
 }
