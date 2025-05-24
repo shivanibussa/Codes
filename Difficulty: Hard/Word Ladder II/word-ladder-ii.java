c40f3dfe09c2a65102bcecb310b9647ff018a414
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-23 20:21:11 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Word Ladder II/word-ladder-ii.java
+++ Difficulty: Hard/Word Ladder II/word-ladder-ii.java
@@ -1,122 +1,65 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class comp implements Comparator<ArrayList<String>> {
-    // override the compare() method
-    public int compare(ArrayList<String> a, ArrayList<String> b) {
-        String x = "";
-        String y = "";
-        for (int i = 0; i < a.size(); i++) x += a.get(i);
-        for (int i = 0; i < b.size(); i++) y += b.get(i);
-        return x.compareTo(y);
-    }
-}
-
-public class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            String[] wordList = new String[n];
-            for (int i = 0; i < n; i++) {
-                wordList[i] = br.readLine().trim();
-            }
-            String startWord, targetWord;
-            startWord = br.readLine().trim();
-            targetWord = br.readLine().trim();
-            Solution obj = new Solution();
-            ArrayList<ArrayList<String>> ans =
-                obj.findSequences(startWord, targetWord, wordList);
-            if (ans.size() == 0)
-                System.out.println(-1);
-            else {
-                Collections.sort(ans, new comp());
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < ans.size(); i++) {
-                    for (int j = 0; j < ans.get(i).size(); j++) {
-                        sb.append(ans.get(i).get(j) + " ");
-                    }
-                    if (i != ans.size() - 1) sb.append("\n");
-                }
-                System.out.println(sb);
-            }
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution 
 {
-    public ArrayList<ArrayList<String>> findSequences(String startWord, String targetWord,
-                                                      String[] wordList)
+public ArrayList<ArrayList<String>> findSequences(String startWord, String targetWord, String[] wordList) 
+                                                      
     {
         Queue<ArrayList<String>> q = new LinkedList<>();
-        ArrayList<String> ls = new ArrayList<>();
         ArrayList<String> usedOnLevel = new ArrayList<>();
-        HashSet<String> hs = new HashSet<>();
+        ArrayList<String> temp = new ArrayList<>();
         ArrayList<ArrayList<String>> res = new ArrayList<>();
-        
-        ls.add(startWord);
-        usedOnLevel.add(startWord);
-        q.add(ls);
-        
-        for(int i=0;i<wordList.length;i++)
-            hs.add(wordList[i]);
+        int level =0;
+        temp.add(startWord);
+        q.add(temp);
+        HashSet<String> hs = new HashSet<>();
+        for(String w:wordList)
+            hs.add(w);
             
-        int level=0;
         while(!q.isEmpty())
         {
             ArrayList<String> al = q.poll();
+            if(al.size()>level)
             {
-                if(level<al.size())
+                level++;
+                for(String w:usedOnLevel)
                 {
-                    level++;
-                    for(String w: usedOnLevel)
-                    {
-                        hs.remove(w);
-                    }
+                    hs.remove(w);
                 }
+                usedOnLevel.clear();
             }
             String w = al.get(al.size()-1);
             if(w.equals(targetWord))
             {
                 if(res.size()==0)
                 {
-                    res.add(new ArrayList<>(al));
+                    res.add(al);
                 }
                 else if(res.get(0).size()==al.size())
                 {
-                    res.add(new ArrayList<>(al));
+                    res.add(al);
                 }
             }
             for(int i=0;i<w.length();i++)
             {
-                for(char c='a';c<='z';c++)
+                for(char ch='a';ch<='z';ch++)
                 {
-                    char chs[] = w.toCharArray();
-                    chs[i] = c;
-                    String news = new String(chs);
+                    char chars[] = w.toCharArray();
+                    chars[i] = ch;
+                    String newW = new String(chars);
                     
-                    if(hs.contains(news))
+                    if(hs.contains(newW))
                     {
-                        ArrayList<String> temp = new ArrayList<>(al);
-                        temp.add(news);
-                        q.add(temp);
-                        usedOnLevel.add(news);
+                        ArrayList<String> temps = new ArrayList<>(al);
+                        temps.add(newW);
+                        q.add(temps);
+                        usedOnLevel.add(newW);
                     }
                 }
             }
         }
+            
         
-        return res;
-        
+        return res; 
     }
 }
