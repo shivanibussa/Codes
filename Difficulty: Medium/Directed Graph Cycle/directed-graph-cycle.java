ce35964dd5cba184ff17632793580a1f9a1324b1
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-23 22:35:09 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,75 +1,47 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        while (tc-- > 0) {
-            int V = sc.nextInt();
-            int E = sc.nextInt();
-            int[][] edges = new int[E][2];
-            for (int i = 0; i < E; i++) {
-                edges[i][0] = sc.nextInt();
-                edges[i][1] = sc.nextInt();
-            }
-
-            Solution obj = new Solution();
-            boolean ans = obj.isCyclic(V, edges);
-            System.out.println(ans ? "true" : "false");
-        }
-        sc.close();
-    }
-}
-// } Driver Code Ends
-
-
 class Solution 
 {
-    public boolean isCyclic(int V, int[][] edges) 
+    public boolean isCyclic(int V, int[][] edges)
     {
-       List<List<Integer>> adj = new ArrayList<>();
-       int visited[] = new int[V];
-       int pathvis[] = new int[V];
-       for(int i=0;i<V;i++)
-            adj.add(new ArrayList<>());
-       for(int pair[]:edges)
-       {
-           int a = pair[0];
-           int b = pair[1];
-           
-           adj.get(a).add(b);
-       }
-        
-       for(int i=0;i<V;i++)
-       {
-           if(visited[i]==0)
-           {
-               if(dfs(adj,visited,pathvis,i)==true)
-                return true;
-           }
-       }
-       return false;
-    }
-    
-    public boolean dfs(List<List<Integer>> adj, int visited[],int pathvis[],int node)
-    {
-        visited[node]=1;
-        pathvis[node]=1;
-        for(int nv:adj.get(node))
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int visited[] = new int[V];
+        int pathvisited[] = new int[V];
+        for(int i=0;i<V;i++)
         {
-            if(visited[nv]==0)
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<edges.length;i++)
+        {
+            int a = edges[i][0];
+            int b = edges[i][1];
+            adj.get(a).add(b);
+        }
+        for(int i=0;i<V;i++)
+        {
+            if(visited[i]==0)
             {
-                if(dfs(adj,visited,pathvis,nv)==true)
+                if(dfs(i,visited,pathvisited,adj)==true)
                     return true;
             }
-            else if(pathvis[nv]==1 && visited[nv]==1)
+        }
+        return false;
+    }
+    public boolean dfs(int node,int visited[],int pathvisited[],ArrayList<ArrayList<Integer>> adj)
+    {
+        visited[node]=1;
+        pathvisited[node]=1;
+        for(int it:adj.get(node))
+        {
+            if(visited[it]==0)
+            {
+                if(dfs(it,visited,pathvisited,adj)==true)
+                    return true;
+            }
+            else if(visited[it]==1 && pathvisited[it]==1)
             {
                 return true;
             }
         }
-        pathvis[node]=0;
+        pathvisited[node]=0;
         return false;
     }
 }
