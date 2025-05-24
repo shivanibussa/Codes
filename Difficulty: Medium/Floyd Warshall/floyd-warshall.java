6843f86e1519d8a4d79a28af29b30b834f4734ef
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-24 12:38:18 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.java
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.java
@@ -1,41 +1,7 @@
-//{ Driver Code Starts
-// Initial template for JAVA
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            int[][] matrix = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                String[] s = br.readLine().trim().split(" ");
-                for (int j = 0; j < n; j++) matrix[i][j] = Integer.parseInt(s[j]);
-            }
-            Solution obj = new Solution();
-            obj.floydWarshall(matrix);
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    System.out.print(matrix[i][j] + " ");
-                }
-                System.out.println();
-            }
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 // User function template for JAVA
 
-class Solution {
+class Solution 
+{
     public void floydWarshall(int[][] dist) 
     {
         int n = dist.length;
@@ -45,11 +11,12 @@ class Solution {
             {
                 for(int j=0;j<n;j++)
                 {
-                    if(dist[i][via]!=1e8 && dist[via][j]!=1e8)
-                    dist[i][j] = Math.min(dist[i][j],dist[i][via]+dist[via][j]);
+                    if(dist[i][via]!=(int)1e8 && dist[via][j]!=(int)1e8)
+                    {
+                        dist[i][j] = Math.min(dist[i][j],dist[i][via]+dist[via][j]);
+                    }
                 }
             }
         }
-        
     }
 }
