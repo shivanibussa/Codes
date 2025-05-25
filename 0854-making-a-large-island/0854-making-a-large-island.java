b92516aefbc5c43006aa83887528996d8bbadd96
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-24 20:38:06 -0500

    Time: 143 ms (27.63%), Space: 80.3 MB (38.27%) - LeetHub

--- 0854-making-a-large-island/0854-making-a-large-island.java
+++ 0854-making-a-large-island/0854-making-a-large-island.java
@@ -1,120 +1,104 @@
-class DisjointSet{
-    
-    ArrayList<Integer> size;
+class DisjointSet
+{
     ArrayList<Integer> parent;
-    
-    DisjointSet(int n)
+    ArrayList<Integer> size;
+    public DisjointSet(int V)
     {
-        size = new ArrayList<>();
         parent = new ArrayList<>();
-        for(int i=0;i<n;i++)
+        size = new ArrayList<>();
+        for(int i=0;i<V;i++)
         {
-            size.add(1);
             parent.add(i);
+            size.add(1);
         }
     }
-    
-    public int findUparent(int node)
+    public int getUparent(int node)
     {
         if(node==parent.get(node))
             return node;
-        int root = findUparent(parent.get(node));
-        parent.set(node,root);
-        return root;
+        int ulp_u = getUparent(parent.get(node));
+        parent.set(node,ulp_u);
+        return parent.get(node);
     }
-    
-    public void UnionBySize(int u,int v)
+    public void unionBySize(int u,int v)
     {
-        int ulp_u = findUparent(u);
-        int ulp_v = findUparent(v);
+        int ulp_u = getUparent(u);
+        int ulp_v = getUparent(v);
         if(ulp_u==ulp_v)
             return;
-        if(size.get(ulp_u)<size.get(ulp_v))
-        {
-            parent.set(ulp_u,ulp_v);
-            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
-        }
-        else
+        if(size.get(ulp_u)>size.get(ulp_v))
         {
             parent.set(ulp_v,ulp_u);
             size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
         }
+        else
+        {
+            parent.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
+        }
     }
 }
+
 class Solution 
 {
     public int largestIsland(int[][] grid) 
     {
-        int m = grid.length,n=grid[0].length;
+        
+        int m = grid.length, n = grid[0].length;
+        int dr[] = new int[]{-1,0,1,0};
+        int dc[] = new int[]{0,-1,0,1};
         DisjointSet ds = new DisjointSet(m*n);
+        int ans = 0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==0)
                     continue;
-                else
+                for(int k=0;k<4;k++)
                 {
-                    int dr[] = {-1,0,1,0};
-                    int dc[] = {0,-1,0,1};
-                    
-                    for(int k=0;k<4;k++)
+                    int nr = dr[k]+i;
+                    int nc = dc[k]+j;
+
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1)
                     {
-                        int nr = i+dr[k];
-                        int nc = j+dc[k];
-                        
-                        if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1)
-                        {
-                            int cell = i*n+j;
-                            int adjCell = nr*n+nc;
-                            
-                            ds.UnionBySize(cell,adjCell);
-                            
-                        }
+                        int neighN = nr*n+nc;
+                        int node = i*n+j;
+                        ds.unionBySize(node,neighN);
                     }
                 }
             }
         }
-        
-        int maxCount=0;
+
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                HashSet<Integer> hs = new HashSet<>();
                 if(grid[i][j]==1)
                     continue;
-                else
+                HashSet<Integer> hs = new HashSet<>();
+                for(int k=0;k<4;k++)
                 {
-                    int dr[] = {-1,0,1,0};
-                    int dc[] = {0,-1,0,1};
-                    
-                    for(int k=0;k<4;k++)
+                    int nr = i+dr[k];
+                    int nc = j+dc[k];
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1)
                     {
-                        int nr = i+dr[k];
-                        int nc = j+dc[k];
-                        
-                        if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc] == 1)
-                        {
-                            int cell = i*n+j;
-                            int adjCell = nr*n+nc;
-                            hs.add(ds.findUparent(adjCell));
-                        }
+                        int neighN = nr*n+nc;
+                        hs.add(ds.getUparent(neighN));
                     }
-                    int tot=0;
-                    for(int ele:hs)
-                    {
-                        tot+=ds.size.get(ele);
-                    }
-                    maxCount = Math.max(maxCount,1+tot);
                 }
+                int Tsize = 0;
+                for(Integer it:hs)
+                {
+                    Tsize+=ds.size.get(it);
+                }
+                ans = Math.max(ans,Tsize+1);
             }
         }
-        
         for(int i=0;i<m*n;i++)
         {
-            maxCount = Math.max(maxCount,ds.size.get(ds.findUparent(i)));
+            ans = Math.max(ans,ds.size.get(ds.getUparent(i)));
         }
-        return maxCount;
+        return ans;
     }
 }
