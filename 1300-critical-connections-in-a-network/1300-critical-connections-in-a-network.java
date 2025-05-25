745f7a8136476e42416671814fe8792a98aec3ec
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-24 22:43:42 -0500

    Time: 80 ms (84.07%), Space: 115.3 MB (73.93%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
@@ -1,54 +1,53 @@
 class Solution 
 {
-    private int timer = 1;
+    private static int timer = 1;
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) 
     {
-        boolean visited[] = new boolean[n];
         int tin[] = new int[n];
         int low[] = new int[n];
+        int visited[] = new int[n];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         List<List<Integer>> bridges = new ArrayList<>();
-        List<List<Integer>> adj = new ArrayList<>();
         for(int i=0;i<n;i++)
         {
             adj.add(new ArrayList<>());
         }
-
-        for(List<Integer> it:connections)
+        for(int i=0;i<connections.size();i++)
         {
-            int u = it.get(0), v = it.get(1);
+            int u = connections.get(i).get(0);
+            int v = connections.get(i).get(1);
+
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-
-        dfs(0,-1,adj,visited,tin,low,bridges);
+        dfs(tin,low,adj,visited,bridges,0,-1);
         return bridges;
-
     }
-    public void dfs(int node, int parent,List<List<Integer>> adj, boolean visited[],int tin[],int low[],List<List<Integer>> bridges)
+    public void dfs(int tin[],int low[],ArrayList<ArrayList<Integer>> adj,int visited[],List<List<Integer>> bridges,int node,int parent)
     {
-        visited[node] = true;
+        visited[node]=1;
         tin[node] = timer;
         low[node] = timer;
         timer++;
-
         for(int it:adj.get(node))
         {
             if(it==parent)
                 continue;
-            if(!visited[it])
+
+            if(visited[it]==0)
             {
-                dfs(it,node,adj,visited,tin,low,bridges);
-                low[node] = Math.min(low[node],low[it]);
+                dfs(tin,low,adj,visited,bridges,it,node);
+                low[node] = Math.min(low[it],low[node]);
                 if(low[it]>tin[node])
                 {
-                    List<Integer> temp = new ArrayList<>();
+                    ArrayList<Integer> temp = new ArrayList<>();
                     temp.add(it);temp.add(node);
                     bridges.add(temp);
-                }   
+                }
             }
             else
             {
-                low[node] = Math.min(low[node],low[it]);
+                low[node] = Math.min(low[it],low[node]);
             }
         }
     }
