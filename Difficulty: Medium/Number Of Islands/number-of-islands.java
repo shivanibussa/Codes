7fbb3ff14719db9bf226833ea503886077df23de
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-24 20:09:13 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number Of Islands/number-of-islands.java
+++ Difficulty: Medium/Number Of Islands/number-of-islands.java
@@ -1,137 +1,95 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-
-// } Driver Code Ends
-
-//User function Template for Java
 class DisjointSet
 {
-    ArrayList<Integer> parent = new ArrayList<>();
-    ArrayList<Integer> size = new ArrayList<>();
-    DisjointSet(int n)
+    ArrayList<Integer> parent;
+    ArrayList<Integer> size;
+    public DisjointSet(int V)
     {
-        for(int i=0;i<n;i++)
+        parent = new ArrayList<>();
+        size = new ArrayList<>();
+
+
+        for(int i=0;i<V;i++)
         {
             parent.add(i);
             size.add(1);
         }
     }
-    
-    public int findUparent(int node)
+
+
+    public int getUparent(int node)
     {
         if(node==parent.get(node))
-        {
             return node;
-        }
-        parent.set(node,findUparent(parent.get(node)));
+
+
+        int ulp_u = getUparent(parent.get(node));
+        parent.set(node,ulp_u);
         return parent.get(node);
     }
-    
-    public void UnionBySize(int u,int v)
+
+
+    public void unionBySize(int u,int v)
     {
-        int ulp_u = findUparent(u);
-        int ulp_v = findUparent(v);
+        int ulp_u = getUparent(u);
+        int ulp_v = getUparent(v);
         if(ulp_u==ulp_v)
             return;
-        if(size.get(ulp_u)<size.get(ulp_v))
+
+
+        if(size.get(ulp_u)>size.get(ulp_v))
         {
-            parent.set(ulp_u,ulp_v);
-            size.set(ulp_v,(size.get(ulp_u)+size.get(ulp_v)));
+            parent.set(ulp_v,ulp_u);
+            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
         }
         else
         {
-            parent.set(ulp_v,ulp_u);
-            size.set(ulp_u,(size.get(ulp_u)+size.get(ulp_v)));
+            parent.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
         }
     }
-    
-    
 }
 class Solution 
 {
-    
     public List<Integer> numOfIslands(int rows, int cols, int[][] operators) 
     {
-        int visited[][] = new int[rows][cols];
         DisjointSet ds = new DisjointSet(rows*cols);
-        ArrayList<Integer> res = new ArrayList<>();
+        int visited[][] = new int[rows][cols];
+        ArrayList<Integer> ans = new ArrayList<>();
         int cnt=0;
-        
-        for(int i=0;i<operators.length;i++)
+        int n = operators.length;
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,-1,0,1};
+        for(int i=0;i<n;i++)
         {
-            int r = operators[i][0];
-            int c = operators[i][1];
-            
-            if(visited[r][c]==1)
-            {
-                res.add(cnt);
+            int a = operators[i][0];
+            int b = operators[i][1];
+            if(visited[a][b]==1){
+                ans.add(cnt);
                 continue;
             }
-            
-            visited[r][c]=1;
-            cnt++;
-            int dr[] = {-1,0,1,0};
-            int dc[] = {0,-1,0,1};
-            
-            for(int j=0;j<4;j++)
-            {
-                int nr = r+dr[j];
-                int nc = c+dc[j];
-                if(nr>=0 && nr<rows && nc>=0 && nc<cols)
-                {
-                    if(visited[nr][nc]==1)
-                    {
-                        int node = r*cols+c, adjNode = nr*cols+nc;
-                        if(ds.findUparent(node)!=ds.findUparent(adjNode))
-                        {
-                            cnt--;
-                            ds.UnionBySize(node,adjNode);
-                        }
-                    }
-                }                
                 
+                
+            visited[a][b] = 1;
+            cnt++;
+            for(int k=0;k<4;k++)
+            {
+                int nr = a+dr[k];
+                int nc = b+dc[k];
+                
+                if(nr>=0 && nr<rows && nc>=0 && nc<cols && visited[nr][nc]==1)
+                {
+                    int neighN = nr*cols+nc;
+                    int node = a*cols+b;
+                    if(ds.getUparent(neighN)!=ds.getUparent(node))
+                    {
+                        cnt--;
+                        ds.unionBySize(neighN,node);
+                        
+                    }
+                }
             }
-            res.add(cnt);
+            ans.add(cnt);
         }
-        return res;
-    }
-    
-}
-
-
-//{ Driver Code Starts.
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            int  k= sc.nextInt();
-            int[][] a = new int[k][2];
-            for(int i=0;i<k;i++){
-            
-                a[i][0] = sc.nextInt();
-                a[i][1] = sc.nextInt();
-            }
-            
-            Solution obj = new Solution();
-            List<Integer> ans = obj.numOfIslands(n,m,a);
-           
-            for(int i:ans){
-                System.out.print(i+" ");
-            }
-            System.out.println();
-        
-System.out.println("~");
-}
+        return ans;
     }
 }
-
-// } Driver Code Ends
