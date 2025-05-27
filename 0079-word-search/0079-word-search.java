681935962c8cd00f574dd1160358916cd698571c
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-26 23:35:45 -0500

    Time: 134 ms (78.15%), Space: 41.9 MB (32.48%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+    public boolean exist(char[][] board, String word) 
+    {
+        int rows = board.length, cols = board[0].length;
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if(f(board,i,j,word,0)==true)
+                    return true;
+            }
+        }
+        return false;
+    }
+    public boolean f(char board[][],int r,int c,String word,int i)
+    {
+        if(i==word.length())
+            return true;
+        if(r<0 || r>=board.length || c<0 || c>=board[0].length || board[r][c]!=word.charAt(i) || board[r][c]=='#')
+            return false;
+        
+        board[r][c] = '#';
+        boolean res = f(board,r+1,c,word,i+1) || f(board,r,c+1,word,i+1) ||f(board,r-1,c,word,i+1) || f(board,r,c-1,    word,i+1);
+        board[r][c] = word.charAt(i);
+        return res;
+    }
+}
