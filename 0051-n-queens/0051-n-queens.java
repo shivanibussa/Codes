d026b143280dce41f26865e72b147dfdc944b170
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-27 00:04:33 -0500

    Time: 1 ms (99.81%), Space: 44.9 MB (57.08%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -11,38 +11,39 @@ class Solution
             }
         }
         List<List<String>> ans = new ArrayList<>();
-        int leftrow[] = new int[n];
-        int belowdiagonal[] = new int[2*n-1];
-        int upperdiagonal[] = new int[2*n-1];
-        helper(ans,leftrow,belowdiagonal,upperdiagonal,board,0);
+        int lr[] = new int[n];
+        int bd[] = new int[2*n-1];
+        int ud[] = new int[2*n-1];
+        f(ans,lr,bd,ud,board,0);
         return ans;
     }
-    public static void helper(List<List<String>> ans, int[] leftrow,int lowd[], int upd[],char board[][], int col)
+    public static void f(List<List<String>> ans,int lr[],int bd[],int ud[],char board[][],int col)
     {
         if(col==board.length)
-        {
             ans.add(construct(board));
-        }
         for(int row=0;row<board.length;row++)
         {
-            if(leftrow[row]==0 && lowd[row+col]==0 && upd[board.length-1+col-row]==0)
+            if(lr[row]==0 && bd[row+col]==0 && ud[board.length-1+col-row]==0)
             {
                 board[row][col] = 'Q';
-                leftrow[row]=1;
-                lowd[row+col]=1;
-                upd[board.length-1+col-row]=1;
-                helper(ans,leftrow,lowd,upd,board,col+1);
+                lr[row]=1;
+                ud[board.length-1+col-row]=1;
+                bd[row+col]=1;
+                f(ans,lr,bd,ud,board,col+1);
                 board[row][col] = '.';
-                leftrow[row]=0;
-                lowd[row+col]=0;
-                upd[board.length-1+col-row]=0;
+                lr[row]=0;
+                ud[board.length-1+col-row]=0;
+                bd[row+col]=0;
             }
         }
     }
-    static List < String > construct(char[][] board) {
-        List < String > res = new LinkedList < String > ();
-        for (int i = 0; i < board.length; i++) {
-            String s = new String(board[i]);
+
+    public static List<String> construct(char[][] board)
+    {
+        List<String> res = new LinkedList<>();
+        for(int i=0;i<board.length;i++)
+        {
+            String s= new String(board[i]);
             res.add(s);
         }
         return res;
