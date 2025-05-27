6171131bcd77415c5af86114faf3b9835a06c9d1
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-27 07:59:26 -0500

    Time: 371 ms (32.12%), Space: 41 MB (73.34%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,12 +1,14 @@
-class Solution {
-    public void solveSudoku(char[][] board) {
-        solve(board);
-    }
-    public static boolean solve(char[][] board)
+class Solution 
+{
+    public void solveSudoku(char[][] board) 
     {
-        for(int i=0;i<board.length;i++)
+        f(board);
+    }
+    public boolean f(char board[][])
+    {
+        for(int i=0;i<9;i++)
         {
-            for(int j=0;j<board[0].length;j++)
+            for(int j=0;j<9;j++)
             {
                 if(board[i][j]=='.')
                 {
@@ -14,28 +16,30 @@ class Solution {
                     {
                         if(isValid(board,i,j,c)==true)
                         {
-                            board[i][j] = c;
-                        if(solve(board)==true)
-                            return true;
-                        else
-                            board[i][j]='.';
+                            board[i][j]=c;
+                            if(f(board)==true)
+                                return true;
+                            else
+                                board[i][j]='.';
                         }
                     }
                     return false;
+                    
                 }
+                
             }
         }
         return true;
     }
-    public static boolean isValid(char[][] board,int row, int col, char c)
+    public boolean isValid(char board[][],int r,int c,char ch)
     {
         for(int i=0;i<9;i++)
         {
-            if(board[i][col]==c)
+            if(board[i][c]==ch)
                 return false;
-            if(board[row][i]==c)
+            if(board[r][i]==ch)
                 return false;
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+            if(board[3*(r/3)+i/3][3*(c/3)+i%3]==ch)
                 return false;
         }
         return true;
