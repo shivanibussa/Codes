7e9ff4559be29453e9d6d643fb781855cb68c423
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-29 12:47:40 -0500

    Time: 98 ms (7.06%), Space: 57.1 MB (90.53%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,56 +1,19 @@
-public class Solution {
-    private int partition(int[] nums, int left, int right) {
-        int mid = (left + right) >> 1;
-        swap(nums, mid, left + 1);
-        
-        if (nums[left] < nums[right]) 
-            swap(nums, left, right);
-        if (nums[left + 1] < nums[right]) 
-            swap(nums, left + 1, right);
-        if (nums[left] < nums[left + 1]) 
-            swap(nums, left, left + 1);
-        
-        int pivot = nums[left + 1];
-        int i = left + 1;
-        int j = right;
-        
-        while (true) {
-            while (nums[++i] > pivot);
-            while (nums[--j] < pivot);
-            if (i > j) break;
-            swap(nums, i, j);
+class Solution 
+{
+    public int findKthLargest(int[] nums, int k) 
+    {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        int n = nums.length;
+        for(int i=0;i<n;i++)
+            pq.offer(nums[i]);
+        int i=0;
+        System.out.println(pq.toString());
+        while(i<k-1)
+        {
+            pq.poll();
+            i=i+1;
         }
-        
-        nums[left + 1] = nums[j];
-        nums[j] = pivot;
-        return j;
-    }
-    
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    
-    private int quickSelect(int[] nums, int k) {
-        int left = 0;
-        int right = nums.length - 1;
-        
-        while (true) {
-            if (right <= left + 1) {
-                if (right == left + 1 && nums[right] > nums[left])
-                    swap(nums, left, right);
-                return nums[k];
-            }
-            
-            int j = partition(nums, left, right);
-            
-            if (j >= k) right = j - 1;
-            if (j <= k) left = j + 1;
-        }
-    }
-    
-    public int findKthLargest(int[] nums, int k) {
-        return quickSelect(nums, k - 1);
+        return pq.peek();
+
     }
 }
