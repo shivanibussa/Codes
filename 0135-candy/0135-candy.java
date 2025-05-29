bb1873b32fd0eb26af41fb5ae8a089a859151c0b
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-29 11:21:45 -0500

    Time: 3 ms (84.81%), Space: 46 MB (45.61%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,33 +1,35 @@
-class Solution 
-{
-    public int candy(int[] ratings) 
-    {
+class Solution {
+    public int candy(int[] ratings) {
         int n = ratings.length;
-        int left[] = new int[n];
-        int right[] = new int[n];
-        int candy=0;
+        int[] left = new int[n];
+        int right = 1;
+        int candy = 0;
+
+        // Fill the left array
         left[0] = 1;
-        for(int i=1;i<n;i++)
-        {
-            if(ratings[i]>ratings[i-1])
-                left[i] = 1+left[i-1];
-            else
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            } else {
                 left[i] = 1;
-        }
-        right[n-1] = 1;
-        for(int i=n-2;i>=0;i--)
-        {
-            if(ratings[i]>ratings[i+1])
-            {
-                right[i] = 1+right[i+1];
             }
-            else
-                right[i] = 1;
         }
-        for(int i=0;i<n;i++)
-        {
-            candy+=Math.max(left[i],right[i]);
+
+        // Start with the last child's candy
+        candy += left[n - 1];
+
+        // Traverse from right to left
+        for (int i = n - 2; i >= 0; i--) {
+            int cur;
+            if (ratings[i] > ratings[i + 1]) {
+                cur = right + 1;
+            } else {
+                cur = 1;
+            }
+            right = cur; // Update right
+            candy += Math.max(left[i], cur);
         }
+
         return candy;
     }
 }
