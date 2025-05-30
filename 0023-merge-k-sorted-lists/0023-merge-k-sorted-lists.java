81e17a373552373fd6f53ccfd85c11dd75f7890c
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-30 11:52:44 -0500

    Time: 6 ms (27.77%), Space: 44.3 MB (78.48%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,54 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+class Pair
+{
+    int val;
+    ListNode node;
+
+    Pair(int val,ListNode node)
+    {
+        this.val = val;
+        this.node = node;
+    }
+}
 class Solution 
 {
     public ListNode mergeKLists(ListNode[] lists) 
     {
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->a.val-b.val);
         if(lists.length==0)
             return null;
         if(lists.length==1)
             return lists[0];
-
-        ListNode merged = merge2(lists[0],lists[1]);
-        for(int i=2;i<lists.length;i++)
+        for(int i=0;i<lists.length;i++)
         {
-            merged = merge2(merged,lists[i]);
+            if(lists[i]!=null)
+                pq.offer(new Pair(lists[i].val,lists[i]));
         }
-        return merged;
-    }
 
-    public ListNode merge2(ListNode l1,ListNode l2)
-    {
-        if(l1==null)
-            return l2;
-        if(l2==null)
-            return l1;
-
-        ListNode temp1 = l1;
-        ListNode temp2 = l2;
         ListNode dummy = new ListNode(0);
         ListNode root = dummy;
-        while(temp1!=null && temp2!=null)
+
+        while(!pq.isEmpty())
         {
-            if(temp1.val<temp2.val)
-            {
-                root.next = temp1;
-                temp1 = temp1.next;
-            }
-            else
-            {
-                root.next = temp2;
-                temp2 = temp2.next;
-            }
+            Pair pop = pq.poll();
+            int node_val = pop.val;
+            ListNode node = pop.node;
+            root.next = node;
+            if(node.next!=null)
+                pq.offer(new Pair(node.next.val,node.next));
             root = root.next;
         }
-        if(temp1!=null)
-            root.next = temp1;
-
-        if(temp2!=null)
-            root.next = temp2;
-
         return dummy.next;
     }
 }
