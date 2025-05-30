4ea15b12ef151a1adef110b5da5a9046dc82e750
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-30 18:50:36 -0500

    Time: 87 ms (51.61%), Space: 46.9 MB (46.96%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -2,10 +2,11 @@ class Pair
 {
     String word;
     int steps;
+
     Pair(String word,int steps)
     {
         this.word = word;
-        this.steps = steps;
+        this.steps = steps; 
     }
 }
 class Solution 
@@ -13,30 +14,31 @@ class Solution
     public int ladderLength(String beginWord, String endWord, List<String> wordList) 
     {
         HashSet<String> hs = new HashSet<>();
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(beginWord,0));
         for(String w:wordList)
             hs.add(w);
         hs.remove(beginWord);
-
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(beginWord,1));
         while(!q.isEmpty())
         {
             Pair pop = q.poll();
-            String rw = pop.word;
+            String word = pop.word;
             int steps = pop.steps;
-            if(rw.equals(endWord))
-                return steps+1;
-            for(int i=0;i<rw.length();i++)
+            // if(word.equals(endWord))
+            //     return steps+1;
+            for(int i=0;i<word.length();i++)
             {
-                for(char ch='a';ch<='z';ch++)
+                for(char c='a';c<='z';c++)
                 {
-                    char chs[] = rw.toCharArray();
-                    chs[i] = ch;
-                    String nw = new String(chs);
+                    char chas[] = word.toCharArray();
+                    chas[i] = c;
+                    String nw = new String(chas);
                     if(hs.contains(nw))
                     {
-                        q.add(new Pair(nw,steps+1));
+                        if(nw.equals(endWord))
+                        return steps+1;
                         hs.remove(nw);
+                        q.offer(new Pair(nw,steps+1));
                     }
                 }
             }
