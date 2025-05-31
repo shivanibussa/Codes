c29cd72c5cb6aac0fbeb55818784fdc9fcebfafe
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-30 21:54:15 -0500

    Time: 4 ms (92.15%), Space: 46.2 MB (29.78%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,45 +1,43 @@
-class Solution {
-    public int[] findOrder(int V, int[][] edges) 
+class Solution 
+{
+    public int[] findOrder(int V, int[][] prerequisites) 
     {
-       int indegree[] = new int[V];
-       ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-       Queue<Integer> q = new LinkedList<>();
-       int res[] = new int[V];
-       int c=0;
-       for(int i=0;i<V;i++)
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int indegree[] = new int[V];
+        int cnt=0;
+        Queue<Integer> q = new LinkedList<>();
+        int res[] = new int[V];
+        for(int i=0;i<V;i++)
+        {
             adj.add(new ArrayList<>());
-            
-       for(int i=0;i<edges.length;i++)
-       {
-           int a = edges[i][0];
-           int b = edges[i][1];
-           
-           adj.get(b).add(a);
-           indegree[a]++;
-       }
-       for(int i=0;i<V;i++)
-       {
-           if(indegree[i]==0)
-           {
-               q.add(i);
-           }
-       }
-       while(!q.isEmpty())
-       {
-           int pop = q.poll();
-           res[c++] = pop;
-           for(int it:adj.get(pop))
-           {
-               indegree[it]--;
-               if(indegree[it]==0)
-               {
-                   q.add(it);
-               }
-           }
-       }
-       if(c!=V)
-        return new int[]{};
-       else
-        return res;
+        }        
+        for(int preq[]:prerequisites)
+        {
+            int a = preq[0];
+            int b = preq[1];
+            adj.get(b).add(a);
+            indegree[a]++;
+        }
+        for(int i=0;i<V;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.offer(i);
+            }
+        }
+        while(!q.isEmpty())
+        {
+            int pop = q.poll();
+            res[cnt++] = pop;
+            for(int it:adj.get(pop))
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                {
+                    q.offer(it);
+                }
+            }
+        }
+        return cnt==V ? res:new int[]{};
     }
 }
