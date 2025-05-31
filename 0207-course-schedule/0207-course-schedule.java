b3882a37fe9cc55381b14da3078ab01cd74dfcb5
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-05-30 21:44:01 -0500

    Time: 7 ms (48.35%), Space: 45.7 MB (42.09%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,42 +1,44 @@
 class Solution 
 {
-    public boolean canFinish(int V, int[][] edges) 
+    public boolean canFinish(int V, int[][] prerequisites) 
     {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int indegree[] = new int[V];
-       ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-       Queue<Integer> q = new LinkedList<>();
-       ArrayList<Integer> res = new ArrayList<>();
-       for(int i=0;i<V;i++)
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i=0;i<V;i++)
+        {
             adj.add(new ArrayList<>());
-            
-       for(int i=0;i<edges.length;i++)
-       {
-           int a = edges[i][0];
-           int b = edges[i][1];
-           
-           adj.get(a).add(b);
-           indegree[b]++;
-       }
-       for(int i=0;i<V;i++)
-       {
-           if(indegree[i]==0)
-           {
-               q.add(i);
-           }
-       }
-       while(!q.isEmpty())
-       {
-           int pop = q.poll();
-           res.add(pop);
-           for(int it:adj.get(pop))
-           {
-               indegree[it]--;
-               if(indegree[it]==0)
-               {
-                   q.add(it);
-               }
-           }
-       }
-       return (res.size()==V);
+
+        }        
+        for(int preq[]:prerequisites)
+        {
+            int a = preq[0];
+            int b = preq[1];
+            adj.get(a).add(b);
+            indegree[b]++;
+        }
+
+        for(int i=0;i<V;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.offer(i);
+            }
+        }
+        while(!q.isEmpty())
+        {
+            int pop = q.poll();
+            res.add(pop);
+            for(int it:adj.get(pop))
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                {
+                    q.offer(it);
+                }
+            }
+        }
+        return res.size()==V ? true:false;
     }
 }
