63f59469f7f34c2f9af336236ba2f39de528461d
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-01 11:11:09 -0500

    Time: 1 ms (92.81%), Space: 41.4 MB (97.04%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -1,41 +1,36 @@
-class Solution 
-{
-    public String reorganizeString(String s) 
-    {
-        HashMap<Character,Integer>hm = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
-        for(char ch:s.toCharArray())
-        {
-            hm.put(ch,hm.getOrDefault(ch,0)+1);
+    class Solution{
+    public String reorganizeString(String S) {
+        int[] hash = new int[26];
+        for (int i = 0; i < S.length(); i++) {
+            hash[S.charAt(i) - 'a']++;
+        } 
+        int max = 0, letter = 0;
+        for (int i = 0; i < hash.length; i++) {
+            if (hash[i] > max) {
+                max = hash[i];
+                letter = i;
+            }
         }
-    
-    PriorityQueue<Character>pq = new PriorityQueue<>((a,b)->hm.get(b)-hm.get(a));
-    pq.addAll(hm.keySet());
-
-    while(pq.size()>1)
-    {
-        Character curr = pq.poll();
-        Character next = pq.poll();
-        sb.append(curr);
-        sb.append(next);
-        hm.put(curr,hm.get(curr)-1);
-        hm.put(next,hm.get(next)-1);
-        if(hm.get(curr)>0)
-            pq.add(curr);
-        if(hm.get(next)>0)
-            pq.add(next);
-    }
-
-    if(pq.size()==1)
-    {
-        char ch = pq.poll();
-        if(hm.get(ch)>1)
-            return "";
-        else
-        {
-            sb.append(ch);
+        if (max > (S.length() + 1) / 2) {
+            return ""; 
         }
+        char[] res = new char[S.length()];
+        int idx = 0;
+        while (hash[letter] > 0) {
+            res[idx] = (char) (letter + 'a');
+            idx += 2;
+            hash[letter]--;
+        }
+        for (int i = 0; i < hash.length; i++) {
+            while (hash[i] > 0) {
+                if (idx >= res.length) {
+                    idx = 1;
+                }
+                res[idx] = (char) (i + 'a');
+                idx += 2;
+                hash[i]--;
+            }
+        }
+        return String.valueOf(res);
     }
-    return sb.toString();
     }
-}
