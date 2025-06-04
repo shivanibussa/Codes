d7ea7616fe204117253d27831bb6be57f8aff7cc
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-03 22:25:40 -0500

    Time: 0 ms (100%), Space: 42.4 MB (26.1%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,25 @@
+class Solution 
+{
+    public void rotate(int[][] mat) 
+    {
+        int n = mat.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n/2;j++)
+            {
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][n-1-j];
+                mat[i][n-1-j] = temp;
+            }
+        }
+    }
+}
