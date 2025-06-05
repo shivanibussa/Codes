df8a328807a3b17d8b951ad98979cd75f95ac722
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-04 20:05:52 -0500

    Time: 0 ms (100%), Space: 41.6 MB (66.69%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -2,17 +2,27 @@ class Solution
 {
     public int compareVersion(String version1, String version2) 
     {
-        String v1[] = version1.split("\\.");
-        String v2[] = version2.split("\\.");
-        int l1 = v1.length,l2 = v2.length;
-
-        for(int i=0;i<Math.max(l1,l2);i++)
+        int i=0,j=0;
+        while(i<version1.length() || j<version2.length())
         {
-            int c1 = i<l1 ? Integer.parseInt(v1[i]) : 0;
-            int c2 = i<l2 ? Integer.parseInt(v2[i]) : 0;
-            if(c1>c2)   return 1;
-            if(c2>c1)   return -1;
+            int num1=0,num2=0;
+            while(i<version1.length() && version1.charAt(i)!='.')
+            {
+                num1 = num1*10 + (version1.charAt(i++)-'0');
+            }
+            while(j<version2.length() && version2.charAt(j)!='.')
+            {
+                num2 = num2*10 + (version2.charAt(j++)-'0');
+            }
+
+            if(num1<num2)
+                return -1;
+            if(num1>num2)
+                return 1;
+            i++;
+            j++;
         }
         return 0;
+
     }
 }
