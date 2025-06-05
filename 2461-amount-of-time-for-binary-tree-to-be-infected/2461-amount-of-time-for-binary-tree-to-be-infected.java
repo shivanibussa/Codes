a47e2cce12488305f1be878cffa05cfe8b8953c4
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-05 17:45:31 -0500

    Time: 106 ms (47.02%), Space: 66.9 MB (98.63%) - LeetHub

--- 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.java
@@ -13,85 +13,84 @@
  *     }
  * }
  */
-
-class Solution 
-{
-    public void parentsFeed(HashMap<TreeNode,TreeNode> parents,TreeNode root)
+class Solution {
+    public void parentsFeed(HashMap<TreeNode,TreeNode> tm,TreeNode root)
     {
         Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
+        q.add(root);
+        // HashMap<Tr,Integer>tm = new HashMap<>();
         while(!q.isEmpty())
         {
             TreeNode pop = q.poll();
             if(pop.left!=null)
             {
                 q.add(pop.left);
-                parents.put(pop.left,pop);
+                tm.put(pop.left,pop);
+            }   
+            if(pop.right!=null)
+            {
+                q.add(pop.right);
+                tm.put(pop.right,pop);
+            }
+        }
+    }
+    public TreeNode getStart(int start,TreeNode root)
+    {
+        if(root.val==start)
+            return root;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            TreeNode pop = q.poll();
+            if(pop.val==start)
+                return pop;
+            if(pop.left!=null)
+            {
+                q.add(pop.left);
             }
             if(pop.right!=null)
             {
                 q.add(pop.right);
-                parents.put(pop.right,pop);
             }
         }
+        return null;
     }
     public int amountOfTime(TreeNode root, int start) 
     {
         HashMap<TreeNode,TreeNode> parents = new HashMap<>();
-        HashMap<TreeNode,Boolean> visited = new HashMap<>();
         parentsFeed(parents,root);
         Queue<TreeNode> q = new LinkedList<>();
-        int curr = 0;
-
-
-        q.offer(root);
-        TreeNode targetN = null;
+        ArrayList<Integer> res = new ArrayList<>();
+        HashMap<TreeNode,Boolean> visited = new HashMap<>();
+        TreeNode target = getStart(start,root);
+        q.add(target);
+        int steps=0;
         while(!q.isEmpty())
         {
-            TreeNode node = q.poll();
-            if(node.val==start)
-            {
-                targetN = node;
-                break;
-            }
-            if(node.left!=null)
-            {
-                q.offer(node.left);
-            }
-            if(node.right!=null)
-            {
-                q.offer(node.right);
-            }
-        }
-        q.clear();
-
-        q.add(targetN);
-        while(!q.isEmpty())
-        {
+            steps++;
             int s = q.size();
-            curr++;
             for(int i=0;i<s;i++)
             {
                 TreeNode pop = q.poll();
                 visited.put(pop,true);
-
                 if(pop.left!=null && visited.get(pop.left)==null)
                 {
-                    q.offer(pop.left);
+                    q.add(pop.left);
                     visited.put(pop.left,true);
-                } 
+                }
                 if(pop.right!=null && visited.get(pop.right)==null)
                 {
-                    q.offer(pop.right);
+                    q.add(pop.right);
                     visited.put(pop.right,true);
                 }
                 if(parents.get(pop)!=null && visited.get(parents.get(pop))==null)
                 {
-                    q.offer(parents.get(pop));
+                    q.add(parents.get(pop));
                     visited.put(parents.get(pop),true);
                 }
             }
         }
-        return curr-1;
+        return steps-1;
     }
 }
