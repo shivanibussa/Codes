903eb8ad62ef5112812b626ad324a2f8bbbe9251
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-05 12:22:36 -0500

    Time: 0 ms (100%), Space: 41.5 MB (81.55%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,51 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution 
 {
     public List<Integer> inorderTraversal(TreeNode root) 
     {
-        TreeNode curr = root;
-        ArrayList<Integer> inorder = new ArrayList<>();
-        while(curr!=null)
+        Stack<TreeNode> st = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+        while(true)
         {
-            if(curr.left==null)
+            if(root!=null)
             {
-                inorder.add(curr.val);
-                curr = curr.right;
+                st.add(root);
+                root = root.left;
             }
             else
             {
-                TreeNode prev = curr.left;
-                while(prev.right!=null && prev.right!=curr)
-                {
-                    prev = prev.right;
-                }
-                if(prev.right==null)
-                {
-                    prev.right = curr;
-                    curr = curr.left;
-                }
-                else
-                {
-                    prev.right = null;
-                    inorder.add(curr.val);
-                    curr = curr.right;
-                }
+                if(st.isEmpty())
+                    break;
+
+                TreeNode pop = st.pop();
+                res.add(pop.val);
+                if(pop.right!=null)
+                    root = pop.right;
             }
         }
-        return inorder;
+        return res;
     }
 }
