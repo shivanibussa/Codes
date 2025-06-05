9f8ebc9a0f63622b14642e1b3c0d4e6592c8eac4
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-05 13:54:15 -0500

    Time: 0 ms (100%), Space: 41 MB (83.68%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,46 +1,14 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution 
 {
-    public boolean isSameTree(TreeNode p, TreeNode qn) 
+    public boolean isSameTree(TreeNode p, TreeNode q) 
     {
-        if(p==null || qn==null)
-            return p==qn;
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(p);
-        q.add(qn);
-        while(!q.isEmpty())
-        {
-            TreeNode pop1 = q.poll();
-            TreeNode pop2 = q.poll();
-
-            if(pop1==null && pop2==null)
-                continue;
-            if(pop1==null ||pop2==null)
-                return false;
-
-            if(pop1.val!=pop2.val)
-                return false;
-
-            q.offer(pop1.left);
-            q.offer(pop2.left);
-            q.offer(pop1.right);
-            q.offer(pop2.right);  
-        }
-        return true;
+        return f(p,q);
+    }
+    public boolean f(TreeNode p,TreeNode q)
+    {
+        if(p==null || q==null)
+            return p==q;
+        
+        return p.val==q.val && f(p.left,q.left) && f(p.right,q.right);
     }
 }
