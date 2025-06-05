4dfb80209081b95a387c0b90e771378bf8d821a1
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-05 16:55:42 -0500

    Time: 0 ms (100%), Space: 42 MB (42.29%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,55 +1,13 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution 
 {
     public boolean isSymmetric(TreeNode root) 
     {
-        if(root==null)
-            return true;
-
-        Queue<TreeNode> q= new LinkedList<>();
-        q.add(root.left);
-        q.add(root.right);
-
-        while(!q.isEmpty())
-        {
-            TreeNode pop1 = q.poll();
-            TreeNode pop2 = q.poll();
-
-            if(pop1==null && pop2==null)
-                continue;
-
-            if(pop1==null || pop2==null)
-                return false;
-
-            if(pop1.val!=pop2.val)
-                return false;
-
-            q.add(pop1.left);
-            q.add(pop2.right);
-            q.add(pop2.left);
-            q.add(pop1.right);
-        }
         return f(root.left,root.right);
     }
     public boolean f(TreeNode p,TreeNode q)
     {
         if(p==null || q==null)
             return p==q;
-
-        return (p.val==q.val) && f(p.left,q.right) && f(p.right,q.left);
+        return p.val==q.val && f(p.left,q.right) && f(p.right,q.left);
     }
 }
