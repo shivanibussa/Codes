b288d8cf001f832456aa3d2307cfe82ccb424c64
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-05 16:49:59 -0500

    Time: 0 ms (100%), Space: 42.1 MB (79.52%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,36 +1,20 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution 
 {
     public List<Integer> rightSideView(TreeNode root) 
     {
         ArrayList<Integer> res = new ArrayList<>();
-        f(root,0,res);
+        f(root,res,0);
         return res;
     }
-    public void f(TreeNode root,int level,ArrayList<Integer> res)
+    public void f(TreeNode root,ArrayList<Integer> res,int level)
     {
         if(root==null)
             return;
-
+        
         if(level==res.size())
             res.add(root.val);
-            
-        f(root.right,level+1,res);
-        f(root.left,level+1,res);
-        
+
+        f(root.right,res,level+1);
+        f(root.left,res,level+1);
     }
 }
