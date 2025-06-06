d1c8069e6766edd5647f7bbd1c4e1798724cb605
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-06 13:49:13 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,78 +1,49 @@
-//{ Driver Code Starts
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+class Solution 
+{
+    static int knapsack(int W, int val[], int wt[])
+    {
 
-public class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCases = Integer.parseInt(br.readLine());
-
-        while (testCases-- > 0) {
-            int capacity = Integer.parseInt(br.readLine());
-            String[] valInput = br.readLine().split(" ");
-            String[] wtInput = br.readLine().split(" ");
-
-            int[] val = new int[valInput.length];
-            int[] wt = new int[wtInput.length];
-
-            for (int i = 0; i < valInput.length; i++) {
-                val[i] = Integer.parseInt(valInput[i]);
-            }
-
-            for (int i = 0; i < wtInput.length; i++) {
-                wt[i] = Integer.parseInt(wtInput[i]);
-            }
-
-            System.out.println(Solution.knapsack(capacity, val, wt));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-class Solution {
-    static int knapsack(int W, int val[], int wt[]) {
-        // code here
         int n = wt.length;
-        int dp[] = new int[[W+1];
+        int dp[][] = new int[n][W+1];
+        
         for(int j=0;j<=W;j++)
         {
             if(j>=wt[0])
                 dp[0][j] = val[0];
+            else 
+                dp[0][j]= 0;
         }
+        
         for(int i=1;i<n;i++)
         {
             for(int j=0;j<=W;j++)
             {
-                int not_take = 0+dp[i-1][j];
                 int take = Integer.MIN_VALUE;
+                int not_take = dp[i-1][j];
                 if(j>=wt[i])
                     take = val[i]+dp[i-1][j-wt[i]];
                     
                 dp[i][j] = Math.max(take,not_take);
-                
             }
         }
+        
         return dp[n-1][W];
     }
-    static int f(int W, int val[],int wt[],int n)
+    static int f(int n,int val[],int wt[],int W)
     {
         if(n==0)
         {
-            if(W>=wt[0])
-                return val[0];
+            if(W>=wt[n])
+                return val[n];
             else
                 return 0;
         }
-        int not_take = 0+f(W,val,wt,n-1);
-        int take = Integer.MIN_VALUE;
-        if(W>=wt[n])
-            take = val[n]+f(W-wt[n],val,wt,n-1);
-            
-        return Math.max(take,not_take);
         
+        int take = Integer.MIN_VALUE;
+        int not_take = f(n-1,val,wt,W);
+        if(W>=wt[n])
+            take = val[n]+f(n-1,val,wt,W-wt[n]);
+        
+        return Math.max(take,not_take);
     }
 }
