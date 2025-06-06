8484780117220604570702f186fe35f9b89593bc
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-06 18:40:51 -0500

    Time: 23 ms (37.27%), Space: 44.5 MB (88.95%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -3,15 +3,15 @@ class Solution
     public boolean isMatch(String s1, String s2) 
     {
         int m = s1.length(), n = s2.length();
-        boolean dp[][] = new boolean [m+1][n+1];
+        boolean dp[][] = new boolean[m+1][n+1];
         dp[0][0] = true;
         for(int i=1;i<=m;i++)
         {
-            dp[i][0] = false;
+            dp[i][0]=false;
         }
         for(int j=1;j<=n;j++)
         {
-            boolean flag = true;
+            boolean flag=true;
             for(int ii=j;ii>0;ii--)
             {
                 if(s2.charAt(ii-1)!='*')
@@ -30,33 +30,11 @@ class Solution
                 if(s1.charAt(i-1)==s2.charAt(j-1) || s2.charAt(j-1)=='?')
                     dp[i][j] = dp[i-1][j-1];
                 else if(s2.charAt(j-1)=='*')
-                    dp[i][j] =  dp[i-1][j] || dp[i][j-1];
+                    dp[i][j] = dp[i-1][j] | dp[i][j-1];
                 else
                     dp[i][j] = false;
             }
         }
         return dp[m][n];
-    }
-    public boolean f(String s1,String s2,int i,int j)
-    {
-        if(i<0 && j<0)
-            return true;
-        if(j<0 && i>=0)
-            return false;
-        if(i<0)
-        {
-            for(int ii=j;ii>=0;ii--)
-            {
-                if(s2.charAt(ii)!='*')
-                    return false;
-            }
-            return true;
-        }
-
-        if(s1.charAt(i)==s2.charAt(j) || s2.charAt(j)=='?')
-            return f(s1,s2,i-1,j-1);
-        else if(s2.charAt(j)=='*')
-            return f(s1,s2,i-1,j) || f(s1,s2,i,j-1);
-        return false;
     }
 }
