8a4b6b4fa01f8e8c68b7803b21695ed2f2985324
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-06 09:43:09 -0500

    Time: 0 ms (100%), Space: 41.1 MB (65.77%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -2,25 +2,28 @@ class Solution
 {
     public int rob(int[] nums) 
     {
-        int n = nums.length;
+        int n=nums.length;
         if(n==1)
             return nums[0];
-        return Math.max(rob(nums,0,n-2),rob(nums,1,n-1));
+        return Math.max(f(0,n-2,nums),f(1,n-1,nums));
     }
-    public int rob(int[] nums,int s,int e) 
+
+    public int f(int s,int e,int nums[]) 
     {
-        int n = nums.length;
-        int prev = nums[s],prev2=0;
+        int n=nums.length;
+        
+        int prev=nums[s];
+        int prev2=0;
         for(int i=s+1;i<=e;i++)
         {
-            int take=nums[i];
+            int take = nums[i];
             int not_take = 0+prev;
             if(i-2>=0)
-                take +=prev2;
+                take+=prev2;
 
             int curr = Math.max(take,not_take);
-            prev2=prev;
-            prev=curr;
+            prev2 = prev;
+            prev = curr;
         }
         return prev;
     }
