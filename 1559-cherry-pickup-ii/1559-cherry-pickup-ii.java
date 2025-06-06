1c886b9a7b053f4f762e1e5204394048037b8e88
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-06 12:41:24 -0500

    Time: 37 ms (21.16%), Space: 46 MB (67.8%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
@@ -2,46 +2,49 @@ class Solution
 {
     public int cherryPickup(int[][] grid) 
     {
-        int m = grid.length, n=grid[0].length;
-        int dp[][][] = new int[m][n][n];
-        for(int j=0;j<n;j++)
+        int r = grid.length,c=grid[0].length;
+        int dp[][][] = new int[r][c][c];
+        for(int j1=0;j1<c;j1++)
         {
-            for(int j2=0;j2<n;j2++)
+            for(int j2=0;j2<c;j2++)
             {
-                if(j==j2)
-                    dp[m-1][j][j2] = grid[m-1][j];
+                if(j1==j2)
+                    dp[r-1][j1][j2] = grid[r-1][j1];
                 else
-                    dp[m-1][j][j2] = grid[m-1][j]+grid[m-1][j2];
+                    dp[r-1][j1][j2] = grid[r-1][j1]+grid[r-1][j2];
             }
         }
-        for(int i=m-2;i>=0;i--)
+
+        for(int i=r-2;i>=0;i--)
         {
-            for(int j1=0;j1<n;j1++)
+            for(int j1=0;j1<c;j1++)
             {
-                for(int j2=0;j2<n;j2++)
+                for(int j2=0;j2<c;j2++)
                 {
-                    int maxi = Integer.MIN_VALUE;
-                    for(int dj1 =-1;dj1<=1;dj1++)
+                    int max = -(int)1e8;
+                    for(int dj1=-1;dj1<=1;dj1++)
                     {
                         for(int dj2=-1;dj2<=1;dj2++)
                         {
-                            int value=0;
+                            int val =0 ;
                             if(j1==j2)
-                                value+=grid[i][j1];
+                                val=grid[i][j1];
                             else
-                                value=grid[i][j1]+grid[i][j2];
-                            if(j1+dj1>=0 && j2+dj2>=0 && j1+dj1<n && j2+dj2<n)
-                                value+=dp[i+1][j1+dj1][j2+dj2];
+                            {
+                                val = grid[i][j1]+grid[i][j2];
+                            }
+                            if(j1+dj1>=0 && j2+dj2>=0 && j1+dj1<c && j2+dj2<c)
+                                val+=dp[i+1][j1+dj1][j2+dj2];
                             else
-                                value = Integer.MIN_VALUE;
-
-                            maxi = Math.max(maxi,value);
+                                val = -(int)1e8;
+                            max = Math.max(max,val);
                         }
                     }
-                    dp[i][j1][j2] = maxi;
+                    dp[i][j1][j2]= max;
                 }
             }
         }
-        return dp[0][0][n-1];
+           return dp[0][0][c-1];
     }
+ 
 }
