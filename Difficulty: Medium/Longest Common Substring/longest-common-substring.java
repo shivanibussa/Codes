234377caffc7d10bd3278f07e5359fa1ac29a048
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-06 17:04:02 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.java
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.java
@@ -2,25 +2,23 @@
 
 class Solution 
 {
-    public int longestCommonSubstr(String s1, String s2) 
+    public int longestCommonSubstr(String text1, String text2)
     {
-        int m = s1.length(), n= s2.length(),maxi=Integer.MIN_VALUE;
+        int m = text1.length(), n=text2.length(),maxL=0;
         int dp[][] = new int[m+1][n+1];
         for(int i=1;i<=m;i++)
         {
             for(int j=1;j<=n;j++)
             {
-                if(s1.charAt(i-1)==s2.charAt(j-1))
-                {
+                if(text1.charAt(i-1)==text2.charAt(j-1))
                     dp[i][j] = 1+dp[i-1][j-1];
-                }
                 else
-                {
                     dp[i][j] = 0;
-                }
-                maxi = Math.max(maxi,dp[i][j]);
+                    
+                maxL = Math.max(maxL,dp[i][j]);
             }
         }
-        return maxi;
+        return maxL;
+        
     }
 }
