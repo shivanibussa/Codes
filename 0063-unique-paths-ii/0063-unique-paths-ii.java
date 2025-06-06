bc6f6f6af43dd31944b61a41bbe2109c98323abc
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-06 10:32:47 -0500

    Time: 0 ms (100%), Space: 41.5 MB (91.23%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,44 +1,25 @@
 class Solution 
 {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) 
+    public int uniquePathsWithObstacles(int[][] grid) 
     {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int dp[] = new int[n];
-
+        int m = grid.length, n= grid[0].length;
+        int dp[][] = new int[m][n];
         for(int i=0;i<m;i++)
         {
-            int temp[] = new int[n];
             for(int j=0;j<n;j++)
             {
-                if(obstacleGrid[i][j]==1)
-                    temp[j] = 0;
-
+                if(grid[i][j]==1)
+                    dp[i][j]=0;
                 else if(i==0 && j==0)
-                    temp[j] = 1;
+                    dp[i][j]=1;
                 else
-                {
-                    if(i-1>=0)
-                        temp[j]+=dp[j];
+                {   if(i-1>=0)
+                        dp[i][j] += dp[i-1][j];
                     if(j-1>=0)
-                        temp[j]+=temp[j-1];
+                        dp[i][j]+=dp[i][j-1];
                 }
             }
-            dp = temp;
         }
-        return dp[n-1];
-    }
-    public int f(int m,int n,int grid[][])
-    {
-        if(m<0 || n<0)
-            return 0;
-
-        if(m==0 && n==0)
-            return 1;
-        
-        if(grid[m][n]==1)
-            return 0;
-
-        return f(m-1,n,grid)+f(m,n-1,grid);
+        return dp[m-1][n-1];    
     }
 }
