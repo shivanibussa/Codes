ff255eec1609ee8573ed3af16310755bd12bfb06
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-06 17:24:38 -0500

    Time: 19 ms (95.92%), Space: 50.2 MB (88.86%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -1,55 +1,53 @@
 class Solution 
 {
-    public String shortestCommonSupersequence(String s1, String s2) 
+    public String shortestCommonSupersequence(String text1, String text2) 
     {
-        int m = s1.length(), n = s2.length();
+        int m = text1.length(), n=text2.length();
         int dp[][] = new int[m+1][n+1];
         for(int i=1;i<=m;i++)
         {
             for(int j=1;j<=n;j++)
             {
-                if(s1.charAt(i-1)==s2.charAt(j-1))
-                {
+                if(text1.charAt(i-1)==text2.charAt(j-1))
                     dp[i][j] = 1+dp[i-1][j-1];
-                }
                 else
-                {
                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-                }
             }
         }
-        int i = m, j = n;
+        int i =m,j=n;
         StringBuilder sb = new StringBuilder();
+
         while(i>0 && j>0)
         {
-            if(s1.charAt(i-1)==s2.charAt(j-1))
+            if(text1.charAt(i-1)==text2.charAt(j-1))
             {
-                sb.append(s1.charAt(i-1));
+                sb.append(text1.charAt(i-1));
                 i=i-1;
                 j=j-1;
             }
             else if(dp[i-1][j]>dp[i][j-1])
             {
-                sb.append(s1.charAt(i-1));
+                sb.append(text1.charAt(i-1));
                 i=i-1;
             }
             else
             {
-                sb.append(s2.charAt(j-1));
+                sb.append(text2.charAt(j-1));
                 j=j-1;
             }
         }
         while(i>0)
         {
-            sb.append(s1.charAt(i-1));
+            sb.append(text1.charAt(i-1));
             i=i-1;
         }
         while(j>0)
         {
-            sb.append(s2.charAt(j-1));
-            j=j-1;
+            sb.append(text2.charAt(j-1));
+                j=j-1;
         }
         sb.reverse();
         return sb.toString();
     }
+    
 }
