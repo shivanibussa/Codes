8b212809d2b9bd38b3bf11092cd144c3413885eb
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-05 23:31:01 -0500

    Time: 3 ms (65.65%), Space: 45.3 MB (38.19%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -1,74 +1,61 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
- class BSTIterator
- {
-    Stack<TreeNode> st = new Stack<>();
-    boolean reverse = false;
-    BSTIterator(TreeNode root,boolean isReverse)
+class BSTIterator {
+Stack<TreeNode> st = new Stack<>();
+boolean reverse;
+    public BSTIterator(TreeNode root,boolean isreverse) 
     {
-        reverse = isReverse;
-        pushAll(root);
+      reverse = isreverse;
+      pushAllNodes(root);
     }
-
-    public void pushAll(TreeNode root)
+    public void pushAllNodes(TreeNode root)
     {
-        while(root!=null)
+        if(reverse==false)
         {
-            st.push(root);
-            if(reverse==false)
-                root = root.left;
-            else
-                root = root.right;
+            for(;root!=null;root=root.left)
+                st.push(root);
+        }
+        else
+        {
+            for(;root!=null;root=root.right)
+                st.push(root);
         }
     }
-
-    public int next()
+    
+    public int next() 
     {
-        TreeNode pop = st.pop();
+        TreeNode pop =st.pop();
         if(reverse==false)
-            pushAll(pop.right);
+            pushAllNodes(pop.right);
         else
-            pushAll(pop.left);
-
+            pushAllNodes(pop.left);
         return pop.val;
     }
- }
-class Solution 
-{
-    public boolean findTarget(TreeNode root, int k) 
+    
+    public boolean hasNext() 
     {
-         if(root==null)
+        return !st.isEmpty();
+    }
+}
+
+class Solution
+{
+    public boolean findTarget(TreeNode root, int k)
+    {
+        if(root==null)
             return false;
         BSTIterator l = new BSTIterator(root,false);
         BSTIterator r = new BSTIterator(root,true);
 
         int i = l.next();
         int j = r.next();
-
         while(i<j)
         {
-            if(i+j==k)
-                return true;
             if(i+j<k)
                 i = l.next();
-            else
+            else if(i+j>k)
                 j = r.next();
+            else
+                return true;
         }
-
         return false;
-
     }
 }
