93454d44e5b9fd6bbdc9895adc6919e9f92190ca
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-05 23:57:15 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest BST/largest-bst.java
+++ Difficulty: Medium/Largest BST/largest-bst.java
@@ -1,20 +1,8 @@
-// User function Template for Java
-
-// class Node
-// {
-//     int data;
-//     Node left, right;
-
-//     public Node(int d)
-//     {
-//         data = d;
-//         left = right = null;
-//     }
-// }
 class Triplet
 {
-    public int maxV,minV,maxS;
-    
+    int minV;
+    int maxV;
+    int maxS;
     Triplet(int maxV,int minV,int maxS)
     {
         this.maxV = maxV;
@@ -22,26 +10,23 @@ class Triplet
         this.maxS = maxS;
     }
 }
-class Solution {
-
-    static Triplet bstHelper(Node root)
+class Solution
+{
+    static Triplet f(Node root)
     {
         if(root==null)
             return new Triplet(Integer.MIN_VALUE,Integer.MAX_VALUE,0);
-            
-        Triplet left = bstHelper(root.left);
-        Triplet right = bstHelper(root.right);
+        Triplet left = f(root.left);
+        Triplet right = f(root.right);
         
-        if(left.maxV < root.data && root.data<right.minV)
-        {
+        if(left.maxV<root.data && root.data<right.minV)
             return new Triplet(Math.max(root.data,right.maxV),Math.min(root.data,left.minV),left.maxS+right.maxS+1);
-        }
-        
         else
             return new Triplet(Integer.MAX_VALUE,Integer.MIN_VALUE,Math.max(left.maxS,right.maxS));
-    }
-    static int largestBst(Node root) {
-        return bstHelper(root).maxS;
         
+    }
+    static int largestBst(Node root) 
+    {
+        return f(root).maxS;
     }
 }
