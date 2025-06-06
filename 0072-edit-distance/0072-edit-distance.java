90e1621e2fb63ddc264920651d3c115d50066479
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-06 17:57:42 -0500

    Time: 5 ms (61.64%), Space: 45.3 MB (30.84%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,46 +1,31 @@
 class Solution 
 {
-    public int minDistance(String s1, String s2) 
+    public int minDistance(String text1, String text2) 
     {
-        int m = s1.length(), n = s2.length();
-        int dp[] = new int[n+1];
+        int m = text1.length(), n=text2.length();
+        int dp[][] = new int[m+1][n+1];
         for(int i=0;i<=m;i++)
         {
-            dp[0] = i;
+            dp[i][0] = i;
         }
         for(int j=0;j<=n;j++)
         {
-            dp[j] = j;
+            dp[0][j]=j;
         }
         for(int i=1;i<=m;i++)
         {
-            int temp [] = new int[n+1];
-            temp[0] = i;
             for(int j=1;j<=n;j++)
             {
-                if(s1.charAt(i-1)==s2.charAt(j-1))
-                    temp[j] = dp[j-1];
-                else
-                {
-                    int insert = temp[j-1];
-                    int update = dp[j-1];
-                    int delete = dp[j];
-                    temp[j] = 1+Math.min(insert,Math.min(update,delete));
+                if(text1.charAt(i-1)==text2.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1];
+                else{
+                    int insert = dp[i][j-1];
+                    int update = dp[i-1][j-1];
+                    int delete = dp[i-1][j];
+                    dp[i][j] = 1+Math.min(insert,Math.min(update,delete));
                 }
             }
-            dp = temp;
         }
-        return dp[n];
-    }
-    public int f(String s1,String s2,int i,int j)
-    {
-        if(i<0)
-            return j+1;
-        if(j<0)
-            return i+1;
-
-        if(s1.charAt(i)==s2.charAt(j))
-            return f(s1,s2,i-1,j-1);
-        return 1+Math.min(f(s1,s2,i-1,j-1),Math.min(f(s1,s2,i-1,j),f(s1,s2,i,j-1)));
+        return dp[m][n];
     }
 }
