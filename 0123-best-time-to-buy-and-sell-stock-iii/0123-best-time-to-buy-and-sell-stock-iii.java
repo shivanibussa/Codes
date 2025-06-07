643f8661d13139cc00cf5868582c5e45c0e9edbc
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-06 19:19:57 -0500

    Time: 56 ms (45.85%), Space: 63.7 MB (38.71%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -2,39 +2,29 @@ class Solution
 {
     public int maxProfit(int[] prices) 
     {
-        int n = prices.length;
-        int dp[][] = new int[2][3];
+        int n=prices.length;
+        int dp[][][] = new int[n+1][2][3];
         for(int i=n-1;i>=0;i--)
         {
-            int temp[][] = new int[2][3];
             for(int j=0;j<2;j++)
             {
                 for(int k=2;k>0;k--)
                 {
                     if(j==1)
                     {
-                        temp[j][k] = Math.max(-prices[i]+dp[0][k],0+dp[1][k]);
+                        int take = -prices[i]+dp[i+1][0][k];
+                        int not_take = 0+dp[i+1][1][k];
+                        dp[i][j][k] = Math.max(take,not_take);
                     }
                     else
                     {
-                        temp[j][k] = Math.max(prices[i]+dp[1][k-1],dp[0][k]);
+                        int sell = prices[i]+dp[i+1][1][k-1];
+                        int not_sell = 0+dp[i+1][0][k];
+                        dp[i][j][k] = Math.max(sell,not_sell);
                     }
                 }
             }
-            dp = temp;
         }
-        return dp[1][2];
-    }
-    public int f(int prices[],int ind,int buy,int cap)
-    {
-        if(ind==prices.length)
-            return 0;
-
-        if(cap==0)
-            return 0;
-
-        if(buy==1)
-            return Math.max(-prices[ind]+f(prices,ind+1,0,cap),f(prices,ind+1,1,cap));
-        return Math.max(prices[ind]+f(prices,ind+1,1,cap-1),f(prices,ind+1,0,cap));
+        return dp[0][1][2];
     }
 }
