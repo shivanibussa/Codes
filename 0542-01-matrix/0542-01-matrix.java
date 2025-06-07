49d430b863f1a5d727bc066037d7d91d7f0943dc
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-07 11:28:36 -0500

    Time: 15 ms (39.17%), Space: 47.2 MB (72.76%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -2,10 +2,12 @@ class Solution
 {
     public int[][] updateMatrix(int[][] mat) 
     {
-        int m = mat.length, n = mat[0].length;
+        int m = mat.length, n= mat[0].length;
+        Queue<int[]> q= new LinkedList<>();
         int visited[][] = new int[m][n];
         int dist[][] = new int[m][n];
-        Queue<int[]> q = new LinkedList<>();
+        int dr[] = new int[]{-1,0,1,0};
+        int dc[] = new int[]{0,-1,0,1};
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -13,27 +15,28 @@ class Solution
                 if(mat[i][j]==0)
                 {
                     q.add(new int[]{i,j,0});
-                    visited[i][j] = 1;
                 }
             }
         }
-        int dr[] = new int[]{-1,0,1,0};
-        int dc[] = new int[]{0,-1,0,1};
+
         while(!q.isEmpty())
         {
             int pop[] = q.poll();
-            int r = pop[0], c = pop[1], d = pop[2];
+            int x = pop[0],y=pop[1],d=pop[2];
+            
             for(int k=0;k<4;k++)
             {
-                int nr = r+dr[k];
-                int nc = c+dc[k];
-                if(nr>=0 && nc>=0 && nr<m && nc<n && visited[nr][nc]==0 && mat[nr][nc]==1)
+                int nr = x+dr[k];
+                int nc = y+dc[k];
+
+                if(nr>=0 && nr<m && nc>=0 && nc<n && visited[nr][nc]==0 && mat[nr][nc]==1)
                 {
-                    visited[nr][nc] = 1;
-                    dist[nr][nc] = d+1;
+                    visited[nr][nc]=1;
+                    dist[nr][nc]=d+1;
                     q.add(new int[]{nr,nc,dist[nr][nc]});
                 }
-            } 
+            }
+
         }
         return dist;
     }
