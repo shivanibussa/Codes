cc4e80082b1e2007e046a44b79845e0a0f590ad4
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-07 10:08:25 -0500

    Time: 6 ms (24.93%), Space: 51.9 MB (24.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,47 +2,45 @@ class Solution
 {
     public int numIslands(char[][] grid) 
     {
-        int m = grid.length, n = grid[0].length;
+        int m=grid.length,n=grid[0].length,islands=0;
         int visited[][] = new int[m][n];
-        int cnt=0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]=='1' && visited[i][j]==0)
                 {
-                    bfs(i,j,grid,visited);
-                    cnt++;
+                    bfs(grid,visited,i,j);
+                    islands++;
                 }
             }
         }
-        return cnt;
+        return islands;
     }
-    public void bfs(int i,int j,char grid[][],int visited[][])
+
+    public void bfs(char grid[][],int visited[][],int r,int c)
     {
         Queue<int[]> q = new LinkedList<>();
-        int rows = grid.length,cols = grid[0].length;
         int dr[] = new int[]{-1,0,1,0};
         int dc[] = new int[]{0,-1,0,1};
-        visited[i][j] = 1;
-        q.offer(new int[]{i,j});
+        int m=grid.length,n=grid[0].length;
+        visited[r][c]=1;
+        q.add(new int[]{r,c});
         while(!q.isEmpty())
         {
             int pop[] = q.poll();
-            int r = pop[0];
-            int c = pop[1];
+            int x=pop[0],y=pop[1];
 
             for(int k=0;k<4;k++)
             {
-                int nr = r+dr[k];
-                int nc = c+dc[k];
-
-                if(nr>=0 && nr<rows && nc>=0 && nc<cols && grid[nr][nc]=='1' && visited[nr][nc]==0)
+                int nr=x+dr[k],nc=y+dc[k];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]=='1' && visited[nr][nc]==0)
                 {
-                    q.offer(new int[]{nr,nc});
+                    q.add(new int[]{nr,nc});
                     visited[nr][nc]=1;
                 }
             }
+
         }
     }
 }
