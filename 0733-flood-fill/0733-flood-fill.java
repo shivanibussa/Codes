1875c62556625465c4bb007ff333431cb7567bba
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-07 10:16:49 -0500

    Time: 1 ms (48.97%), Space: 45.2 MB (33.48%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,35 +1,46 @@
 class Solution 
 {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color)
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) 
     {
-        int m = image.length, n=image[0].length;
-        boolean visited[][] = new boolean[m][n];
+        int prevC = image[sr][sc];
         Queue<int[]> q = new LinkedList<>();
-        int col = image[sr][sc];
-        q.add(new int[]{sr,sc});
-        visited[sr][sc]=true;
-        image[sr][sc] = color;
-        int dr[] =new int[]{-1,0,1,0};
+        int dr[] = new int[]{-1,0,1,0};
         int dc[] = new int[]{0,-1,0,1};
+        int m=image.length,n=image[0].length;
+        int visited[][] = new int[m][n];
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                visited[i][j]=-1;
+        visited[sr][sc]=1;
+        q.add(new int[]{sr,sc});
         while(!q.isEmpty())
         {
             int pop[] = q.poll();
-            int r = pop[0], c = pop[1];
+            int x=pop[0],y=pop[1];
 
             for(int k=0;k<4;k++)
             {
-                int nr = dr[k]+r;
-                int nc = c+dc[k];
-
-                if(nr>=0 && nc>=0 && nr<m && nc<n && image [nr][nc]==col && visited[nr][nc]==false)
+                int nr=x+dr[k],nc=y+dc[k];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && image[nr][nc]==prevC && visited[nr][nc]==-1)
                 {
-                    visited[nr][nc] = true;
-                    image[nr][nc] = color;
                     q.add(new int[]{nr,nc});
+                    visited[nr][nc]=1;
                 }
             }
+        }
 
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(visited[i][j]==1)
+                {
+                    image[i][j]=color;
+                }
+            }
         }
         return image;
     }
+    
 }
