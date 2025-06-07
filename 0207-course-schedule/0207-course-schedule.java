aca96559e6f88b2253e9f9e5ad97b1c7e22739db
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-07 13:57:35 -0500

    Time: 7 ms (48.31%), Space: 45.2 MB (70.09%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,45 +1,44 @@
 class Solution 
 {
-    public boolean canFinish(int V, int[][] prerequisites) 
+    public boolean canFinish(int V, int[][] edges) 
     {
+       int indegree[] = new int[V];
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int indegree[] = new int[V];
-        Queue<Integer> q = new LinkedList<>();
         ArrayList<Integer> res = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
         for(int i=0;i<V;i++)
-        {
             adj.add(new ArrayList<>());
-
-        }        
-        for(int preq[]:prerequisites)
+            
+        for(int i=0;i<edges.length;i++)
         {
-            int a = preq[0];
-            int b = preq[1];
-            adj.get(a).add(b);
-            indegree[b]++;
+           int a = edges[i][0];
+           int b = edges[i][1];
+           
+           adj.get(a).add(b);
+           indegree[b]++;
         }
-
+        
         for(int i=0;i<V;i++)
         {
             if(indegree[i]==0)
             {
-                q.offer(i);
+                q.add(i);
             }
         }
-        int cnt=0;
         while(!q.isEmpty())
         {
             int pop = q.poll();
-            cnt++;
+            res.add(pop);
             for(int it:adj.get(pop))
             {
                 indegree[it]--;
                 if(indegree[it]==0)
                 {
-                    q.offer(it);
+                    q.add(it);
                 }
             }
         }
-        return cnt==V ? true:false;
+        return res.size()==V;
+    
     }
 }
