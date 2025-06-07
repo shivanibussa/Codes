58bdf120769515a0e2c896a7b2437c121270fb90
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-07 12:54:17 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bipartite Graph/bipartite-graph.java
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.java
@@ -1,31 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        while (tc-- > 0) {
-            int V = sc.nextInt();
-            int E = sc.nextInt();
-            int[][] edges = new int[E][2];
-            for (int i = 0; i < E; i++) {
-                edges[i][0] = sc.nextInt();
-                edges[i][1] = sc.nextInt();
-            }
-
-            Solution obj = new Solution();
-            boolean ans = obj.isBipartite(V, edges);
-            System.out.println(ans ? "true" : "false");
-            System.out.println("~");
-        }
-        sc.close();
-    }
-}
-// } Driver Code Ends
-
-
 class Solution 
 {
     public boolean isBipartite(int V, int[][] edges) 
@@ -43,29 +15,36 @@ class Solution
             adj.get(x).add(y);
             adj.get(y).add(x);
         }
+        
         for(int i=0;i<V;i++)
         {
             if(color[i]==-1)
             {
-                if(dfs(i,adj,color,0)==false)
+                if(bfs(i,color,adj)==false)
                     return false;
             }
         }
         return true;
     }
-    public boolean dfs(int node, List<List<Integer>> adj, int color[],int col)
+    public boolean bfs(int node,int color[],List<List<Integer>> adj)
     {
-        color[node]=col;
-        for(int it:adj.get(node))
+        Queue<Integer> q = new LinkedList<>();
+        q.add(node);
+        color[node]=1;
+        while(!q.isEmpty())
         {
-            if(color[it]==-1)
+            int pop = q.poll();
+            for(int it:adj.get(pop))
             {
-                if(dfs(it,adj,color,1-col)==false)
+                if(color[it]==-1)
+                {
+                    q.add(it);
+                    color[it]=1-color[pop];
+                }
+                else if(color[it]==color[pop])
+                {
                     return false;
-            }
-            else if(color[node]==color[it])
-            {
-                return false;
+                }
             }
         }
         return true;
