0bc75fc80bf50a5228f7149d788fd9b6e2573124
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-07 13:55:53 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.java
+++ Difficulty: Medium/Topological sort/topological-sort.java
@@ -2,41 +2,42 @@ class Solution
 {
     public static ArrayList<Integer> topoSort(int V, int[][] edges) 
     {
-       int indegree[] = new int[V];
-       ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-       Queue<Integer> q = new LinkedList<>();
-       ArrayList<Integer> res = new ArrayList<>();
-       for(int i=0;i<V;i++)
+        int indegree[] = new int[V];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<V;i++)
             adj.add(new ArrayList<>());
             
-       for(int i=0;i<edges.length;i++)
-       {
+        for(int i=0;i<edges.length;i++)
+        {
            int a = edges[i][0];
            int b = edges[i][1];
            
            adj.get(a).add(b);
            indegree[b]++;
-       }
-       for(int i=0;i<V;i++)
-       {
-           if(indegree[i]==0)
-           {
-               q.add(i);
-           }
-       }
-       while(!q.isEmpty())
-       {
-           int pop = q.poll();
-           res.add(pop);
-           for(int it:adj.get(pop))
-           {
-               indegree[it]--;
-               if(indegree[it]==0)
-               {
-                   q.add(it);
-               }
-           }
-       }
-       return res;
+        }
+        
+        for(int i=0;i<V;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty())
+        {
+            int pop = q.poll();
+            res.add(pop);
+            for(int it:adj.get(pop))
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                {
+                    q.add(it);
+                }
+            }
+        }
+        return res;
     }
 }
