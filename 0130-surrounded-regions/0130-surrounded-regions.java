e73a8684d80f428f2aed76cdae83cbf558080f56
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-07 12:02:24 -0500

    Time: 3 ms (33.21%), Space: 45.3 MB (72.86%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -2,42 +2,54 @@ class Solution
 {
     public void solve(char[][] board) 
     {
+        Queue<int[]> q= new LinkedList<>();
         int m = board.length, n=board[0].length;
         int visited[][] = new int[m][n];
-        Queue<int[]> q = new LinkedList<>();
+        int dr[] = new int[]{-1,0,1,0};
+        int dc[] = new int[]{0,-1,0,1};
         for(int i=0;i<m;i++)
         {
             if(board[i][0]=='O')
+            {
                 q.add(new int[]{i,0});
+                visited[i][0] = 1;
+            }
 
             if(board[i][n-1]=='O')
+            {
                 q.add(new int[]{i,n-1});
+                visited[i][n-1]=1;
+            }
         }
 
         for(int j=0;j<n;j++)
         {
             if(board[0][j]=='O')
+            {
                 q.add(new int[]{0,j});
-            
+                visited[0][j]=1;
+            }
+
             if(board[m-1][j]=='O')
+            {
                 q.add(new int[]{m-1,j});
+                visited[m-1][j]=1;
+            }
         }
 
-        int dr[] = new int[]{-1,0,1,0};
-        int dc[] = new int[]{0,-1,0,1};
         while(!q.isEmpty())
         {
             int pop[] = q.poll();
-            int r = pop[0], c = pop[1];
-            visited[r][c] = 1;
+            int x = pop[0],y=pop[1];
+
             for(int k=0;k<4;k++)
             {
-                int nr = r+dr[k];
-                int nc = c+dc[k];
+                int nr = x+dr[k];
+                int nc = y+dc[k];
 
-                if(nr>=0 && nc>=0 && nr<m && nc<n && board[nr][nc]=='O' && visited[nr][nc]==0)
+                if(nr>=0 && nc>=0 && nr<m && nc<n && visited[nr][nc]==0 && board[nr][nc]=='O')
                 {
-                    visited[nr][nc] = 1;
+                    visited[nr][nc]=1;
                     q.add(new int[]{nr,nc});
                 }
             }
@@ -46,10 +58,12 @@ class Solution
         {
             for(int j=0;j<n;j++)
             {
-                if(board[i][j]=='O' && visited[i][j]==0)
+                if(visited[i][j]==0)
                     board[i][j] = 'X';
+                else
+                    board[i][j] ='O';
             }
         }
-        // return board;
+
     }
 }
