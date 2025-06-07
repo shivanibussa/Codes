f902f43d08db546b315e2ab40f01bf77adea807f
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-07 12:42:18 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.java
+++ Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.java
@@ -2,9 +2,9 @@
 
 class Solution 
 {
-
     int countDistinctIslands(int[][] grid) 
     {
+        
         int m = grid.length, n = grid[0].length;
         int visited[][] = new int[m][n];
         HashSet<ArrayList<String>> hs = new HashSet<>();
@@ -15,47 +15,40 @@ class Solution
                 if(visited[i][j]==0 && grid[i][j]==1)
                 {
                     ArrayList<String> temp = new ArrayList<>();
-                    bfs(i,j,visited,temp,grid,i,j);
+                    bfs(grid,visited,temp,i,j);
                     hs.add(temp);
                 }
             }
         }
         return hs.size();
-        
     }
     
-    public void bfs(int i,int j,int visited[][],ArrayList<String> temp, int grid[][],int r0,int c0)
+    public void bfs(int grid[][],int visited[][],ArrayList<String> al,int r,int c)
     {
-        int m = grid.length,n = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{i,j});
-        visited[i][j] = 1;
+        int m = grid.length,n=grid[0].length;
+        Queue<int[]>q = new LinkedList<>();
+        q.add(new int[]{r,c});
+        visited[r][c]=1;
         int dr[] = new int[]{-1,0,1,0};
         int dc[] = new int[]{0,-1,0,1};
-        
         while(!q.isEmpty())
         {
             int pop[] = q.poll();
-            int r = pop[0], c = pop[1];
-            temp.add(toString(r-r0,c-c0));
+            int x = pop[0],y=pop[1];
+            al.add(String.valueOf(x-r)+" "+String.valueOf(y-c));
+            
             
             for(int k=0;k<4;k++)
             {
+                int nr = x+dr[k];
+                int nc = y+dc[k];
                 
-                int nr = r+dr[k];
-                int nc = c+dc[k];
                 if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1 && visited[nr][nc]==0)
                 {
                     q.add(new int[]{nr,nc});
                     visited[nr][nc]=1;
                 }
-                
             }
-            
         }
-    }
-    public String toString(int a,int b)
-    {
-        return String.valueOf(a)+" "+String.valueOf(b);
     }
 }
