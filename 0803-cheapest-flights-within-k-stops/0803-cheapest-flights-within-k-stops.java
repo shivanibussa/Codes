a7b1da0042cedaa3e8f198f485ad3457cec7f9ab
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-07 21:13:23 -0500

    Time: 6 ms (83.21%), Space: 44.7 MB (36.46%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -2,42 +2,43 @@ class Solution
 {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) 
     {
-        int V = flights.length;
         int dist[] = new int[n];
         ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
         PriorityQueue<int[]> q = new PriorityQueue<>((a,b)->a[0]-b[0]);
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             adj.add(new ArrayList<>());
-            dist[i] = (int)1e9;
+            dist[i]=(int)1e9;
         }
         for(int i=0;i<flights.length;i++)
         {
-            int a = flights[i][0];
-            int b = flights[i][1];
-            int c = flights[i][2];
+            int a = flights[i][0], b = flights[i][1], c=flights[i][2];
             adj.get(a).add(new int[]{b,c});
         }
+        q.offer(new int[]{0,0,src});
         dist[src]=0;
-        q.add(new int[]{0,0,src});
+
         while(!q.isEmpty())
         {
             int pop[] = q.poll();
-            int stops = pop[0], wt = pop[1],ver = pop[2];
+            int stops = pop[0];
+            int d = pop[1];
+            int v = pop[2];
             if(stops>k)
-                continue;     
-            for(int it[]:adj.get(ver))
+                break;
+            for(int it[]:adj.get(v))
             {
                 int nv = it[0];
                 int nw = it[1];
-                if(dist[nv]>wt+nw)
+
+                if(dist[nv]>nw+d)
                 {
-                    dist[nv] = wt+nw;
+                    dist[nv]=nw+d;
                     q.offer(new int[]{stops+1,dist[nv],nv});
                 }
             }
         }
-        if(dist[dst]==(int)1e9)
-            return -1;
-        return dist[dst];
+        return (dist[dst]==(int)1e9 ? -1:dist[dst]);
+
     }
 }
