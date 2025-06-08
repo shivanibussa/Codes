c3aeb97cf0fde098371d7318feb57d4c8504407a
shivanibussa <98083010+shivanibussa@users.noreply.github.com>
2025-06-07 23:34:19 -0500

    Time: 4 ms (19.69%), Space: 48.4 MB (9.29%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,26 +1,75 @@
+class DisjointSet
+{
+    ArrayList<Integer> parent = new ArrayList<>();
+    ArrayList<Integer> size = new ArrayList<>();
+    DisjointSet(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            parent.add(i);
+            size.add(1);
+        }
+    }   
+    
+    
+    public int getUparent(int node)
+    {
+        if(node==parent.get(node))
+            return node;
+        int ulp = getUparent(parent.get(node));
+        parent.set(node,ulp);
+        return parent.get(node);
+    }
+    
+    
+    public void unionBySize(int u,int v)
+    {
+        int ulp_u = getUparent(u);
+        int ulp_v = getUparent(v);
+        
+        if(ulp_u==ulp_v)
+            return;
+            
+        else if(size.get(ulp_u)>size.get(ulp_v))
+        {
+            parent.set(ulp_v,ulp_u);
+            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
+        }
+        else
+        {
+             parent.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
+        }
+    }
+}
 class Solution 
 {
     public int findCircleNum(int[][] adj) 
     {
-        int V = adj.length, cntC=0;
-        boolean visited[] = new boolean[V];
-        ArrayList<Integer> res = new ArrayList<>();
+        int V = adj.length,cntC=0;
+        DisjointSet ds = new DisjointSet(V);
+        PriorityQueue<int[]>pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
+        int res=0;
+        for(int i=0;i<adj.length;i++)
+        {
+            for(int j=0;j<adj[0].length;j++)
+            {
+                if(adj[i][j]==1)
+                {
+                    if(ds.getUparent(i)!=ds.getUparent(j))
+                    {
+                        ds.unionBySize(i,j);
+                    }
+                }
+            }
+        }
         for(int i=0;i<V;i++)
         {
-            if(!visited[i]){
-                dfs(res,adj,visited,i);
+            if(ds.parent.get(i)==i)
+            {
                 cntC++;
             }
         }
         return cntC;
-    }
-    public void dfs(ArrayList<Integer> res, int adj[][],boolean visited[],int node)
-    {
-        visited[node]=true;
-        for(int i=0;i<adj[0].length;i++)
-        {
-            if(adj[node][i]==1 && visited[i]==false)
-                 dfs(res,adj,visited,i);
-        }
     }
 }
